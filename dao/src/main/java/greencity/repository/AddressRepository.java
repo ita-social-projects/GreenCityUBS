0c25a4d2fef1d05edc03d2c33d232c3f1fafab83
Oleh Bilonizhka <bilonigka19999@gmail.com>
2021-02-08 09:36:11 +0200

    fix

--- dao/src/main/java/greencity/repository/AddressRepository.java
+++ dao/src/main/java/greencity/repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package greencity.repository;
 
 import greencity.entity.coords.Coordinates;
+import greencity.entity.lang.Language;
 import greencity.entity.user.ubs.Address;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends CrudRepository<Address, Long> {
+    /**
+     * Method returns {@link Coordinates} of undelivered orders.
+     *
+     * @return list of {@link Coordinates}.
+     */
     @Query("select a.coordinates from Address a inner join UBSuser u on a = u.userAddress "
         + "inner join Order o on u = o.ubsUser "
         + "where o.orderStatus = 'NEW' and a.coordinates is not null")
