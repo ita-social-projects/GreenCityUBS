8e1a1c75e6958fd98d4228ddf0298efd848e45ce
Olya Levko <olyagusar@ukr.net>
2021-04-05 12:43:12 +0300

    added scheduler for updating certificate status

--- service/src/main/java/greencity/config/SenderScheduledMessages.java
+++ service/src/main/java/greencity/config/SenderScheduledMessages.java
@@ -1,16 +1,20 @@
 package greencity.config;
 
+import greencity.entity.order.Certificate;
+import greencity.repository.CertificateRepository;
 import greencity.ubstelegrambot.UBSBotService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @EnableScheduling
 @AllArgsConstructor
 public class SenderScheduledMessages {
     private final UBSBotService ubsBotService;
+    private final CertificateRepository certificateRepository;
 
     /**
      * The method every day at 09:00 am send a message to users that have not paid
@@ -28,5 +32,15 @@ public class SenderScheduledMessages {
     @Scheduled(cron = "0 0 8 * * ?", zone = "Europe/Kiev")
     public void sendMessageWhenGarbageTruckArrives() {
         ubsBotService.sendMessageWhenGarbageTruckArrives();
+    }
+
+    /**
+     * Method schedules updating a {@link Certificate} status to expired for each
+     * {@link Certificate} in which expiration date is off every midnight.
+     */
+    @Transactional
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void scheduleUpdateExpiredCertificates() {
+        certificateRepository.updateCertificateStatusToExpired();
     }
 }
