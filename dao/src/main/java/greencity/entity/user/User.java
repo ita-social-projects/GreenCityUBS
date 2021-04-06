5ab5223931743db9e452afc5ac68be3a215e88b3
Woland <gutejvova@gmail.com>
2021-04-06 17:38:08 +0300

    added entity ChangeOfPoints,changed User,Order service,added changelog

--- dao/src/main/java/greencity/entity/user/User.java
+++ dao/src/main/java/greencity/entity/user/User.java
@@ -1,15 +1,27 @@
 package greencity.entity.user;
 
+import greencity.entity.order.ChangeOfPoints;
 import greencity.entity.order.Order;
 import greencity.entity.telegram.TelegramBot;
 import greencity.entity.user.ubs.UBSuser;
-import lombok.*;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -32,12 +44,8 @@ public class User {
     @Column(columnDefinition = "int default 0")
     private Integer currentPoints;
 
-    @ElementCollection
-    @CollectionTable(name = "change_of_points_mapping",
-        joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")})
-    @MapKeyColumn(name = "date")
-    @Column(name = "amount")
-    private Map<LocalDateTime, Integer> changeOfPoints;
+    @OneToMany(mappedBy = "order")
+    private List<ChangeOfPoints> changeOfPointsList;
 
     @Column(columnDefinition = "int default 0")
     private Integer violations;
