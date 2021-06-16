446dfb29c143c6b66b73c34cc95e63ec85137d33
Orest Mahdziack <orestit@icloud.com>
2021-06-16 13:56:46 +0300

    Added new columns to Order entity.Added new methods to UBSManagementService for get order detail info,update order detail,and sum order detail.

--- service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
@@ -5,9 +5,7 @@ import greencity.client.RestClient;
 import greencity.constant.ErrorMessage;
 import greencity.dto.*;
 import greencity.entity.coords.Coordinates;
-import greencity.entity.order.Certificate;
-import greencity.entity.order.ChangeOfPoints;
-import greencity.entity.order.Order;
+import greencity.entity.order.*;
 import greencity.entity.user.User;
 import greencity.entity.user.ubs.Address;
 import greencity.exceptions.*;
@@ -22,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -40,6 +39,9 @@ public class UBSManagementServiceImpl implements UBSManagementService {
     private final HttpServletRequest httpServletRequest;
     private final AllValuesFromTableRepo allValuesFromTableRepo;
     private final ObjectMapper objectMapper;
+    private final BagRepository bagRepository;
+    private final BagTranslationRepository bagTranslationRepository;
+    private final UpdateOrderDetail updateOrderRepository;
 
     /**
      * {@inheritDoc}
@@ -486,6 +488,141 @@ public class UBSManagementServiceImpl implements UBSManagementService {
             .getUbsUser().getAddress();
         addressRepository.save(updateAddressOrderInfo(address, dtoUpdate));
         return modelMapper.map(addressRepository.findById(address.getId()).get(), OrderAddressDtoResponse.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+
+    @Override
+    public List<OrderDetailInfoDto> getOrderDetails(Long orderId, String language) {
+        OrderDetailDto dto = new OrderDetailDto();
+        Order order = orderRepository.getOrderDetails(orderId)
+            .orElseThrow(() -> new UnexistingOrderException(ORDER_WITH_CURRENT_ID_DOES_NOT_EXIST + orderId));
+        setOrderDetailDto(dto, order, orderId, language);
+        return modelMapper.map(dto, new TypeToken<List<OrderDetailInfoDto>>() {
+        }.getType());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+
+    @Override
+    public List<OrderDetailInfoDto> setOrderDetail(List<UpdateOrderDetailDto> request, String language) {
+        OrderDetailDto dto = new OrderDetailDto();
+
+        for (int i = 0; i < request.size(); i++) {
+            updateOrderRepository.updateAmount(request.get(i).getAmount(), request.get(i).getOrderId(),
+                request.get(i).getBagId().longValue());
+            updateOrderRepository.updateExporter(request.get(i).getExportedQuantity(), request.get(i).getOrderId(),
+                request.get(i).getBagId().longValue());
+            updateOrderRepository.updateConfirm(request.get(i).getConfirmedQuantity(), request.get(i).getOrderId(),
+                request.get(i).getBagId().longValue());
+        }
+
+        Order order = orderRepository.getOrderDetails(request.get(0).getOrderId())
+            .orElseThrow(() -> new UnexistingOrderException(
+                ORDER_WITH_CURRENT_ID_DOES_NOT_EXIST + request.get(0).getOrderId()));
+
+        setOrderDetailDto(dto, order, request.get(0).getOrderId(), language);
+
+        return modelMapper.map(dto, new TypeToken<List<OrderDetailInfoDto>>() {
+        }.getType());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+
+    @Override
+    public CounterOrderDetailsDto getOrderSumDetails(Long id) {
+        CounterOrderDetailsDto dto = new CounterOrderDetailsDto();
+        Order order = orderRepository.getOrderDetails(id)
+            .orElseThrow(() -> new UnexistingOrderException(ORDER_WITH_CURRENT_ID_DOES_NOT_EXIST + id));
+        List<Bag> bag = bagRepository.findBagByOrderId(id);
+        List<Certificate> currentCertificate = certificateRepository.findCertificate(id);
+
+        double sumAmount = 0;
+        double sumConfirmed = 0;
+        double sumExported = 0;
+
+        List<Integer> amountValues =
+            order.getAmountOfBagsOrdered().entrySet().stream().map(amount -> amount.getValue())
+                .collect(Collectors.toList());
+
+        List<Integer> confirmedValues =
+            order.getConfirmedQuantity().entrySet().stream().map(confirmed -> confirmed.getValue())
+                .collect(Collectors.toList());
+
+        List<Integer> exportedValues =
+            order.getExportedQuantity().entrySet().stream().map(exported -> exported.getValue())
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < bag.size(); i++) {
+            sumAmount += amountValues.get(i) * bag.get(i).getPrice();
+            sumConfirmed += confirmedValues.get(i) * bag.get(i).getPrice();
+            sumExported += exportedValues.get(i) * bag.get(i).getPrice();
+        }
+
+        if (currentCertificate.size() > 0) {
+            dto.setTotalSumAmount(
+                sumAmount - (currentCertificate.stream().map(x -> x.getPoints()).reduce((a1, a2) -> a1 + a2).get())
+                    - order.getPointsToUse());
+            dto.setTotalSumConfirmed(
+                sumConfirmed - (currentCertificate.stream().map(x -> x.getPoints()).reduce((a1, a2) -> a1 + a2).get())
+                    - order.getPointsToUse());
+            dto.setTotalSumExported(
+                sumExported - (currentCertificate.stream().map(x -> x.getPoints()).reduce((a1, a2) -> a1 + a2).get())
+                    - order.getPointsToUse());
+            dto.setCertificateBonus(
+                currentCertificate.stream().map(x -> x.getPoints()).reduce((a1, a2) -> a1 + a2).get().doubleValue());
+            dto.setCertificate(
+                currentCertificate.stream().map(x -> x.getCode()).collect(Collectors.toList()));
+        } else {
+            dto.setTotalSumAmount(sumAmount - order.getPointsToUse());
+            dto.setTotalSumConfirmed(sumConfirmed - order.getPointsToUse());
+            dto.setTotalSumExported(sumExported - order.getPointsToUse());
+        }
+
+        dto.setTotalAmount(
+            orderRepository.getOrderDetails(id).get().getAmountOfBagsOrdered().entrySet()
+                .stream().map(x -> x.getValue()).reduce(Integer::sum).get().doubleValue());
+        dto.setTotalConfirmed(
+            orderRepository.getOrderDetails(id).get().getConfirmedQuantity().entrySet()
+                .stream().map(x -> x.getValue()).reduce(Integer::sum).get().doubleValue());
+
+        dto.setTotalExported(
+            orderRepository.getOrderDetails(id).get().getExportedQuantity().entrySet()
+                .stream().map(x -> x.getValue()).reduce(Integer::sum).get().doubleValue());
+
+        dto.setSumAmount(sumAmount);
+        dto.setSumConfirmed(sumConfirmed);
+        dto.setSumExported(sumExported);
+        dto.setOrderComment(order.getComment());
+        dto.setNumberOrderFromShop(order.getAdditionalOrders());
+        dto.setBonus(order.getPointsToUse().doubleValue());
+
+        return dto;
+    }
+
+    private OrderDetailDto setOrderDetailDto(OrderDetailDto dto, Order order, Long orderId, String language) {
+        dto.setAmount(modelMapper.map(order, new TypeToken<List<BagMappingDto>>() {
+        }.getType()));
+
+        dto.setCapacityAndPrice(bagRepository.findBagByOrderId(orderId)
+            .stream()
+            .map(b -> modelMapper.map(b, BagInfoDto.class))
+            .collect(Collectors.toList()));
+
+        dto.setName(bagTranslationRepository.findAllByLanguageOrder(language, orderId)
+            .stream()
+            .map(b -> modelMapper.map(b, BagTransDto.class))
+            .collect(Collectors.toList()));
+
+        dto.setOrderId(orderId);
+
+        return dto;
     }
 
     private Address updateAddressOrderInfo(Address address, OrderAddressDtoUpdate dto) {
