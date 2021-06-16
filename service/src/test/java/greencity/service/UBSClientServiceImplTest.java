94d1dc2a421e5fa32b09f096e342752da90634f5
DanylkoMykola <69077320+DanylkoMykola@users.noreply.github.com>
2021-06-16 16:02:37 +0000

    Deploying to gh-pages from  @ 5cd951398580ecbd49c9182bf00af16acf680a29 🚀

--- service/src/test/java/greencity/service/UBSClientServiceImplTest.java
+++ service/src/test/java/greencity/service/UBSClientServiceImplTest.java
@@ -13,6 +13,7 @@ import greencity.entity.user.User;
 import greencity.entity.user.ubs.UBSuser;
 import greencity.exceptions.*;
 import greencity.entity.user.ubs.Address;
+import greencity.entity.user.ubs.UBSuser;
 import greencity.exceptions.BadOrderStatusRequestException;
 import greencity.exceptions.CertificateNotFoundException;
 import greencity.exceptions.NotFoundOrderAddressException;
@@ -149,14 +150,14 @@ class UBSClientServiceImplTest {
     @Test
     void makeOrderAgain() {
         List<OrderBagDto> dto = Collections.singletonList(getOrderBagDto());
-        when(entityManager.find(Order.class, 1L)).thenReturn(getOrderDoneByUser());
+        when(orderRepository.findById(1L)).thenReturn(Optional.of(getOrderDoneByUser()));
         when(modelMapper.map(any(Order.class), eq(new TypeToken<List<OrderBagDto>>() {
         }.getType()))).thenReturn(dto);
 
         List<OrderBagDto> result = ubsService.makeOrderAgain(1L);
 
         assertEquals(dto, result);
-        verify(entityManager, times(1)).find(Order.class, 1L);
+        verify(orderRepository, times(1)).findById(1L);
     }
 
     @Test
@@ -170,7 +171,7 @@ class UBSClientServiceImplTest {
     void makeOrderAgainShouldThrowBadOrderStatusException() {
         Order order = getOrderDoneByUser();
         order.setOrderStatus(OrderStatus.CANCELLED);
-        when(entityManager.find(Order.class, 1L)).thenReturn(order);
+        when(orderRepository.findById(1L)).thenReturn(Optional.of(order));
         Exception thrown = assertThrows(BadOrderStatusRequestException.class,
             () -> ubsService.makeOrderAgain(1L));
         assertEquals(thrown.getMessage(), ErrorMessage.BAD_ORDER_STATUS_REQUEST
