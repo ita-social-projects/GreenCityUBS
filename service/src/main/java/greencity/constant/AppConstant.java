7f86902be2ecac77c37d58a807c2d72de7ee9392
Zahar Veremchuk <zaharverem@gmail.com>
2021-06-20 16:36:48 +0300

    fix server error

--- service/src/main/java/greencity/constant/AppConstant.java
+++ service/src/main/java/greencity/constant/AppConstant.java
@@ -3,8 +3,7 @@ package greencity.constant;
 public final class AppConstant {
     public static final String AUTHORIZATION = "Authorization";
     public static final String ubsLink = "/ubs/userProfile";
-    public static final String DEFAULT_IMAGE =
-        "https://previews.123rf.com/images/carmenbobo/carmenbobo1409/carmenbobo140900741/31777554-stamp-with-word-standard-inside.jpg";
+    public static final String DEFAULT_IMAGE = "https://csb10032000a548f571.blob.core.windows.net/allfiles/4af6c99e-f2ec-4cab-bfb1-017f8c8b1ffbdefault_image.jpg";
 
     /**
      * Constructor.
