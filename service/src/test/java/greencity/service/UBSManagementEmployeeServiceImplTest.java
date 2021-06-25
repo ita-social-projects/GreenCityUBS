5fb6e246d7556aefe9b4743c6342f84c185913f3
DanylkoMykola <69077320+DanylkoMykola@users.noreply.github.com>
2021-06-25 18:18:59 +0300

    Add tests

--- service/src/test/java/greencity/service/UBSManagementEmployeeServiceImplTest.java
+++ service/src/test/java/greencity/service/UBSManagementEmployeeServiceImplTest.java
@@ -359,13 +359,13 @@ class UBSManagementEmployeeServiceImplTest {
         when(repository.findById(2L)).thenReturn(Optional.empty());
 
         Exception thrown1 = assertThrows(EmployeeNotFoundException.class,
-                () -> employeeService.deleteEmployeeImage(2L));
+            () -> employeeService.deleteEmployeeImage(2L));
         assertEquals(thrown1.getMessage(), ErrorMessage.EMPLOYEE_NOT_FOUND + 2L);
 
         when(repository.findById(1L)).thenReturn(Optional.of(employee));
 
         Exception thrown2 = assertThrows(EmployeeIllegalOperationException.class,
-                () -> employeeService.deleteEmployeeImage(1L));
+            () -> employeeService.deleteEmployeeImage(1L));
         assertEquals(thrown2.getMessage(), ErrorMessage.CANNOT_DELETE_DEFAULT_IMAGE);
     }
 }
