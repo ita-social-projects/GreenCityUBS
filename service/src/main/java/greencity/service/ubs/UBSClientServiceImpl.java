55bf8f296b9a169d1375d4f971e4b172eff58668
NazariySt <struk.nazariy@gmail.com>
2021-06-26 15:19:30 +0300

    Created controller for getting all information about order payments.v4.0

--- service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
@@ -3,7 +3,6 @@ package greencity.service.ubs;
 import greencity.client.RestClient;
 
 import greencity.constant.ErrorMessage;
-import static greencity.constant.ErrorMessage.*;
 import greencity.dto.*;
 import greencity.entity.enums.AddressStatus;
 import greencity.entity.enums.CertificateStatus;
