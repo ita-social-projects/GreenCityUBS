0b72a11ce353a7669539d3f9c2e67221de356569
Zahar Veremchuk <zaharverem@gmail.com>
2021-06-27 16:26:19 +0300

    Fix sort of addresses wrote some logic for certificates

--- service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
@@ -219,7 +219,6 @@ public class UBSClientServiceImpl implements UBSClientService {
             .stream()
             .sorted(Comparator.comparing(Address::getId))
             .filter(u -> u.getAddressStatus() != AddressStatus.DELETED)
-            .filter(u -> u.getAddressStatus() != AddressStatus.IN_ORDER || u.getActual() != false)
             .map(u -> modelMapper.map(u, AddressDto.class))
             .collect(Collectors.toList());
         return new OrderWithAddressesResponseDto(addressDtoList);
@@ -493,10 +492,10 @@ public class UBSClientServiceImpl implements UBSClientService {
                 certificate.setOrder(order);
                 orderCertificates.add(certificate);
                 sumToPay -= certificate.getPoints();
-
                 certPoints += certificate.getPoints();
                 if (certPoints >= sumToPay) {
                     sumToPay = 0;
+                    certificate.setCertificateStatus(CertificateStatus.USED);
                     tooManyCertificates = true;
                     if (dto.getPointsToUse() > 0) {
                         throw new IncorrectValueException(SUM_IS_COVERED_BY_CERTIFICATES);
