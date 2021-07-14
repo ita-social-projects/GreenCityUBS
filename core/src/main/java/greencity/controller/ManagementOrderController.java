a3caaf547827f0b345d87e67f07a3db081f807ee
rusanovscaia <rusanovscaia.n@gmail.com>
2021-07-14 15:58:11 +0000

    Deploying to gh-pages from  @ c9c0a4ea6f0320222ce8827f8b5d909663c92550 🚀

--- core/src/main/java/greencity/controller/ManagementOrderController.java
+++ core/src/main/java/greencity/controller/ManagementOrderController.java
@@ -432,4 +432,23 @@ public class ManagementOrderController {
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(ubsManagementService.updateOrderDetailStatus(id, dto));
     }
+
+    /**
+     * Controller deletes violation from order.
+     *
+     * @return {@link HttpStatus}
+     * @author Nadia Rusanovscaia.
+     */
+    @ApiOperation(value = "Delete violation from order")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = HttpStatuses.OK, response = ViolationDetailInfoDto.class),
+        @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
+        @ApiResponse(code = 401, message = HttpStatuses.UNAUTHORIZED),
+        @ApiResponse(code = 404, message = HttpStatuses.NOT_FOUND)
+    })
+    @DeleteMapping("/delete-violation-from-order/{orderId}")
+    public ResponseEntity<HttpStatus> deleteViolationFromOrder(@PathVariable Long orderId) {
+        ubsManagementService.deleteViolation(orderId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
