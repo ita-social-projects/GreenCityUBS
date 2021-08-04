726eddac49076d1491f71e7fdc15938709df6505
Ostap Mykhailivkiy <djtomsawyer1@gmail.com>
2021-08-04 23:44:29 +0300

    refactor order payment statuses in returnOverpayment methods

--- service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
@@ -227,6 +227,9 @@ public class UBSManagementServiceImpl implements UBSManagementService {
     public PaymentTableInfoDto getPaymentInfo(long orderId, Long sumToPay) {
         Order order = orderRepository.findById(orderId)
             .orElseThrow(() -> new UnexistingOrderException(ORDER_WITH_CURRENT_ID_DOES_NOT_EXIST + orderId));
+        if(order.getOrderStatus().equals(OrderStatus.CANCELLED)){
+           sumToPay = 0L;
+        }
         Long paidAmount = calculatePaidAmount(order);
         Long overpayment = calculateOverpayment(order, sumToPay);
         Long unPaidAmount = calculateUnpaidAmount(sumToPay, paidAmount);
