ffadc72719a871791d1cc23d8ba454c9273455fb
Ihor <volyanskiy.i@gmail.com>
2021-08-10 20:38:01 +0300

    Add check for an existing address

--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -51,6 +51,7 @@ public class ErrorMessage {
     public static String CANNOT_DELETE_DEFAULT_IMAGE = "You can't delete default image.";
     public static String NOT_FOUND_ADDRESS_BY_USER_UUID = "Not found address for user. ";
     public static String PAYMENT_NOT_FOUND = "Payment not found for order id: ";
+    public static final String ADDRESS_ALREADY_EXISTS = "Address already exists";
 
     /**
      * Constructor.
