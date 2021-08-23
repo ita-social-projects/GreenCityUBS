d55125c28fc300f82dcfd958472e9176d1b86451
Sakhnyasha <sakhnyasha@gmail.com>
2021-08-23 20:52:08 +0000

    Deploying to gh-pages from  @ 0b15dd057532f6133104c7a0941d7809f08cead4 🚀

--- service-api/src/main/java/greencity/service/ubs/ViberService.java
+++ service-api/src/main/java/greencity/service/ubs/ViberService.java
@@ -32,9 +32,9 @@ public interface ViberService {
      * of user.
      *
      * @param receiverId - indicates which user to send the greeting message.
-     * @param context    - contains uuid user.
+     * @param uuid       - contains uuid user.
      */
-    void sendWelcomeMessageAndPreRegisterViberBotForUser(String receiverId, String context);
+    void sendWelcomeMessageAndPreRegisterViberBotForUser(String receiverId, String uuid);
 
     /**
      * The method sends a message to user and is performed registration of user.
