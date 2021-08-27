e793a492f7d3c29f0d0decbebfb7bdacb31e83f6
Pashtetollo <oliksander007@ukr.net>
2021-08-27 18:19:24 +0300

    fixed sql injection vulnerability

--- dao/src/main/java/greencity/repository/AllValuesFromTableRepo.java
+++ dao/src/main/java/greencity/repository/AllValuesFromTableRepo.java
@@ -2,9 +2,13 @@ package greencity.repository;
 
 import greencity.filters.SearchCriteria;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -108,6 +112,13 @@ public class AllValuesFromTableRepo {
     public List<Map<String, Object>> findAllWithSorting(String column, String sortingType, int pages, int size) {
         int offset = pages * size;
         return jdbcTemplate
-            .queryForList(QUERY + " order by " + column + " " + sortingType + " limit " + size + " offset " + offset);
+            .queryForList((QUERY + " order by ? ? limit ? offset ?"),
+                (PreparedStatementCallback<Boolean>) ps -> {
+                    ps.setString(1, column);
+                    ps.setString(2, sortingType);
+                    ps.setString(3, Integer.toString(size));
+                    ps.setString(4, Integer.toString(offset));
+                    return ps.execute();
+                });
     }
 }
