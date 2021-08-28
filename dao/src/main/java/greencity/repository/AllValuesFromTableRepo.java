33db4bae52e40e6181ee982923b995985b3797a7
Vadym Makitra <hmarax3@gmail.com>
2021-08-28 16:21:34 +0300

    Roll beck AllValuesFromTableRepo

--- dao/src/main/java/greencity/repository/AllValuesFromTableRepo.java
+++ dao/src/main/java/greencity/repository/AllValuesFromTableRepo.java
@@ -109,13 +109,7 @@ public class AllValuesFromTableRepo {
     public List<Map<String, Object>> findAllWithSorting(String column, String sortingType, int pages, int size) {
         int offset = pages * size;
         return jdbcTemplate
-            .queryForList((QUERY + " order by ? ? limit ? offset ?"),
-                (PreparedStatementCallback<String>) ps -> {
-                    ps.setString(1, column);
-                    ps.setString(2, sortingType);
-                    ps.setString(3, Integer.toString(size));
-                    ps.setString(4, Integer.toString(offset));
-                    return String.valueOf(ps.execute());
-                });
+                .queryForList(QUERY + " order by " + column + " " + sortingType + " limit " + size + " offset " + offset);
+
     }
 }
