aa5fa3f60e27c83f9765f333169f9a075029bd44
VadymMakitra <84317209+VadymMakitra@users.noreply.github.com>
2021-08-28 11:27:29 +0000

    Deploying to gh-pages from  @ a5b4dfa23a661da2ec574649a8bccb2416b58fa6 🚀

--- dao/src/main/java/greencity/repository/AllValuesFromTableRepo.java
+++ dao/src/main/java/greencity/repository/AllValuesFromTableRepo.java
@@ -109,7 +109,13 @@ public class AllValuesFromTableRepo {
     public List<Map<String, Object>> findAllWithSorting(String column, String sortingType, int pages, int size) {
         int offset = pages * size;
         return jdbcTemplate
-            .queryForList(QUERY + " order by ? ? limit ? offset ?",
-                column, sortingType, Integer.toString(size), Integer.toString(offset));
+            .queryForList((QUERY + " order by ? ? limit ? offset ?"),
+                (PreparedStatementCallback<String>) ps -> {
+                    ps.setString(1, column);
+                    ps.setString(2, sortingType);
+                    ps.setString(3, Integer.toString(size));
+                    ps.setString(4, Integer.toString(offset));
+                    return String.valueOf(ps.execute());
+                });
     }
 }
