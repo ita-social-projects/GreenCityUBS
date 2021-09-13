5ea867e4f087a9c82a2aae20807e341d99a5ab34
LiubomyrPater <pater87lm@gmail.com>
2021-09-13 16:38:43 +0300

    entities have been commented

--- dao/src/main/java/greencity/entity/parameters/ColumnState.java
+++ dao/src/main/java/greencity/entity/parameters/ColumnState.java
@@ -3,38 +3,16 @@ package greencity.entity.parameters;
 import greencity.entity.enums.DataColumnType;
 import greencity.entity.enums.EditType;
 import greencity.entity.language.Title;
-import lombok.*;
 
-//@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(exclude = {"id"})
-@ToString
-@Builder
 public class ColumnState {
-    //@Id
     private long id;
-    //@Column(nullable = false)
     private String name;
-    //@Column(nullable = false)
     private int weight;
-    //@Column(nullable = false)
     private boolean sticky;
-    //@Column(nullable = false)
     private boolean visible;
-    //@Column(nullable = false)
     private int index;
-    //@Column(nullable = false)
     private EditType editType;
-
-    //@OneToOne
-    //@Column(nullable = false)
     private Title title;
-
-    //@ManyToOne
-    //@JoinColumn(name = "table_parameters_id")
     private TableParameters tableParameters;
-
     private DataColumnType dataColumnType;
 }
--- dao/src/main/java/greencity/entity/parameters/TableParameters.java
+++ dao/src/main/java/greencity/entity/parameters/TableParameters.java
@@ -2,27 +2,13 @@ package greencity.entity.parameters;
 
 import greencity.entity.enums.SortingOrder;
 import greencity.entity.user.User;
-import lombok.*;
 
 import java.util.List;
 
-//@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(exclude = {"id"})
-@ToString
-@Builder
 public class TableParameters {
-    //@Id
     private long id;
-    //@Column(nullable = false)
     private User user;
-    //@Column(nullable = false)
     private String sortingByColumn;
-    //@Column(nullable = false)
     private SortingOrder sortingOrder;
-
-    //@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "tableParameters")
     private List<ColumnState> columnStateDTOList;
 }
