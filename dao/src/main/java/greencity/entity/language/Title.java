5eaa51ffb3196be0296f015edd994828eb3421cf
LiubomyrPater <pater87lm@gmail.com>
2021-09-13 16:41:50 +0300

    entities have been commented

--- dao/src/main/java/greencity/entity/language/Title.java
+++ dao/src/main/java/greencity/entity/language/Title.java
@@ -1,21 +1,8 @@
 package greencity.entity.language;
 
-import lombok.*;
-
-//@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(exclude = {"id"})
-@ToString
-@Builder
-//@Table(name = "translatedTitles")
 public class Title {
-    //@Id
     private long id;
-    //@Column(name = "ukrainian", nullable = false)
     private String ua;
-    //@Column(name = "english", nullable = false)
     private String en;
 }
 
