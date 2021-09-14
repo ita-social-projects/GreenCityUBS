a057e8f692f800c3ec882da131d63b95161464fd
LiubomyrPater <pater87lm@gmail.com>
2021-09-14 19:04:39 +0300

    Add some trial methods.

--- dao/src/main/java/greencity/repository/SaveChangeInOrderRepo.java
+++ dao/src/main/java/greencity/repository/SaveChangeInOrderRepo.java
@@ -11,7 +11,6 @@ public class SaveChangeInOrderRepo {
      * Method for save changes imputed from orders table.
      */
     public boolean saveChange(String query, Long orderId, MultiValue multiValue) {
-        Class clazz = multiValue.getClazz();
         return true;
     }
 }
