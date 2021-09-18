6cd89c62d11750807837f36300514f8dd3f11b6b
Pashtetollo <oliksander007@ukr.net>
2021-09-18 19:41:50 +0300

    fixed typo

--- core/src/main/java/greencity/controller/ClientController.java
+++ core/src/main/java/greencity/controller/ClientController.java
@@ -56,7 +56,7 @@ public class ClientController {
      */
     @ApiOperation(value = "returns all user orders for logged user")
     @ApiResponses({
-        @ApiResponse(code = 200, message = HttpStatuses.OK, response = OrderInfoDto[].class),
+        @ApiResponse(code = 200, message = HttpStatuses.OK, response = OrderStatusPageDto[].class),
         @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
         @ApiResponse(code = 401, message = HttpStatuses.UNAUTHORIZED),
         @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN),
