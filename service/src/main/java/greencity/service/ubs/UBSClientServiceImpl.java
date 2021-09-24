d7bd567b4ac2ac41d41e5103f9ff1d5f7b51d657
LiubomyrPater <57452025+LiubomyrPater@users.noreply.github.com>
2021-09-24 17:14:15 +0000

    Deploying to gh-pages from  @ 19a8d0db5b74c9112610cd03e9034d3dfb259958 🚀

--- service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
@@ -738,11 +738,8 @@ public class UBSClientServiceImpl implements UBSClientService {
         if (userRepository.findByUuid(uuid) == null) {
             UbsCustomersDto ubsCustomersDto = restClient.findUserByUUid(uuid)
                 .orElseThrow(() -> new EntityNotFoundException("Such UUID have not been found"));
-            User user = userRepository.save(User.builder().currentPoints(0).violations(0).uuid(uuid)
+            userRepository.save(User.builder().currentPoints(0).violations(0).uuid(uuid)
                 .recipientEmail(ubsCustomersDto.getEmail()).recipientName(ubsCustomersDto.getName()).build());
-            UBSuser ubSuser =
-                UBSuser.builder().user(user).email(user.getRecipientEmail()).firstName(user.getRecipientName()).build();
-            ubsUserRepository.save(ubSuser);
         }
     }
 
