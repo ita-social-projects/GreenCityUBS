7cb1d056a09ba6a1d83c8e2ffa6b599653ad4bac
StepanTehlivets <steptehl@gmail.com>
2021-10-13 14:47:26 +0300

    Repo for returning user rewritten to CriteriaQuery Now repo returns page

--- service-api/src/main/java/greencity/service/ubs/ValuesForUserTableService.java
+++ service-api/src/main/java/greencity/service/ubs/ValuesForUserTableService.java
@@ -1,6 +1,11 @@
 package greencity.service.ubs;
 
 import greencity.dto.FieldsForUsersTableDto;
+import greencity.dto.PageableDto;
+import greencity.dto.UserWithSomeOrderDetailDto;
+import greencity.entity.enums.SortingOrder;
+import greencity.filters.UserSearchCriteria;
+import org.springframework.data.domain.Pageable;
 
 public interface ValuesForUserTableService {
     /**
@@ -9,5 +14,6 @@ public interface ValuesForUserTableService {
      * @return {@link FieldsForUsersTableDto}.
      * @author Stepan Tehlivets.
      */
-    FieldsForUsersTableDto getAllFields();
+    PageableDto<UserWithSomeOrderDetailDto> getAllFields(Pageable page, String columnName,
+        SortingOrder sortingOrder, UserSearchCriteria userSearchCriteria);
 }
