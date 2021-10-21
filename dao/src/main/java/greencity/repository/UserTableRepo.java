24f2c8cdb545a3181f9ba883b488466ee5611a39
StepanTehlivets <71794544+StepanTehlivets@users.noreply.github.com>
2021-10-21 14:53:58 +0000

    Deploying to gh-pages from  @ aea172e579091fc68be342d083ae4e3fe646d408 🚀

--- dao/src/main/java/greencity/repository/UserTableRepo.java
+++ dao/src/main/java/greencity/repository/UserTableRepo.java
@@ -2,7 +2,7 @@ package greencity.repository;
 
 import greencity.entity.enums.SortingOrder;
 import greencity.entity.user.User;
-import greencity.filters.UserSearchCriteria;
+import greencity.filters.UserFilterCriteria;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +10,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Objects.nonNull;
@@ -37,23 +36,19 @@ public class UserTableRepo {
      * @author - Stepan Tehlivets.
      */
 
-    public Page<User> findAll(UserSearchCriteria userSearchCriteria, String column,
+    public Page<User> findAll(UserFilterCriteria userFilterCriteria, String column,
         SortingOrder sortingOrder, Pageable page) {
         CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
-
         Root<User> userRoot = criteriaQuery.from(User.class);
         userRoot.join(ORDERS, JoinType.INNER);
-        criteriaQuery.distinct(true);
-
-        Predicate predicate = getPredicate(userSearchCriteria, userRoot);
-        criteriaQuery.where(predicate);
+        criteriaQuery.groupBy(userRoot.get("id"));
         setOrder(column, sortingOrder, criteriaQuery, userRoot);
 
         TypedQuery<User> typedQuery = entityManager.createQuery(criteriaQuery);
         typedQuery.setFirstResult(page.getPageNumber() * 10);
         typedQuery.setMaxResults(10);
 
-        Sort sort = Sort.by(Sort.Direction.valueOf(sortingOrder.toString()), column);
+        Sort sort = Sort.by(Sort.Direction.valueOf(sortingOrder.toString()), "recipientPhone");
         Pageable pageable = PageRequest.of(page.getPageNumber(), 10, sort);
 
         List<User> resultList = typedQuery.getResultList();
@@ -61,33 +56,26 @@ public class UserTableRepo {
         return new PageImpl<>(resultList, pageable, resultList.size());
     }
 
-    private Predicate getPredicate(UserSearchCriteria us, Root<User> userRoot) {
+    private Predicate getPredicate(UserFilterCriteria us, Root<User> userRoot) {
         List<Predicate> predicateList = new ArrayList<>();
 
-        if (nonNull(us.getUserEmail())) {
-            CriteriaBuilder.In<String> email = criteriaBuilder.in(
-                criteriaBuilder.upper(userRoot.get("recipient_email")));
-            Arrays.stream(us.getUserEmail())
-                .map(String::toUpperCase)
-                .forEach(email::value);
-            predicateList.add(email);
-        }
-        if (nonNull(us.getUserName())) {
-            CriteriaBuilder.In<String> name = criteriaBuilder.in(
-                criteriaBuilder.upper(userRoot.get("recipient_name")));
-            Arrays.stream(us.getUserEmail())
-                .map(String::toUpperCase)
-                .forEach(name::value);
-            predicateList.add(name);
-        }
         return criteriaBuilder.and(predicateList.toArray(new Predicate[0]));
     }
 
     private void setOrder(String column, SortingOrder sortingOrder, CriteriaQuery<User> criteriaQuery,
         Root<User> userRoot) {
-        Path<Object> sortBy = userRoot.get("recipientName");
+        Expression<?> sortBy = userRoot.get("recipientName");
+
         if (nonNull(column)) {
-            sortBy = userRoot.get(column);
+            if (column.equals("orderDate")) {
+                sortBy = criteriaBuilder.max(userRoot.getJoins().stream().findFirst().get().get("orderDate"));
+            } else if (column.equals("number_of_orders")) {
+                sortBy = criteriaBuilder.count(userRoot.getJoins().stream().findFirst().get().get("user"));
+            } else if (column.equals("clientName")) {
+                sortBy = userRoot.get("recipientName");
+            } else {
+                sortBy = userRoot.get(column);
+            }
         }
         if (sortingOrder.equals(SortingOrder.DESC)) {
             criteriaQuery.orderBy(criteriaBuilder.desc(sortBy));
