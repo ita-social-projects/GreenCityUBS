ea7b7d4fbcb1324c679c151bfc8aa07568b69b6d
KuzbytMaksym <max.dogmarion.lviv@gmail.com>
2021-11-12 18:44:59 +0200

    add predicate

--- dao/src/main/java/greencity/repository/BigOrderTableRepository.java
+++ dao/src/main/java/greencity/repository/BigOrderTableRepository.java
@@ -179,24 +179,28 @@ public class BigOrderTableRepository {
                 criteriaBuilder.like(orderRoot.get("orderDate").as(String.class),
                         "%" + sc.getSearch() + "%"),
                 criteriaBuilder.like(orderRoot.get("payment").get("orderTime").as(String.class),
-                        "%" + sc.getSearch() + "%"),
-                criteriaBuilder.like(orderRoot.get("user").get("violations").as(String.class),
                         "%" + sc.getSearch() + "%")
         );
 
     }
 
     private Predicate formUbsUserLikePredicate(OrderSearchCriteria sc, Root<Order> orderRoot) {
+        Expression<String> firstName = criteriaBuilder.concat(orderRoot.get(UBS_USER).get("firstName"), " ");
+        Expression<String> lastName = orderRoot.get(UBS_USER).get("lastName");
+        Expression<String> userName = criteriaBuilder.concat(firstName,lastName);
         return criteriaBuilder.or(
-            criteriaBuilder.like(criteriaBuilder.upper(orderRoot.get(UBS_USER).get("firstName")),
-                "%" + sc.getSearch().toUpperCase() + "%"),
-            criteriaBuilder.like(criteriaBuilder.upper(orderRoot.get(UBS_USER).get("lastName")),
-                "%" + sc.getSearch().toUpperCase() + "%"),
+            criteriaBuilder.like(criteriaBuilder.upper(firstName),"%" + sc.getSearch().toUpperCase() + "%"),
+            criteriaBuilder.like(criteriaBuilder.upper(lastName), "%" + sc.getSearch().toUpperCase() + "%"),
+            criteriaBuilder.like(criteriaBuilder.upper(userName), "%" + sc.getSearch().toUpperCase() + "%"),
             criteriaBuilder.like(criteriaBuilder.upper(orderRoot.get(UBS_USER).get("phoneNumber")),
                 "%" + sc.getSearch().toUpperCase() + "%"),
             criteriaBuilder.like(criteriaBuilder.upper(orderRoot.get(UBS_USER).get("email")),
                 "%" + sc.getSearch().toUpperCase() + "%"));
     }
+
+
+
+
 
     private Predicate formUserLikePredicate(OrderSearchCriteria sc, Root<Order> orderRoot) {
         return criteriaBuilder.or(
@@ -207,7 +211,10 @@ public class BigOrderTableRepository {
             criteriaBuilder.like(criteriaBuilder.upper(orderRoot.get(USER).get("recipientPhone")),
                 "%" + sc.getSearch().toUpperCase() + "%"),
             criteriaBuilder.like(criteriaBuilder.upper(orderRoot.get(USER).get("recipientEmail")),
-                "%" + sc.getSearch().toUpperCase() + "%"));
+                "%" + sc.getSearch().toUpperCase() + "%"),
+                criteriaBuilder.like(orderRoot.get(USER).get("violations").as(String.class),
+                        "%" + sc.getSearch() + "%")
+        );
     }
 
     private Predicate formAddressLikePredicate(OrderSearchCriteria sc, Root<Order> orderRoot) {
