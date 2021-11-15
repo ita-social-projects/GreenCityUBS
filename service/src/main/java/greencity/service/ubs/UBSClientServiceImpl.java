967ff292df5688ad22ab5ddac6d7c7da454f11ce
Zingerd <dima111847@gmail.com>
2021-11-15 22:38:39 +0200

    I created new method deleteOrder for ClientController and wrote unit test for this method

--- service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
@@ -1036,4 +1036,11 @@ public class UBSClientServiceImpl implements UBSClientService {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         return dateFormatter.format(date);
     }
+
+    @Override
+    public void deleteOrder(Long id) {
+        Order order = orderRepository.findById(id)
+            .orElseThrow(() -> new OrderNotFoundException(ORDER_WITH_CURRENT_ID_DOES_NOT_EXIST));
+        orderRepository.delete(order);
+    }
 }
