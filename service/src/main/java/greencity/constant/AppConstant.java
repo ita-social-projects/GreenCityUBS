9029df0631e8faf53c2536016f7b92269787177a
Max877866 <bohonko.max456@gmail.com>
2021-11-18 20:30:52 +0200

    fix blank line

--- service/src/main/java/greencity/constant/AppConstant.java
+++ service/src/main/java/greencity/constant/AppConstant.java
@@ -7,5 +7,4 @@ public final class AppConstant {
     public static final String ubsLink = "/ubs/userProfile";
     public static final String DEFAULT_IMAGE =
         "https://csb10032000a548f571.blob.core.windows.net/allfiles/90370622-3311-4ff1-9462-20cc98a64d1ddefault_image.jpg";
-
 }
