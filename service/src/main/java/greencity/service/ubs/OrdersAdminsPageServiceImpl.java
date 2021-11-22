3f1b4ef1b355619fd1886310a95c71facea1287e
LiubomyrPater <pater87lm@gmail.com>
2021-11-22 10:01:41 +0200

    add logger

--- service/src/main/java/greencity/service/ubs/OrdersAdminsPageServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/OrdersAdminsPageServiceImpl.java
@@ -17,7 +17,6 @@ import greencity.repository.*;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
