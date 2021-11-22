2791f0ed8afe915bc47c8b30241abd6c570a6e04
LiubomyrPater <pater87lm@gmail.com>
2021-11-22 22:07:52 +0200

    other changes

--- dao/src/main/java/greencity/entity/enums/OrderStatus.java
+++ dao/src/main/java/greencity/entity/enums/OrderStatus.java
@@ -6,14 +6,14 @@ import java.util.Set;
 
 // if values changed, change in order_status_translations table is required
 public enum OrderStatus {
-    FORMED(1, "CANCELED"),
-    ADJUSTMENT(2, "CANCELED"),
-    BROUGHT_IT_HIMSELF(3, "CANCELED"),
-    CONFIRMED(4, "CANCELED"),
-    ON_THE_ROUTE(5, "CANCELED"),
-    DONE(6, "CANCELED"),
-    NOT_TAKEN_OUT(7, "CANCELED"),
-    CANCELED(8, "CANCELED");
+    FORMED(1, "ADJUSTMENT", "CANCELLED", "BROUGHT_IT_HIMSELF"),
+    ADJUSTMENT(2, "BROUGHT_IT_HIMSELF", "CANCELLED", "CONFIRMED"),
+    BROUGHT_IT_HIMSELF(3, "DONE"),
+    CONFIRMED(4, "CANCELLED", "FORMED", "ON_THE_ROUTE"),
+    ON_THE_ROUTE(5, "DONE", "NOT_TAKEN_OUT"),
+    DONE(6, "DONE"),
+    NOT_TAKEN_OUT(7, "ADJUSTMENT", "NOT_TAKEN_OUT"),
+    CANCELLED(8, "CANCELLED");
 
     private int statusValue;
     private String[] possibleStatus;
