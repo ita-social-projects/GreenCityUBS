c7674009bb64a0f35213f7f22d3a40c39e66f2a6
Dima Sannytski <sbymix91@gmail.com>
2021-11-22 17:09:17 +0200

    code smell

--- dao/src/main/java/greencity/repository/LocationRepository.java
+++ dao/src/main/java/greencity/repository/LocationRepository.java
@@ -1,13 +1,10 @@
 package greencity.repository;
 
-import greencity.entity.order.Bag;
 import greencity.entity.user.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
