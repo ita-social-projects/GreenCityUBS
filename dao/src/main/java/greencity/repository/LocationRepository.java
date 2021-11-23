e00eb9eecf3f5c664885741026fc25a0da35fa73
Dima Sannytski <33062320+DimaSannytski@users.noreply.github.com>
2021-11-23 14:06:03 +0200

    Revert "devDev #3555"

--- dao/src/main/java/greencity/repository/LocationRepository.java
+++ dao/src/main/java/greencity/repository/LocationRepository.java
@@ -2,19 +2,8 @@ package greencity.repository;
 
 import greencity.entity.user.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
-    /**
-     * {@inheritDoc}
-     */
-    @Query(value = "SELECT * FROM locations as L"
-        + "where L.id in ("
-        + "SELECT DISTINCT B.location_id  FROM ORDER_BAG_MAPPING as OBM "
-        + "JOIN BAG AS B ON OBM.ORDER_ID = :orderId and OBM.BAG_ID = B.ID )"
-        + "LIMIT 1", nativeQuery = true)
-    Location findByOrderId(@Param("orderId") Long id);
 }
