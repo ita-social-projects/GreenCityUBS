c60cca8197b184f61c5551fdf04f72cc9d580bf9
RossSihovsk <rsihovskiy@gmail.com>
2021-11-24 09:57:28 +0200

    Fixed code smells again and checkstyle

--- service/src/test/java/greencity/config/CertificatesSchedulerTest.java
+++ service/src/test/java/greencity/config/CertificatesSchedulerTest.java
@@ -7,7 +7,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
