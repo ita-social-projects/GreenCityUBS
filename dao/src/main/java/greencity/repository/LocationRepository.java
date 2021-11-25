1b186109ff027860dd6dec219e3a927b9e016823
Dima Sannytski <sbymix91@gmail.com>
2021-11-25 19:17:21 +0200

    formatter

--- dao/src/main/java/greencity/repository/LocationRepository.java
+++ dao/src/main/java/greencity/repository/LocationRepository.java
@@ -12,9 +12,9 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
      * {@inheritDoc}
      */
     @Query(value = "SELECT * FROM locations as L "
-        + "where L.id in ("
+        + "where L.id in ( "
         + "SELECT DISTINCT B.location_id  FROM ORDER_BAG_MAPPING as OBM "
-        + "JOIN BAG AS B ON OBM.ORDER_ID = :orderId and OBM.BAG_ID = B.ID )"
-        + "LIMIT 1", nativeQuery = true)
+        + "JOIN BAG AS B ON OBM.ORDER_ID = :orderId and OBM.BAG_ID = B.ID ) "
+        + "LIMIT 1 ", nativeQuery = true)
     Location findByOrderId(@Param("orderId") Long id);
 }
