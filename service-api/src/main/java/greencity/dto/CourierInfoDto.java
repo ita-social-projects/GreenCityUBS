18591872152891e9ee929ed986d8ab920cec28d9
Dima Sannytski <sbymix91@gmail.com>
2021-11-25 19:12:35 +0200

    #3555

---
+++ service-api/src/main/java/greencity/dto/CourierInfoDto.java
@@ -0,0 +1,21 @@
+package greencity.dto;
+
+import greencity.entity.enums.CourierLimit;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+/**
+ * {@inheritDoc}
+ */
+public class CourierInfoDto {
+    Long minAmountOfBigBags;
+    Long maxAmountOfBigBags;
+    Long minPriceOfOrder;
+    Long maxPriceOfOrder;
+    CourierLimit courierLimit;
+}
