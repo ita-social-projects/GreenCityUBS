57577b5c5b0890badeb91ef5a6c575f8185e9430
RomanSulymka <44112418+RomanSulymka@users.noreply.github.com>
2021-12-07 15:03:54 +0000

    Deploying to gh-pages from  @ 19b4e175d362f8a9e4d9870d32c0df2c72541874 🚀

--- dao/src/main/java/greencity/repository/ViolationRepository.java
+++ dao/src/main/java/greencity/repository/ViolationRepository.java
@@ -1,8 +1,11 @@
 package greencity.repository;
 
 import greencity.entity.user.Violation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ViolationRepository extends CrudRepository<Violation, Long> {
@@ -13,4 +16,38 @@ public interface ViolationRepository extends CrudRepository<Violation, Long> {
      * @return optional of {@link Violation} .
      */
     Optional<Violation> findByOrderId(Long orderId);
+
+    /**
+     * Method gets all violations for user by id.
+     *
+     * @author Roman Sulymka
+     */
+    @Query(value = "select * from violations_description_mapping"
+        + "    INNER JOIN orders o on o.id = violations_description_mapping.order_id"
+        + "    INNER JOIN users on o.users_id = users.id"
+        + "    WHERE users.id = :userId", nativeQuery = true)
+    List<Violation> getAllViolationsByUserId(@Param(value = "userId") Long userId);
+
+    /**
+     * Method gets orderId for user by violationId.
+     *
+     * @author Roman Sulymka
+     */
+    @Query(value = "select order_id from violations_description_mapping"
+        + " INNER JOIN orders o on o.id = violations_description_mapping.order_id"
+        + " INNER JOIN users on o.users_id = users.id "
+        + " WHERE violations_description_mapping.id = :violationId", nativeQuery = true)
+    Long getOrderIdByViolationId(@Param(value = "violationId") Long violationId);
+
+    /**
+     * Method gets number of violations for user by userId.
+     *
+     * @author Roman Sulymka
+     */
+    @Query(value = "select count(violations_description_mapping.id)"
+        + " from violations_description_mapping"
+        + " INNER JOIN orders o on o.id = violations_description_mapping.order_id"
+        + " INNER JOIN users on o.users_id = users.id "
+        + " WHERE users_id = :userId", nativeQuery = true)
+    Long getNumberOfViolationsByUser(@Param(value = "userId") Long userId);
 }
