c15d6b0958dd1aeb230ddc4ef67a2e94870c2a58
DimaSannytski <33062320+DimaSannytski@users.noreply.github.com>
2021-12-09 18:02:47 +0000

    Deploying to gh-pages from  @ 19974d1d57faff55697e1f9357c51bdb4bf82e8e 🚀

--- dao/src/main/java/greencity/filters/EmployeePage.java
+++ dao/src/main/java/greencity/filters/EmployeePage.java
@@ -12,6 +12,6 @@ import org.springframework.data.domain.Sort;
 public class EmployeePage {
     private int pageNumber = 0;
     private int pageSize = 10;
-    private Sort.Direction sortDirection = Sort.Direction.DESC;
+    private Sort.Direction sortDirection = Sort.Direction.ASC;
     private String sortBy = "firstName";
 }
