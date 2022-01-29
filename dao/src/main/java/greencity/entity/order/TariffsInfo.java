25c3e6deb40fce869d872dd7f1b8c9987c4dc4f4
Bohdan Melnyk <bohdanmel@i.ua>
2022-01-29 21:48:44 +0200

    #[3799] second commit

--- dao/src/main/java/greencity/entity/order/TariffsInfo.java
+++ dao/src/main/java/greencity/entity/order/TariffsInfo.java
@@ -1,6 +1,8 @@
 package greencity.entity.order;
 
+import greencity.entity.enums.LocationStatus;
 import greencity.entity.user.Region;
+import greencity.entity.user.employee.ReceivingStation;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -8,11 +10,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.util.List;
 
@@ -34,4 +40,11 @@ public class TariffsInfo {
 
     @OneToMany
     List<Region> regions;
+
+    @OneToOne
+    ReceivingStation receivingStation;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    LocationStatus locationStatus;
 }
