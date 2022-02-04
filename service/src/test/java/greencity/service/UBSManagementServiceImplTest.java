7e520dd44fe413259c5466ac81bf1c4df2aff04d
Roman Matsuiev <romanmatsuiev@gmail.com>
2022-02-04 21:47:24 +0200

    Removed import

--- service/src/test/java/greencity/service/UBSManagementServiceImplTest.java
+++ service/src/test/java/greencity/service/UBSManagementServiceImplTest.java
@@ -30,7 +30,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
