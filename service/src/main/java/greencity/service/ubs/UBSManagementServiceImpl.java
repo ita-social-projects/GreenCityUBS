e74ebe9c84f1722bb5af65c19ddd1b4a0d0c7726
Andrii-Yezenitskyi <98427395+Andrii-Yezenitskyi@users.noreply.github.com>
2022-02-08 12:03:19 +0000

    Deploying to gh-pages from  @ f8fda4efa7b433dd62b312f8ecc29de85cbf8291 🚀

--- service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
@@ -571,8 +571,8 @@ public class UBSManagementServiceImpl implements UBSManagementService {
             for (Map.Entry<Integer, Integer> entry : exported.entrySet()) {
                 if (Boolean.TRUE.equals(!updateOrderRepository.ifRecordExist(orderId,
                     entry.getKey().longValue()))) {
-                    updateOrderRepository.insertNewRecord(orderId,
-                        entry.getKey().longValue());
+                    updateOrderRepository.insertNewRecord(orderId, entry.getKey().longValue());
+                    updateOrderRepository.updateAmount(0, orderId, entry.getKey().longValue());
                 }
                 updateOrderRepository
                     .updateExporter(entry.getValue(), orderId,
@@ -583,8 +583,8 @@ public class UBSManagementServiceImpl implements UBSManagementService {
             for (Map.Entry<Integer, Integer> entry : confirmed.entrySet()) {
                 if (Boolean.TRUE.equals(!updateOrderRepository.ifRecordExist(orderId,
                     entry.getKey().longValue()))) {
-                    updateOrderRepository.insertNewRecord(orderId,
-                        entry.getKey().longValue());
+                    updateOrderRepository.insertNewRecord(orderId, entry.getKey().longValue());
+                    updateOrderRepository.updateAmount(0, orderId, entry.getKey().longValue());
                 }
                 updateOrderRepository
                     .updateConfirm(entry.getValue(), orderId,
@@ -625,7 +625,7 @@ public class UBSManagementServiceImpl implements UBSManagementService {
                 Optional<Long> confirmWasteWas = Optional.empty();
                 if (Boolean.TRUE.equals(updateOrderRepository.ifRecordExist(orderId, entry.getKey().longValue()))) {
                     confirmWasteWas =
-                        Optional.of(updateOrderRepository.getConfirmWaste(orderId, entry.getKey().longValue()));
+                        Optional.ofNullable(updateOrderRepository.getConfirmWaste(orderId, entry.getKey().longValue()));
                 }
                 if (entry.getValue().longValue() != confirmWasteWas.orElse(0L)) {
                     if (countOfChanges == 0) {
@@ -651,7 +651,8 @@ public class UBSManagementServiceImpl implements UBSManagementService {
                 Optional<Long> exporterWasteWas = Optional.empty();
                 if (Boolean.TRUE.equals(updateOrderRepository.ifRecordExist(orderId, entry.getKey().longValue()))) {
                     exporterWasteWas =
-                        Optional.of(updateOrderRepository.getExporterWaste(orderId, entry.getKey().longValue()));
+                        Optional
+                            .ofNullable(updateOrderRepository.getExporterWaste(orderId, entry.getKey().longValue()));
                 }
                 if (entry.getValue().longValue() != exporterWasteWas.orElse(0L)) {
                     if (countOfChanges == 0) {
@@ -689,7 +690,7 @@ public class UBSManagementServiceImpl implements UBSManagementService {
         Order order = orderRepository.getOrderDetails(id)
             .orElseThrow(() -> new UnexistingOrderException(ORDER_WITH_CURRENT_ID_DOES_NOT_EXIST + id));
         List<Bag> bag = bagRepository.findBagByOrderId(id);
-        List<Certificate> currentCertificate = certificateRepository.findCertificate(id);
+        final List<Certificate> currentCertificate = certificateRepository.findCertificate(id);
 
         double sumAmount = 0;
         double sumConfirmed = 0;
@@ -698,19 +699,30 @@ public class UBSManagementServiceImpl implements UBSManagementService {
         double totalSumConfirmed;
         double totalSumExported;
 
-        List<Integer> amountValues = new ArrayList<>(order.getAmountOfBagsOrdered().values());
-
-        List<Integer> confirmedValues = new ArrayList<>(order.getConfirmedQuantity().values());
-
-        List<Integer> exportedValues = new ArrayList<>(order.getExportedQuantity().values());
-
-        for (int i = 0; i < bag.size(); i++) {
-            sumAmount += amountValues.get(i) * bag.get(i).getFullPrice();
-            if (!confirmedValues.isEmpty()) {
-                sumConfirmed += confirmedValues.get(i) * bag.get(i).getFullPrice();
+        if (!bag.isEmpty()) {
+            for (Map.Entry<Integer, Integer> entry : order.getAmountOfBagsOrdered().entrySet()) {
+                sumAmount += entry.getValue() * bag
+                    .stream()
+                    .filter(b -> b.getId().equals(entry.getKey()))
+                    .findFirst()
+                    .orElseThrow(() -> new BagNotFoundException(BAG_NOT_FOUND + entry.getKey()))
+                    .getFullPrice();
             }
-            if (!exportedValues.isEmpty()) {
-                sumExported += exportedValues.get(i) * bag.get(i).getFullPrice();
+            for (Map.Entry<Integer, Integer> entry : order.getConfirmedQuantity().entrySet()) {
+                sumConfirmed += entry.getValue() * bag
+                    .stream()
+                    .filter(b -> b.getId().equals(entry.getKey()))
+                    .findFirst()
+                    .orElseThrow(() -> new BagNotFoundException(BAG_NOT_FOUND + entry.getKey()))
+                    .getFullPrice();
+            }
+            for (Map.Entry<Integer, Integer> entry : order.getExportedQuantity().entrySet()) {
+                sumExported += entry.getValue() * bag
+                    .stream()
+                    .filter(b -> b.getId().equals(entry.getKey()))
+                    .findFirst()
+                    .orElseThrow(() -> new BagNotFoundException(BAG_NOT_FOUND + entry.getKey()))
+                    .getFullPrice();
             }
         }
 
@@ -736,10 +748,10 @@ public class UBSManagementServiceImpl implements UBSManagementService {
             totalSumConfirmed = sumConfirmed - order.getPointsToUse();
             totalSumExported = sumExported - order.getPointsToUse();
         }
-        if (confirmedValues.isEmpty()) {
+        if (order.getConfirmedQuantity().isEmpty()) {
             totalSumConfirmed = 0;
         }
-        if (exportedValues.isEmpty()) {
+        if (order.getExportedQuantity().isEmpty()) {
             totalSumExported = 0;
         }
         dto.setTotalAmount(
