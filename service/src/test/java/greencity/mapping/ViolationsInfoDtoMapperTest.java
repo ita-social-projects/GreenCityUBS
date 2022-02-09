e6cc13769338b6eac9171401415b213136acf56f
Max Boiarchuk <50172465+Zingerd@users.noreply.github.com>
2022-02-09 13:57:31 +0200

    created new unit tests for mappers (#675)
    * created new unit tests for mappers
    
    * added formatter

---
+++ service/src/test/java/greencity/mapping/ViolationsInfoDtoMapperTest.java
@@ -0,0 +1,26 @@
+package greencity.mapping;
+
+import greencity.dto.ViolationsInfoDto;
+
+import greencity.entity.user.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class ViolationsInfoDtoMapperTest {
+
+    @InjectMocks
+    private ViolationsInfoDtoMapper mapper;
+
+    @Test
+    void convert() {
+        ViolationsInfoDto violationsInfoDto = ViolationsInfoDto.builder().violationsAmount(1).build();
+        User user = User.builder().violations(1).build();
+        assertEquals(violationsInfoDto.getViolationsAmount(), mapper.convert(user).getViolationsAmount());
+
+    }
+}
