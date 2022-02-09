725f5fcfea1d88c9141b99a6e597ddc4dec7be86
Zingerd <50172465+Zingerd@users.noreply.github.com>
2022-02-09 12:01:53 +0000

    Deploying to gh-pages from  @ e6cc13769338b6eac9171401415b213136acf56f 🚀

---
+++ service/src/test/java/greencity/mapping/ReceivingStationMapperTest.java
@@ -0,0 +1,27 @@
+package greencity.mapping;
+
+import greencity.ModelUtils;
+import greencity.dto.ReceivingStationDto;
+import greencity.entity.user.employee.ReceivingStation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class ReceivingStationMapperTest {
+
+    @InjectMocks
+    ReceivingStationMapper mapper;
+
+    @Test
+    void convert() {
+        ReceivingStation expected = ModelUtils.getReceivingStation();
+        ReceivingStationDto receivingStationDto = ModelUtils.getReceivingStationDto();
+
+        assertEquals(expected, mapper.convert(receivingStationDto));
+    }
+
+}
