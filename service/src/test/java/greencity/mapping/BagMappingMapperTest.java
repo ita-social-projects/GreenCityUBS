c596385f7f8c49842ca578ca7a5a6d164c9269d0
yourromchikkkkk <92727544+yourromchikkkkk@users.noreply.github.com>
2022-02-13 10:56:53 +0000

    Deploying to gh-pages from  @ 7eeeb09450f9d646c06f5e248b0cecc5325e3804 🚀

---
+++ service/src/test/java/greencity/mapping/BagMappingMapperTest.java
@@ -0,0 +1,55 @@
+package greencity.mapping;
+
+import greencity.ModelUtils;
+import greencity.dto.BagMappingDto;
+import greencity.entity.order.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+class BagMappingMapperTest {
+    @InjectMocks
+    BagMappingMapper bagMappingMapper;
+
+    @Test
+    void convertReturnsEmptyList() {
+        Order order = ModelUtils.getOrder();
+        order.setAmountOfBagsOrdered(new HashMap<>());
+        order.setExportedQuantity(new HashMap<>());
+        order.setConfirmedQuantity(new HashMap<>());
+        List<BagMappingDto> list = bagMappingMapper.convert(order);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    void convert() {
+        Order order = ModelUtils.getOrder();
+
+        Map<Integer, Integer> amountOfBagsOrdered = new HashMap<>();
+        amountOfBagsOrdered.put(1, 1);
+        order.setAmountOfBagsOrdered(amountOfBagsOrdered);
+
+        Map<Integer, Integer> exportedQuantity = new HashMap<>();
+        exportedQuantity.put(1, 1);
+        order.setExportedQuantity(exportedQuantity);
+
+        Map<Integer, Integer> confirmedQuantity = new HashMap<>();
+        confirmedQuantity.put(1, 1);
+        order.setConfirmedQuantity(confirmedQuantity);
+
+        List<BagMappingDto> list = bagMappingMapper.convert(order);
+        assertEquals(1, list.size());
+        assertEquals(1, list.get(0).getAmount());
+        assertEquals(1, list.get(0).getConfirmed());
+        assertEquals(1, list.get(0).getExported());
+    }
+
+}
