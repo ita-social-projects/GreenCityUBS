e0c00edadac6ebb0d2b8385a8bfa0b5bf0ad62a9
VadymMakitra <hmarax3@gmail.com>
2022-05-20 01:37:10 +0300

    Removed code smell's

--- service-api/src/main/java/greencity/dto/CreateAddressRequestDto.java
+++ service-api/src/main/java/greencity/dto/CreateAddressRequestDto.java
@@ -1,10 +1,15 @@
 package greencity.dto;
 
-import lombok.*;
-
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
@@ -14,27 +19,27 @@ import javax.validation.constraints.Pattern;
 @Builder
 public class CreateAddressRequestDto {
     // CHECKSTYLE:OFF
-    private final String VALIDATION_MESSAGE = "use only English,or Ukrainian latter";
-    private final String NOT_EMPTY_MESSAGE = "name must not be empty";
-    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ,0-9']*", message = VALIDATION_MESSAGE)
-    @NotEmpty(message = NOT_EMPTY_MESSAGE)
+    private final static String validationMessage = "use only English,or Ukrainian latter";
+    private final static String notEmptyValidationMessage = "name must not be empty";
+    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ,0-9']*", message = validationMessage)
+    @NotEmpty(message = notEmptyValidationMessage)
     private String searchAddress;
-    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ']*", message = VALIDATION_MESSAGE)
-    @NotEmpty(message = NOT_EMPTY_MESSAGE)
+    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ']*", message = validationMessage)
+    @NotEmpty(message = notEmptyValidationMessage)
     private String districtEn;
-    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ']*", message = VALIDATION_MESSAGE)
-    @NotEmpty(message = NOT_EMPTY_MESSAGE)
+    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ']*", message = validationMessage)
+    @NotEmpty(message = notEmptyValidationMessage)
     private String district;
-    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ']*", message = VALIDATION_MESSAGE)
+    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ']*", message = validationMessage)
     @NotEmpty(message = "name must not be empty")
     private String regionEn;
-    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ']*", message = VALIDATION_MESSAGE)
-    @NotEmpty(message = NOT_EMPTY_MESSAGE)
+    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ ']*", message = validationMessage)
+    @NotEmpty(message = notEmptyValidationMessage)
     private String region;
     @Min(1)
     private String houseNumber;
     private String entranceNumber;
     private String houseCorpus;
-    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ 0-9']*", message = VALIDATION_MESSAGE)
+    @Pattern(regexp = "[A-Za-zА-Яа-яЇїІіЄєҐґ 0-9']*", message = validationMessage)
     private String addressComment;
 }
