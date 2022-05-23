9e4f7a55f792f91c586d925a3fa6d07b02eeac9c
VadymMakitra <hmarax3@gmail.com>
2022-05-23 21:20:47 +0300

    Added fall back strategy for registration.

--- core/src/main/java/greencity/validators/LanguageValidator.java
+++ core/src/main/java/greencity/validators/LanguageValidator.java
@@ -17,10 +17,10 @@ public class LanguageValidator implements ConstraintValidator<ValidLanguage, Loc
 
     @Override
     public void initialize(ValidLanguage constraintAnnotation) {
-        try{
+        try {
             codes = languageService.findAllLanguageCodes();
         } catch (Exception e) {
-            codes = List.of("en","ua");
+            codes = List.of("en", "ua");
         }
     }
 
