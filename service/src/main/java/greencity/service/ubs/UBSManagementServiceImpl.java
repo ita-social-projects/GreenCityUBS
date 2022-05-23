4c574c414c6104b2f2e2df80b4352c34a27eb74d
VadymMakitra <84317209+VadymMakitra@users.noreply.github.com>
2022-05-23 19:09:36 +0000

    Deploying to gh-pages from  @ 1b249ac9342e22f398d711dea3ef980d113738a8 🚀

--- service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
@@ -361,7 +361,7 @@ public class UBSManagementServiceImpl implements UBSManagementService {
             .userInfoDto(userInfoDto)
             .addressExportDetailsDto(addressDtoForAdminPage)
             .addressComment(address.getAddressComment()).bags(bagInfo)
-            .orderFullPrice(setTotalPrice(prices))
+            .orderFullPrice(prices.getSumAmount())
             .orderDiscountedPrice(getPaymentInfo(orderId, prices.getSumAmount().longValue()).getUnPaidAmount())
             .orderBonusDiscount(prices.getBonus()).orderCertificateTotalDiscount(prices.getCertificateBonus())
             .orderExportedPrice(prices.getSumExported()).orderExportedDiscountedPrice(prices.getTotalSumExported())
@@ -377,16 +377,6 @@ public class UBSManagementServiceImpl implements UBSManagementService {
             .courierPricePerPackage(fullPrice)
             .courierInfo(modelMapper.map(order.getTariffsInfo(), CourierInfoDto.class))
             .build();
-    }
-
-    private Double setTotalPrice(CounterOrderDetailsDto dto) {
-        if (dto.getTotalExported() != 0) {
-            return dto.getTotalExported();
-        }
-        if (dto.getTotalConfirmed() != 0) {
-            return dto.getTotalConfirmed();
-        }
-        return dto.getSumAmount();
     }
 
     /**
