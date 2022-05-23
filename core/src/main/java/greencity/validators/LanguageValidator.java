06fca60eb8aeeadc0ca65f854f2e9bed1fa23bd2
VadymMakitra <hmarax3@gmail.com>
2022-05-23 21:27:58 +0300

    Added logging.

--- core/src/main/java/greencity/validators/LanguageValidator.java
+++ core/src/main/java/greencity/validators/LanguageValidator.java
@@ -2,6 +2,9 @@ package greencity.validators;
 
 import greencity.annotations.ValidLanguage;
 import greencity.service.language.LanguageService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
@@ -11,6 +14,7 @@ import java.util.Locale;
 
 public class LanguageValidator implements ConstraintValidator<ValidLanguage, Locale> {
     private List<String> codes;
+    Logger logger = LoggerFactory.getLogger(LanguageValidator.class);
 
     @Autowired
     private LanguageService languageService;
@@ -21,6 +25,7 @@ public class LanguageValidator implements ConstraintValidator<ValidLanguage, Loc
             codes = languageService.findAllLanguageCodes();
         } catch (Exception e) {
             codes = List.of("en", "ua");
+            logger.warn("Occurred error during processing request: {}", e.getMessage());
         }
     }
 
