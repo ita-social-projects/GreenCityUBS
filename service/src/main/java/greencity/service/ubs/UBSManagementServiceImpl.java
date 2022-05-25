31714e4abc0b71da24d64dcab2c157a64b972bfa
MaksymKorolkov <max12xit02@gmail.com>
2022-05-25 17:17:57 +0300

    fixed bug

--- service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSManagementServiceImpl.java
@@ -1258,8 +1258,10 @@ public class UBSManagementServiceImpl implements UBSManagementService {
 
         if (paymentsForCurrentOrder > 0 && totalAmount > paymentsForCurrentOrder) {
             order.setOrderPaymentStatus(OrderPaymentStatus.HALF_PAID);
+            notificationService.notifyHalfPaidPackage(order);
         } else if (paymentsForCurrentOrder > 0 && totalAmount >= paymentsForCurrentOrder) {
             order.setOrderPaymentStatus(OrderPaymentStatus.PAID);
+            notificationService.notifyPaidOrder(order);
         } else if (paymentsForCurrentOrder == 0) {
             order.setOrderPaymentStatus(OrderPaymentStatus.UNPAID);
         }
