bf7997310fa1b88092f4e7d6487c270290175c2a
NataliaHlazova <nataskaglazova@gmail.com>
2022-05-26 17:36:11 +0300

    added alternate_email for ubs order

--- service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
@@ -317,7 +317,7 @@ public class UBSClientServiceImpl implements UBSClientService {
         PersonalDataDto dto = modelMapper.map(currentUser, PersonalDataDto.class);
         dto.setUbsUserId(ubsUser.get(0).getId());
         if (currentUser.getAlternateEmail() != null
-            && currentUser.getAlternateEmail() != "") {
+            && !currentUser.getAlternateEmail().equals("")) {
             dto.setEmail(currentUser.getAlternateEmail());
         }
         return dto;
