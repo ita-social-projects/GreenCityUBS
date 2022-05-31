99c1c5dfc224b4bf25ee954458c7ca34c6c73e68
SomeOne35719 <someone35719@gmail.com>
2022-05-31 14:10:32 +0300

    basic impl1

---
+++ service-api/src/main/java/greencity/dto/tariff/DeactivateTariffsLocationsDto.java
@@ -0,0 +1,21 @@
+package greencity.dto.tariff;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeactivateTariffsLocationsDto {
+    @NotNull
+    @NotEmpty
+    private List<@Min(1)Long> locationIds;
+}
