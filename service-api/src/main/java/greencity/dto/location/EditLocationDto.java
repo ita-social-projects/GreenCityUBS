61b95708888bc863f0577310abfb4ce768660074
SomeOne35719 <someone35719@gmail.com>
2022-06-06 16:31:57 +0300

    formatter2

--- service-api/src/main/java/greencity/dto/location/EditLocationDto.java
+++ service-api/src/main/java/greencity/dto/location/EditLocationDto.java
@@ -16,11 +16,11 @@ public class EditLocationDto {
     @NotNull
     @Min(1)
     private Long locationId;
-    @Pattern(regexp = "^([A-Z]{1}[a-zA-Z]{2,}(\\-| ){1}[A-Z]{1}[a-zA-Z]{1,})|([A-Z]{1}[a-z]{1,})",
+    @Pattern(regexp = "^([A-Z]{1}[a-zA-Z]{2,}[\\-| ]{1}[A-Z]{1}[a-zA-Z]{1,})|([A-Z]{1}[a-z]{1,})",
         message = "use only English letters")
     @NotEmpty(message = "en name must not be empty")
     private String nameEn;
-    @Pattern(regexp = "^([А-ЯЇІЄҐ]{1}[а-яіїєґ]{1,}[ -]?[А-ЯЇІЄҐ]{1}[а-яіїєґ]{1,})*$",
+    @Pattern(regexp = "^([А-ЯЇІЄҐ]{1}[а-яіїєґ]{1,}[ -]?[А-ЯЇІЄҐ]{1}[а-яіїєґ]{1,})",
         message = "use only Ukrainian letters")
     @NotEmpty(message = "ua name must not be empty")
     private String nameUa;
