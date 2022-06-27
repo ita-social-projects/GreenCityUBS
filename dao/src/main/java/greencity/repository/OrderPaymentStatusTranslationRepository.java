0c819cccc71e21951fc6fef3192bb42078f2086f
Pavl0-Gural <pg6369509@gmail.com>
2022-06-27 17:44:30 +0300

    Clear native queries.

--- dao/src/main/java/greencity/repository/OrderPaymentStatusTranslationRepository.java
+++ dao/src/main/java/greencity/repository/OrderPaymentStatusTranslationRepository.java
@@ -2,6 +2,7 @@ package greencity.repository;
 
 import greencity.entity.order.OrderPaymentStatusTranslation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
