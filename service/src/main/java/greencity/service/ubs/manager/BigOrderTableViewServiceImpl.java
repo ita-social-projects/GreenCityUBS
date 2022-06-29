aea18fdf12e4051f1039a8135b39577036f0e7b1
NataliaHlazova <nataskaglazova@gmail.com>
2022-06-29 19:48:57 +0300

    1

--- service/src/main/java/greencity/service/ubs/manager/BigOrderTableViewServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/manager/BigOrderTableViewServiceImpl.java
@@ -7,13 +7,19 @@ import greencity.filters.OrderPage;
 import greencity.filters.OrderSearchCriteria;
 import greencity.repository.BigOrderTableRepository;
 import greencity.repository.CustomTableViewRepo;
+import greencity.repository.EmployeeRepository;
+import greencity.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
+
+import static greencity.constant.ErrorMessage.EMPLOYEE_NOT_FOUND;
 
 @Service
 @AllArgsConstructor
@@ -21,10 +27,16 @@ public class BigOrderTableViewServiceImpl implements BigOrderTableServiceView {
     private final BigOrderTableRepository bigOrderTableRepository;
     private final CustomTableViewRepo customTableViewRepo;
     private final ModelMapper modelMapper;
+    private final EmployeeRepository employeeRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Page<BigOrderTableDTO> getOrders(OrderPage orderPage, OrderSearchCriteria searchCriteria, String uuid) {
-        var orders = bigOrderTableRepository.findAll(orderPage, searchCriteria);
+        String email = userRepository.findByUuid(uuid).getRecipientEmail();
+        Long employeeId = employeeRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException(EMPLOYEE_NOT_FOUND)).getId();
+        List<Long> tariffsInfoIds = employeeRepository.findTariffsInfoForEmployee(employeeId);
+        var orders = bigOrderTableRepository.findAll(orderPage, searchCriteria, tariffsInfoIds);
         var orderList = new ArrayList<BigOrderTableDTO>();
         orders.forEach(o -> orderList.add(modelMapper.map(o, BigOrderTableDTO.class)));
         return new PageImpl<>(orderList, orders.getPageable(), orders.getTotalElements());
