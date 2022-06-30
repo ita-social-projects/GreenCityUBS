93ccce4d65e5aa85dcca1aee4e99573d8be9e51b
NataliaHlazova <nataskaglazova@gmail.com>
2022-06-30 16:45:57 +0300

    testing

--- core/src/test/java/greencity/repository/BigOrderTableRepositoryTest.java
+++ core/src/test/java/greencity/repository/BigOrderTableRepositoryTest.java
@@ -16,6 +16,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.*;
+
 @Sql(scripts = "/sqlFiles/bigOrderTableRepository/insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/sqlFiles/bigOrderTableRepository/delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @ExtendWith(SpringExtension.class)
@@ -27,16 +29,17 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
     @Test
     void get_All_Orders_Default_Page_ASC() {
         var orders = ModelUtils.getListBOTViewsStandardPageASC();
-
+        List<Long> tariffsInfoIds = new ArrayList<>();
         Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandardASC(),
-            getOrderSearchCriteria()).getContent());
+            getOrderSearchCriteria(), tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Default_Page_DESC() {
         var orders = ModelUtils.getListBOTViewsStandardPageDESC();
+        List<Long> tariffsInfoIds = new ArrayList<>();
         Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(),
-            getOrderSearchCriteria()).getContent());
+            getOrderSearchCriteria(), tariffsInfoIds).getContent());
     }
 
     @Test
@@ -44,22 +47,26 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var orders = ModelUtils.getListBOTViewsOSC_Sorting_By_Order_Payment_Status_DESC();
         var page = new OrderPage().setPageNumber(0).setPageSize(10).setSortBy("orderPaymentStatus")
             .setSortDirection(Sort.Direction.DESC);
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(page, getOrderSearchCriteria()).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(page, getOrderSearchCriteria(), tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Size_Two_Page_One_DESC() {
         var orders = ModelUtils.getListBOTViewsSizeTwoPageOneDESC();
+        List<Long> tariffsInfoIds = new ArrayList<>();
         Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageSizeTwoPageOneDESC(),
-            getOrderSearchCriteria()).getContent());
+            getOrderSearchCriteria(), tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Order_Status_Is_Formed_DESC() {
         var filter = new OrderSearchCriteria().setOrderStatus(new OrderStatus[] {OrderStatus.FORMED});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Order_Status_Is_Formed();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
@@ -67,8 +74,9 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var filter =
             new OrderSearchCriteria().setOrderStatus(new OrderStatus[] {OrderStatus.FORMED, OrderStatus.CONFIRMED});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Order_Status_Is_Formed_And_CONFIRMED();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
@@ -76,56 +84,63 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var filter =
             new OrderSearchCriteria().setOrderPaymentStatus(new OrderPaymentStatus[] {OrderPaymentStatus.PAID});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Payment_Status_Is_PAID();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Region_DESC() {
         var filter = new OrderSearchCriteria().setRegion(new String[] {"Київська область"});
         var orders = ModelUtils.getListBOTViewsStandardPageDESC();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_City_DESC() {
         var filter = new OrderSearchCriteria().setCity(new String[] {"Київ"});
         var orders = ModelUtils.getListBOTViewsStandardPageDESC();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Districts_DESC() {
         var filter = new OrderSearchCriteria().setDistricts(new String[] {"Печерський"});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_District();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Order_Date_Between_DESC() {
         var filter = new OrderSearchCriteria().setOrderDate(new DateFilter().setFrom("2022-02-01").setTo("2022-02-02"));
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Order_Date_Between();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Order_Date_Less_Then_Or_Equal() {
         var filter = new OrderSearchCriteria().setOrderDate(new DateFilter().setTo("2022-02-01"));
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Order_Date_Less_Then_Or_Equal();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Order_Date_Greater_Then_Or_Equal() {
         var filter = new OrderSearchCriteria().setOrderDate(new DateFilter().setFrom("2022-02-01"));
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Order_Date_Greater_Then_Or_Equal();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
@@ -133,8 +148,9 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var filter =
             new OrderSearchCriteria().setDeliveryDate(new DateFilter().setFrom("2022-02-03").setTo("2022-02-04"));
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Date_Of_Export_Between();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
@@ -142,64 +158,72 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var filter =
             new OrderSearchCriteria().setPaymentDate(new DateFilter().setFrom("2022-02-02").setTo("2022-02-02"));
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Payment_Date_Between();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Receiving_Station() {
         var filter = new OrderSearchCriteria().setReceivingStation(new Long[] {1L});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Receving_station();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Responsible_Caller() {
         var filter = new OrderSearchCriteria().setResponsibleCallerId(new Long[] {15L});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Responsible_Caller();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Responsible_Logic_Man() {
         var filter = new OrderSearchCriteria().setResponsibleLogicManId(new Long[] {10L});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Logic_Man();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Responsible_Driver() {
         var filter = new OrderSearchCriteria().setResponsibleDriverId(new Long[] {10L});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Responsible_Driver();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Filter_By_Responsible_Navigator() {
         var filter = new OrderSearchCriteria().setResponsibleNavigatorId(new Long[] {10L});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_By_Responsible_Navigator();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Search_by_phone() {
         var filter = new OrderSearchCriteria().setSearch(new String[] {"+380676666666"});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_Search();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_Search_by_Client_Name() {
         var filter = new OrderSearchCriteria().setSearch(new String[] {"Myroslav", "Vir"});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_Search_by_Client_name();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
@@ -208,8 +232,9 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
             .setOrderPaymentStatus(new OrderPaymentStatus[] {OrderPaymentStatus.PAID})
             .setOrderStatus(new OrderStatus[] {OrderStatus.FORMED});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_Combination_DESC();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandard(), filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandard(), filter, tariffsInfoIds).getContent());
     }
 
     @Test
@@ -219,16 +244,18 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
             .setOrderPaymentStatus(new OrderPaymentStatus[] {OrderPaymentStatus.PAID})
             .setOrderStatus(new OrderStatus[] {OrderStatus.FORMED});
         var orders = ModelUtils.getListBOTViewsOSC_Filter_Combination_ASC();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(orderPageASC, filter).getContent());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(orderPageASC, filter, tariffsInfoIds).getContent());
     }
 
     @Test
     void get_All_Orders_PageImpl_ASC() {
         var filter = new OrderSearchCriteria();
         var orders = ModelUtils.getPageableAllBOTViews_ASC();
-
-        Assertions.assertEquals(orders, bigOrderTableRepository.findAll(getOrderPageStandardASC(), filter));
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders,
+            bigOrderTableRepository.findAll(getOrderPageStandardASC(), filter, tariffsInfoIds));
     }
 
     @Test
@@ -236,9 +263,9 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var filter = new OrderSearchCriteria();
         var page = getOrderPage_Two_Element_On_Page_ASC();
         var orders = ModelUtils.getPageableAllBOTViews_Two_Element_On_Page_ASC();
-
+        List<Long> tariffsInfoIds = new ArrayList<>();
         Assertions.assertEquals(orders.getTotalElements(),
-            bigOrderTableRepository.findAll(page, filter).getTotalElements());
+            bigOrderTableRepository.findAll(page, filter, tariffsInfoIds).getTotalElements());
     }
 
     @Test
@@ -246,8 +273,9 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var filter = new OrderSearchCriteria();
         var page = getOrderPage_Two_Element_On_Page_ASC();
         var orders = ModelUtils.getPageableAllBOTViews_Two_Element_On_Page_ASC();
-
-        Assertions.assertEquals(orders.getSize(), bigOrderTableRepository.findAll(page, filter).getSize());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders.getSize(),
+            bigOrderTableRepository.findAll(page, filter, tariffsInfoIds).getSize());
     }
 
     @Test
@@ -255,8 +283,9 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var filter = new OrderSearchCriteria();
         var page = getOrderPage_Two_Element_On_Page_ASC();
         var orders = ModelUtils.getPageableAllBOTViews_Two_Element_On_Page_ASC();
-
-        Assertions.assertEquals(orders.getNumber(), bigOrderTableRepository.findAll(page, filter).getNumber());
+        List<Long> tariffsInfoIds = new ArrayList<>();
+        Assertions.assertEquals(orders.getNumber(),
+            bigOrderTableRepository.findAll(page, filter, tariffsInfoIds).getNumber());
     }
 
     @Test
@@ -264,9 +293,9 @@ class BigOrderTableRepositoryTest extends IntegrationTestBase {
         var filter = new OrderSearchCriteria();
         var page = getOrderPage_Two_Element_On_Page_ASC();
         var orders = ModelUtils.getPageableAllBOTViews_Two_Element_On_Page_ASC();
-
+        List<Long> tariffsInfoIds = new ArrayList<>();
         Assertions.assertEquals(orders.getNumberOfElements(),
-            bigOrderTableRepository.findAll(page, filter).getNumberOfElements());
+            bigOrderTableRepository.findAll(page, filter, tariffsInfoIds).getNumberOfElements());
     }
 
     private OrderPage getOrderPageSizeTwoPageOneDESC() {
