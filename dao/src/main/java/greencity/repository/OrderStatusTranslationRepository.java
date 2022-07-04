944819bd1d75377ba3a679012a25b50c9858d984
Pavl0-Gural <pg6369509@gmail.com>
2022-07-04 13:42:58 +0300

    Fix code smells

--- dao/src/main/java/greencity/repository/OrderStatusTranslationRepository.java
+++ dao/src/main/java/greencity/repository/OrderStatusTranslationRepository.java
@@ -3,7 +3,6 @@ package greencity.repository;
 import greencity.entity.order.OrderStatusTranslation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import java.util.List;
 import java.util.Optional;
 
