7f1e53edb21c93ebacd61177f0382fa067303310
Andrii Yezenitskyi <andrii.yezenitskyi@gmail.com>
2022-07-14 16:07:35 +0300

    Extracted regexps

--- service-api/src/main/java/greencity/dto/location/EditLocationDto.java
+++ service-api/src/main/java/greencity/dto/location/EditLocationDto.java
@@ -1,5 +1,7 @@
 package greencity.dto.location;
 
+import greencity.constant.ErrorMessage;
+import greencity.constant.ValidationConstant;
 import lombok.*;
 
 import javax.validation.constraints.*;
@@ -13,12 +15,10 @@ public class EditLocationDto {
     @NotNull
     @Min(1)
     private Long locationId;
-    @Pattern(regexp = "^([A-Z][a-z]{0,39}'?[a-z]{1,39}($|[ -](?=[A-Z]))){1,10}$",
-        message = "use only English letters")
-    @Size(min = 3, max = 40, message = "a minimum of 3 to a maximum of 40 characters are allowed")
+    @Pattern(regexp = ValidationConstant.CITY_EN_REGEXP, message = ErrorMessage.USE_ONLY_ENGLISH_LETTERS)
+    @Size(min = 3, max = 40, message = ErrorMessage.CITY_NAME_CHARACTER_LIMIT)
     private String nameEn;
-    @Pattern(regexp = "^([А-ЯЇІЄҐ][а-яіїєґ]{0,39}'?[а-яіїєґ]{1,39}($|[ -](?=[А-ЯЇІЄҐ]))){1,10}$",
-        message = "use only Ukrainian letters")
-    @Size(min = 3, max = 40, message = "a minimum of 3 to a maximum of 40 characters are allowed")
+    @Pattern(regexp = ValidationConstant.CITY_UK_REGEXP, message = ErrorMessage.USE_ONLY_UKRAINIAN_LETTERS)
+    @Size(min = 3, max = 40, message = ErrorMessage.CITY_NAME_CHARACTER_LIMIT)
     private String nameUa;
 }
