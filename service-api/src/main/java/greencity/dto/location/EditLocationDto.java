2f0aad30c6c3aae27803053ebc7c338480582c20
Andrii Yezenitskyi <andrii.yezenitskyi@gmail.com>
2022-07-14 15:08:51 +0300

    Improved regex to check string length

--- service-api/src/main/java/greencity/dto/location/EditLocationDto.java
+++ service-api/src/main/java/greencity/dto/location/EditLocationDto.java
@@ -13,12 +13,10 @@ public class EditLocationDto {
     @NotNull
     @Min(1)
     private Long locationId;
-    @Pattern(regexp = "^([A-Z][a-z]{0,39}'?[a-z]{1,39}($|[ -](?=[A-Z]))){1,10}$",
-        message = "use only English letters")
-    @Size(min = 3, max = 40, message = "a minimum of 3 to a maximum of 40 characters are allowed")
+    @Pattern(regexp = "^(?=.{3,40}$)([A-Z][a-z]*'?[a-z]+($|[ -](?=[A-Z])))+$",
+        message = "use only English letters, a minimum of 3 to a maximum of 40 characters are allowed")
     private String nameEn;
-    @Pattern(regexp = "^([А-ЯЇІЄҐ][а-яіїєґ]{0,39}'?[а-яіїєґ]{1,39}($|[ -](?=[А-ЯЇІЄҐ]))){1,10}$",
-        message = "use only Ukrainian letters")
-    @Size(min = 3, max = 40, message = "a minimum of 3 to a maximum of 40 characters are allowed")
+    @Pattern(regexp = "^(?=.{3,40}$)([А-ЯЇІЄҐ][а-яіїєґ]*'?[а-яіїєґ]+($|[ -](?=[А-ЯЇІЄҐ])))+$",
+        message = "use only Ukrainian letters, a minimum of 3 to a maximum of 40 characters are allowed")
     private String nameUa;
 }
