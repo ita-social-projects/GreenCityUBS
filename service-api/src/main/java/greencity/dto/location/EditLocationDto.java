7035b034e13b1fed05b2f558261c4ca3ff30ece6
Andrii Yezenitskyi <andrii.yezenitskyi@gmail.com>
2022-07-14 15:32:35 +0300

    Format

--- service-api/src/main/java/greencity/dto/location/EditLocationDto.java
+++ service-api/src/main/java/greencity/dto/location/EditLocationDto.java
@@ -14,11 +14,11 @@ public class EditLocationDto {
     @Min(1)
     private Long locationId;
     @Pattern(regexp = "^([A-Z][a-z]{0,39}'?[a-z]{1,39}($|[ -](?=[A-Z]))){1,10}$",
-            message = "use only English letters")
+        message = "use only English letters")
     @Size(min = 3, max = 40, message = "a minimum of 3 to a maximum of 40 characters are allowed")
     private String nameEn;
     @Pattern(regexp = "^([А-ЯЇІЄҐ][а-яіїєґ]{0,39}'?[а-яіїєґ]{1,39}($|[ -](?=[А-ЯЇІЄҐ]))){1,10}$",
-            message = "use only Ukrainian letters")
+        message = "use only Ukrainian letters")
     @Size(min = 3, max = 40, message = "a minimum of 3 to a maximum of 40 characters are allowed")
     private String nameUa;
 }
