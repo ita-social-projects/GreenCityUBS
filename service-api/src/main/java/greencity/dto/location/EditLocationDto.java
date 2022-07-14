9eff25a51c98fbc345bc45ed69161a9973b8c8fc
Andrii Yezenitskyi <andrii.yezenitskyi@gmail.com>
2022-07-14 14:56:29 +0300

    Fixed ReDoS vulnerability

--- service-api/src/main/java/greencity/dto/location/EditLocationDto.java
+++ service-api/src/main/java/greencity/dto/location/EditLocationDto.java
@@ -13,11 +13,11 @@ public class EditLocationDto {
     @NotNull
     @Min(1)
     private Long locationId;
-    @Pattern(regexp = "^([A-Z][a-z]*'?[a-z]+($|[ -](?=[A-Z])))+$",
+    @Pattern(regexp = "^([A-Z][a-z]{0,39}'?[a-z]{1,39}($|[ -](?=[A-Z]))){1,10}$",
         message = "use only English letters")
     @Size(min = 3, max = 40, message = "a minimum of 3 to a maximum of 40 characters are allowed")
     private String nameEn;
-    @Pattern(regexp = "^([А-ЯЇІЄҐ][а-яіїєґ]*'?[а-яіїєґ]+($|[ -](?=[А-ЯЇІЄҐ])))+$",
+    @Pattern(regexp = "^([А-ЯЇІЄҐ][а-яіїєґ]{0,39}'?[а-яіїєґ]{1,39}($|[ -](?=[А-ЯЇІЄҐ]))){1,10}$",
         message = "use only Ukrainian letters")
     @Size(min = 3, max = 40, message = "a minimum of 3 to a maximum of 40 characters are allowed")
     private String nameUa;
