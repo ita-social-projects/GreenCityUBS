3988382e10b6fd42c96cc4d0c348ebda98aa9a05
NataliaHlazova <nataskaglazova@gmail.com>
2022-07-22 23:45:20 +0300

    fix

--- service-api/src/main/java/greencity/dto/address/AddressDto.java
+++ service-api/src/main/java/greencity/dto/address/AddressDto.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.io.Serializable;
+import static greencity.constant.ValidationConstant.*;
 
 @Getter
 @Setter
@@ -23,27 +24,27 @@ public class AddressDto implements Serializable {
     private Long id;
     @NotBlank
     @Length(max = 30)
-    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-я\\s-'.]{1,30}")
+    @Pattern(regexp = CITY_UK_REGEXP)
     private String city;
     @NotBlank
     @Length(max = 30)
-    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-я\\s-'.]{1,30}")
+    @Pattern(regexp = CH_UA + "{1,30}")
     private String district;
     @NotBlank
     @Length(max = 30)
-    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-я\\s-'.]{1,30}")
+    @Pattern(regexp = CH_UA + "{1,30}")
     private String region;
     @Length(max = 4)
-    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-яA-Za-z0-9\\s-'.]{1,4}")
+    @Pattern(regexp = CH_NUM + "{1,4}")
     private String entranceNumber;
     @Length(max = 5)
-    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-яA-Za-z0-9\\s-.]{1,5}")
+    @Pattern(regexp = CH_NUM + "{1,5}")
     private String houseCorpus;
     @Length(max = 5)
-    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-яA-Za-z0-9\\s-.]{1,5}")
+    @Pattern(regexp = CH_NUM + "{1,5}")
     private String houseNumber;
     @Length(max = 50)
-    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-я\\s-'.]{1,50}")
+    @Pattern(regexp = CH_UA + "{1,50}")
     private String street;
 
     private String addressComment;
@@ -53,17 +54,17 @@ public class AddressDto implements Serializable {
     private Boolean actual;
     @NotBlank
     @Length(max = 30)
-    @Pattern(regexp = "[A-Za-z\\s-'.]{1,30}")
+    @Pattern(regexp = CITY_EN_REGEXP)
     private String cityEn;
     @NotBlank
     @Length(max = 30)
-    @Pattern(regexp = "[A-Za-z\\s-'.]{1,30}")
+    @Pattern(regexp = CH_EN + "{1,30}")
     private String regionEn;
     @Length(max = 50)
-    @Pattern(regexp = "[A-Za-z\\s-'.]{1,50}")
+    @Pattern(regexp = CH_EN + "{1,50}")
     private String streetEn;
     @NotBlank
     @Length(max = 30)
-    @Pattern(regexp = "[A-Za-z\\s-'.]{1,30}")
+    @Pattern(regexp = CH_EN + "{1,30}")
     private String districtEn;
 }
