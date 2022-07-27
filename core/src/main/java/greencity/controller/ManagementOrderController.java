2b71a0b04f8778942f395f7146e9231c8305a661
innayashna <inna.miasoid@gmail.com>
2022-07-27 21:32:58 +0300

    Removed code smell

--- core/src/main/java/greencity/controller/ManagementOrderController.java
+++ core/src/main/java/greencity/controller/ManagementOrderController.java
@@ -35,7 +35,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
