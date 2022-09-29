96ade2af8d673c88c228f58a9daa3087875df188
OlegVat <73220885+OlegVat@users.noreply.github.com>
2022-09-29 12:10:21 +0300

    #4419 Fixed the maximum amount of order addresses for the user (#871)
    * #4419
    Added exception for maximum capacity of user orders addresses
    
    * #4419
    fixed code smell

--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -107,6 +107,7 @@ public final class ErrorMessage {
     public static final String USE_ONLY_UKRAINIAN_LETTERS = "use only Ukrainian letters";
     public static final String CITY_NAME_CHARACTER_LIMIT = "A minimum of 3 to a maximum of 40 characters are allowed";
     public static final String CANNOT_ACCESS_ORDER_FOR_EMPLOYEE = "Cannot access order with id: ";
+    public static final String NUMBER_OF_ADDRESSES_EXCEEDED = "Number of addresses reached maximum";
 
     /**
      * Constructor.
