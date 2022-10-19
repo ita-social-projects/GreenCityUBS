d48d7c35785208350f1655bc350ebd92c525cb81
innayashna <inna.miasoid@gmail.com>
2022-10-19 12:33:44 +0300

    Formatter changes

--- core/src/main/java/greencity/configuration/SecurityConfig.java
+++ core/src/main/java/greencity/configuration/SecurityConfig.java
@@ -133,7 +133,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 SUPER_ADMIN_LINK + "/**")
             .hasAnyRole(ADMIN, UBS_EMPLOYEE)
             .antMatchers(HttpMethod.PATCH,
-                    SUPER_ADMIN_LINK + "/setLimitDescription/{courierId}")
+                SUPER_ADMIN_LINK + "/setLimitDescription/{courierId}")
             .hasAnyRole(ADMIN, UBS_EMPLOYEE)
             .antMatchers(HttpMethod.PATCH,
                 UBS_MANAG_LINK + "/update-order-page-admin-info/{id}",
