44243bd5986eaf126daf0025dedff4b00742a5a0
OlegVat <73220885+OlegVat@users.noreply.github.com>
2022-11-01 15:08:22 +0200

    #4698 (#902)
    fixed saving tariffs with bagTranslation null variables

--- service-api/src/main/java/greencity/dto/tariff/TariffTranslationDto.java
+++ service-api/src/main/java/greencity/dto/tariff/TariffTranslationDto.java
@@ -2,6 +2,7 @@ package greencity.dto.tariff;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
@@ -12,9 +13,12 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class TariffTranslationDto {
     @NotNull
+    @NotEmpty
     private String name;
     @NotNull
+    @NotEmpty
     private String description;
     @NonNull
+    @NotEmpty
     private String nameEng;
 }
