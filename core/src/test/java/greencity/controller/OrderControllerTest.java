f2623fa83668b53a2c2c118b4c6abd2da68b20b9
OlegVat <73220885+OlegVat@users.noreply.github.com>
2022-11-07 11:45:43 +0000

    Deploying to gh-pages from  @ e7d4d1ba651c26fe3868237fb6b3e4f55d078bad 🚀

--- core/src/test/java/greencity/controller/OrderControllerTest.java
+++ core/src/test/java/greencity/controller/OrderControllerTest.java
@@ -77,14 +77,15 @@ class OrderControllerTest {
 
     @Test
     void getCurrentUserPoints() throws Exception {
+        Long locationId = 1L;
         when(userRemoteClient.findUuidByEmail((anyString()))).thenReturn("35467585763t4sfgchjfuyetf");
 
-        mockMvc.perform(get(ubsLink + "/order-details")
+        mockMvc.perform(get(ubsLink + "/order-details/1")
             .principal(principal))
             .andExpect(status().isOk());
 
         verify(userRemoteClient).findUuidByEmail("test@gmail.com");
-        verify(ubsClientService).getFirstPageData("35467585763t4sfgchjfuyetf");
+        verify(ubsClientService).getFirstPageData("35467585763t4sfgchjfuyetf", locationId);
     }
 
     @Test
