6212b08db28f6d712b5f6f52f5d7afa94ec03f2e
JJJazl <98067127+JJJazl@users.noreply.github.com>
2022-11-28 09:46:57 +0000

    Deploying to gh-pages from  @ d69c9140c9403180dfa01866a014353ec764e777 🚀

--- dao/src/main/java/greencity/repository/RegionRepository.java
+++ dao/src/main/java/greencity/repository/RegionRepository.java
@@ -2,8 +2,10 @@ package greencity.repository;
 
 import greencity.entity.user.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
@@ -18,4 +20,17 @@ public interface RegionRepository extends JpaRepository<Region, Long> {
      */
     Optional<Region> findRegionByEnNameAndUkrName(@Param("EnName") String nameEn,
         @Param("UkrName") String nameUk);
+
+    /**
+     * Method for get regions with only active locations.
+     * 
+     * @return List of {@link Region} if at least one region exists
+     * @author Safarov Renat
+     */
+    @Query(nativeQuery = true,
+        value = "select * from regions r "
+            + "join locations l on r.id = l.region_id "
+            + "where l.location_status = 'ACTIVE' "
+            + "group by r.id, l.id")
+    List<Region> findRegionsWithActiveLocations();
 }
