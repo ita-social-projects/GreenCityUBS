a34f34b410501dfdc315330c291e30d2abed1928
Jose Castellanos <jcast@softserveinc.com>
2022-12-22 16:37:53 -0600

    Format NotificationStep

--- dao/src/main/java/greencity/enums/NotificationStep.java
+++ dao/src/main/java/greencity/enums/NotificationStep.java
@@ -1,10 +1,8 @@
 package greencity.enums;
 
 public enum NotificationStep {
-    ATTEMPT_1, //after 3 days when order made
-
-    ATTEMPT_2, // first notification during first 7 days
-
-    ATTEMPT_3, // second notification during first 7 days (3 days after attempt_2)
+    ATTEMPT_1,
+    ATTEMPT_2,
+    ATTEMPT_3,
 
 }
