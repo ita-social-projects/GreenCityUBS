9db1da7f24d6a59677a201711f2e873e599fcb84
NikitaKorzh <nik.korzh.ita@gmail.com>
2023-01-01 14:38:34 +0200

    delete public from TariffsForLocationDtoMapperTest.java

--- service/src/test/java/greencity/mapping/tariff/TariffsForLocationDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/tariff/TariffsForLocationDtoMapperTest.java
@@ -12,7 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class TariffsForLocationDtoMapperTest {
+class TariffsForLocationDtoMapperTest {
 
     @InjectMocks
     TariffsForLocationDtoMapper mapper;
