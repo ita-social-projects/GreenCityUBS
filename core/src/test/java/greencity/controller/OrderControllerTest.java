207eff9b1984566217161b79223d9cd4b1d6e3f7
Jose Castellanos <jcast@softserveinc.com>
2023-01-17 09:05:01 -0600

    Reformat test case

--- core/src/test/java/greencity/controller/OrderControllerTest.java
+++ core/src/test/java/greencity/controller/OrderControllerTest.java
@@ -45,7 +45,6 @@ import static greencity.ModelUtils.getRedirectionConfig;
 import static greencity.ModelUtils.getUbsCustomersDto;
 import static greencity.ModelUtils.getUbsCustomersDtoUpdate;
 import static greencity.ModelUtils.getUserInfoDto;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
