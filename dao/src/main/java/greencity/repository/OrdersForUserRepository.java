3d2e91b4b19f9054abea9f28343d84364ff1bf37
Maksym Golik <maksym.golik@gmail.com>
2023-01-18 21:28:02 +0200

    Removed unused and not correctly worked methods from OrdersForUserRepository

--- dao/src/main/java/greencity/repository/OrdersForUserRepository.java
+++ dao/src/main/java/greencity/repository/OrdersForUserRepository.java
@@ -22,30 +22,6 @@ public interface OrdersForUserRepository extends PagingAndSortingRepository<Orde
     Page<Order> getAllOrdersByUserId(Pageable pageable, @Param(value = "userId") Long userId);
 
     /**
-     * Method gets all orders for user by id and sorting by amount column DESC.
-     *
-     * @author Roman Sulymka
-     */
-    @Query(value = "SELECT * FROM orders"
-        + "    INNER JOIN payment on orders.id = payment.order_id"
-        + "    INNER JOIN users ON orders.users_id = users.id"
-        + "    INNER JOIN ubs_user on orders.ubs_user_id = ubs_user.id"
-        + "    WHERE orders.users_id = :userId order by payment.amount desc", nativeQuery = true)
-    Page<Order> getAllOrdersByUserIdAndAmountDesc(Pageable pageable, @Param(value = "userId") Long userId);
-
-    /**
-     * Method gets all orders for user id by and sorting by amount column ASC.
-     *
-     * @author Roman Sulymka
-     */
-    @Query(value = "SELECT * FROM orders"
-        + "    INNER JOIN payment on orders.id = payment.order_id"
-        + "    INNER JOIN users ON orders.users_id = users.id"
-        + "    INNER JOIN ubs_user on orders.ubs_user_id = ubs_user.id"
-        + "    WHERE orders.users_id = :userId order by payment.amount asc", nativeQuery = true)
-    Page<Order> getAllOrdersByUserIdAndAmountASC(Pageable pageable, @Param(value = "userId") Long userId);
-
-    /**
      * Returns a page with orders for the specified user.
      *
      * @param pageable {@link Pageable} page to return.
