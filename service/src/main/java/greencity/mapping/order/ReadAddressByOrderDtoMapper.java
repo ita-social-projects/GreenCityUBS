0fb52895c3a3200c33da4cafa1f4b98bda012c92
JJJazl <98067127+JJJazl@users.noreply.github.com>
2023-01-23 16:00:02 +0000

    Deploying to gh-pages from  @ 4e0e231dd075bd04bf1d87d1f3bc1d0a9f2112b6 🚀

--- service/src/main/java/greencity/mapping/order/ReadAddressByOrderDtoMapper.java
+++ service/src/main/java/greencity/mapping/order/ReadAddressByOrderDtoMapper.java
@@ -2,6 +2,7 @@ package greencity.mapping.order;
 
 import greencity.dto.order.ReadAddressByOrderDto;
 import greencity.entity.user.ubs.Address;
+import greencity.entity.user.ubs.OrderAddress;
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -11,14 +12,14 @@ import org.springframework.stereotype.Component;
  * {@link ReadAddressByOrderDto}.
  */
 @Component
-public class ReadAddressByOrderDtoMapper extends AbstractConverter<Address, ReadAddressByOrderDto> {
+public class ReadAddressByOrderDtoMapper extends AbstractConverter<OrderAddress, ReadAddressByOrderDto> {
     /**
-     * Method convert {@link Address} to {@link ReadAddressByOrderDto}.
+     * Method convert {@link OrderAddress} to {@link ReadAddressByOrderDto}.
      *
      * @return {@link ReadAddressByOrderDto}
      */
     @Override
-    protected ReadAddressByOrderDto convert(Address address) {
+    protected ReadAddressByOrderDto convert(OrderAddress address) {
         return ReadAddressByOrderDto.builder()
             .district(address.getDistrict())
             .entranceNumber(address.getEntranceNumber())
