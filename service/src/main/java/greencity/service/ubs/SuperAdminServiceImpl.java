f60ee6bfa158bb6d1e53ed9d744988250be3e8ec
Julia Seti <jseti18@gmail.com>
2023-01-24 10:15:44 +0200

    imports are specified explicitly

--- service/src/main/java/greencity/service/ubs/SuperAdminServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/SuperAdminServiceImpl.java
@@ -65,7 +65,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @org.springframework.stereotype.Service
