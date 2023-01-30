293f0b8df9fe432928cd3ba6c365e0bbc3940a99
Maksym Golik <maksym.golik@gmail.com>
2023-01-30 11:48:39 +0200

    Added validation for ManualPaymentRequestDto

--- service-api/src/main/java/greencity/dto/payment/ManualPaymentRequestDto.java
+++ service-api/src/main/java/greencity/dto/payment/ManualPaymentRequestDto.java
@@ -2,14 +2,22 @@ package greencity.dto.payment;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ManualPaymentRequestDto {
+    @NotEmpty
     private String settlementdate;
+    @NotNull
+    @Positive
     private Long amount;
+    @NotEmpty
     private String paymentId;
     private String receiptLink;
     private String imagePath;
