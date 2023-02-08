3dc1c1f61c2a1588ea9a7318d9fd9338182c6c48
NikitaKorzh <nikitakorzh10@gmail.com>
2023-02-08 13:13:32 +0200

    add imports

--- dao/src/main/java/greencity/repository/TariffsInfoRepository.java
+++ dao/src/main/java/greencity/repository/TariffsInfoRepository.java
@@ -3,11 +3,14 @@ package greencity.repository;
 import greencity.entity.order.TariffsInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface TariffsInfoRepository extends JpaRepository<TariffsInfo, Long>, JpaSpecificationExecutor<TariffsInfo> {
