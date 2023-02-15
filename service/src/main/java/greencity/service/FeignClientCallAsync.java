593d8bb46f9a7bd3b4bed56ba617668c53955e59
NikitaKorzh <nikitakorzh10@gmail.com>
2023-02-15 23:30:44 +0200

    add async config

---
+++ service/src/main/java/greencity/service/FeignClientCallAsync.java
@@ -0,0 +1,26 @@
+package greencity.service;
+
+import greencity.client.UserRemoteClient;
+import greencity.dto.user.UserVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+public class FeignClientCallAsync {
+    private final UserRemoteClient userRemoteClient;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Async
+    public CompletableFuture<Optional<UserVO>> getRecordsAsync(String email) {
+        return CompletableFuture.completedFuture(userRemoteClient.findNotDeactivatedByEmail(email));
+    }
+}
