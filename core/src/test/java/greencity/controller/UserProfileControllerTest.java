7e68362303ccfe7fadd142fdae94d583cf59fba2
Maksym Golik <maksym.golik@gmail.com>
2023-02-23 12:48:23 +0200

    Optimized imports

--- core/src/test/java/greencity/controller/UserProfileControllerTest.java
+++ core/src/test/java/greencity/controller/UserProfileControllerTest.java
@@ -26,7 +26,9 @@ import java.util.List;
 import static greencity.ModelUtils.getPrincipal;
 import static greencity.ModelUtils.getUserProfileCreateDto;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
