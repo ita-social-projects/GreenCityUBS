bb2a62292e73619ecd3a768659c8e5385a180134
ABbondar <53192419+ABbondar@users.noreply.github.com>
2023-03-16 09:22:55 +0000

    Deploying to gh-pages from  @ 546618a4a4613355129996b912a5e731725d6f49 🚀

--- service-api/src/main/java/greencity/dto/employee/EmployeeDto.java
+++ service-api/src/main/java/greencity/dto/employee/EmployeeDto.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -32,6 +33,7 @@ public class EmployeeDto {
     @ValidPhoneNumber
     private String phoneNumber;
     @Email
+    @NotBlank
     private String email;
     private String image;
     @NotEmpty
