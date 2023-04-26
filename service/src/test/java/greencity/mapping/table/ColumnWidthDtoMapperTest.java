076ab49ce7761bc0f96cc528ea3a144142df7c06
Oleh Kulbaba <elatedexorcist@gmail.com>
2023-04-26 22:59:21 +0300

    Removed public modifier

--- service/src/test/java/greencity/mapping/table/ColumnWidthDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/table/ColumnWidthDtoMapperTest.java
@@ -11,7 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
-public class ColumnWidthDtoMapperTest {
+class ColumnWidthDtoMapperTest {
 
     @InjectMocks
     ColumnWidthDtoMapper columnWidthDtoMapper;
