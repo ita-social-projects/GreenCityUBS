788167006b3f568585975cc34d3733d8992d409b
Oleh Kulbaba <47332977+BranetE@users.noreply.github.com>
2023-04-27 10:08:47 +0300

    Changed orderId to Id (#1102)

---
+++ service/src/test/java/greencity/mapping/table/ColumnWidthDtoMapperTest.java
@@ -0,0 +1,27 @@
+package greencity.mapping.table;
+
+import greencity.ModelUtils;
+import greencity.dto.table.ColumnWidthDto;
+import greencity.entity.table.TableColumnWidthForEmployee;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class ColumnWidthDtoMapperTest {
+
+    @InjectMocks
+    ColumnWidthDtoMapper columnWidthDtoMapper;
+
+    @Test
+    void convert() {
+        TableColumnWidthForEmployee tableColumnWidthForEmployee = ModelUtils.getTestTableColumnWidth();
+
+        ColumnWidthDto columnWidthDto = columnWidthDtoMapper.convert(tableColumnWidthForEmployee);
+
+        assertEquals(tableColumnWidthForEmployee.getAmountDue(), columnWidthDto.getAmountDue());
+    }
+}
