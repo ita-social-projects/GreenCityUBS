5caccabf8e22bc73e1fdc916ed0cf6070c5a30dc
Anton <bondar.local@gmail.com>
2023-05-10 21:46:45 +0300

    edit error decoder

--- core/src/main/java/greencity/exception/handler/RetrieveMessageErrorDecoder.java
+++ core/src/main/java/greencity/exception/handler/RetrieveMessageErrorDecoder.java
@@ -12,25 +12,18 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class RetrieveMessageErrorDecoder implements ErrorDecoder {
     @Override
     public Exception decode(String methodKey, Response response) {
-        Map<String, Object> errorAttributes = new HashMap<>();
-        errorAttributes.put("message", "An error occurred");
-        errorAttributes.put("timestamp", "");
-
-        ExceptionResponce exception = new ExceptionResponce(errorAttributes);
-
+        ExceptionResponce exception;
         try (InputStream body = response.body().asInputStream()) {
             exception = ExceptionResponce
                 .builder()
                 .message(IOUtils.toString(body, StandardCharsets.UTF_8)).build();
         } catch (IOException e) {
-            return new Exception(exception.getMessage());
+            return new Exception(e.getMessage());
         }
 
         switch (response.status()) {
