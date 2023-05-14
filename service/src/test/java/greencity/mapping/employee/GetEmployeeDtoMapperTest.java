17301d15d1427fdd900f1c5be7b2b1a86b962120
Maksym Lenets <maximlenets@gmail.com>
2023-05-14 19:08:02 +0300

    fixed mapper updated test and dtos

--- service/src/test/java/greencity/mapping/employee/GetEmployeeDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/employee/GetEmployeeDtoMapperTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,5 +22,19 @@ class GetEmployeeDtoMapperTest {
         Employee employee = ModelUtils.getFullEmployee();
         GetEmployeeDto dto = ModelUtils.getGetEmployeeDto();
         assertEquals(mapper.convert(employee), dto);
+    }
+
+    @Test
+    void convertEmployeeWithEmptyTariffLocationToGetEmployeeDtoTest() {
+        Employee employee = ModelUtils.getFullEmployee();
+        employee.getTariffInfos().forEach(tariffsInfo -> tariffsInfo.setTariffLocations(Collections.emptySet()));
+
+        GetEmployeeDto dto = ModelUtils.getGetEmployeeDto();
+        dto.getTariffs().forEach(tariffs -> {
+            tariffs.setRegion(null);
+            tariffs.setLocationsDtos(Collections.emptyList());
+        });
+
+        assertEquals(dto, mapper.convert(employee));
     }
 }
