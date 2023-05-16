f3525b2962796c3630ce6e904df0d906fbebd6f3
Maksym Lenets <maximlenets@gmail.com>
2023-05-16 17:45:55 +0300

    fix code smell

--- dao/src/test/java/greencity/entity/table/TableColumnWidthForEmployeeTest.java
+++ dao/src/test/java/greencity/entity/table/TableColumnWidthForEmployeeTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TableColumnWidthForEmployeeTest {
+class TableColumnWidthForEmployeeTest {
     @Test
     void TableColumnWidthForEmployeeWithDefaultWidthTest() {
         Employee employee = Employee.builder().build();
