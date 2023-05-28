be0aca09251894a3f732d5142f73b4a44cc89afa
Markiro <65sumo@gmail.com>
2023-05-28 13:02:46 +0300

    - remove import

--- service-api/src/main/java/greencity/dto/AddNewTariffDto.java
+++ service-api/src/main/java/greencity/dto/AddNewTariffDto.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
