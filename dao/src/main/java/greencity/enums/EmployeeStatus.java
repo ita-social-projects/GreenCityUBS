1f0f26c4f8f46f470d4fb9135fc343ab3cf8bdce
Safarov Renat <SafarovRenat1995@gmail.com>
2023-06-08 19:42:23 +0300

    1) added documentation 2) refactored ManagementEmployeeControllerTest

--- dao/src/main/java/greencity/enums/EmployeeStatus.java
+++ dao/src/main/java/greencity/enums/EmployeeStatus.java
@@ -39,8 +39,14 @@ public enum EmployeeStatus {
         return "";
     }
 
+    /**
+     * This is method which checks if an EmployeeStatus with that name exists.
+     *
+     * @param statusName {@link String}.
+     * @return boolean.
+     */
     public static boolean employeeStatusExist(String statusName) {
         return Arrays.stream(EmployeeStatus.values())
-                .anyMatch(status -> status.name().equals(statusName));
+            .anyMatch(status -> status.name().equals(statusName));
     }
 }
