3e6416fc9e02e39e366616a5ef0044468d44aa09
Yurii Midianyi <loopfordev@gmail.com>
2023-06-30 15:48:09 +0300

    deleted unused library

--- dao/src/test/java/greencity/entity/table/TableColumnWidthForEmployeeTest.java
+++ dao/src/test/java/greencity/entity/table/TableColumnWidthForEmployeeTest.java
@@ -1,14 +1,11 @@
 package greencity.entity.table;
 
 import greencity.entity.user.employee.Employee;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TableColumnWidthForEmployeeTest {
