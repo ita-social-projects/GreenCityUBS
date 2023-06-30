005356d7140122f3e207b52a0f301db9eaaf6554
ospodaryk <ospodaryk11@gmail.com>
2023-06-30 20:23:07 +0300

    Fixed bug and Vulnerability

--- service/src/main/java/greencity/service/locations/LocationApiService.java
+++ service/src/main/java/greencity/service/locations/LocationApiService.java
@@ -263,28 +263,33 @@ public class LocationApiService {
     /**
      * Sends a GET request to a specified URL and processes the response.
      *
-     * @param url The URL to send the GET request to.
+     * @param fullUrl The URL to send the GET request to.
      * @return A List of LocationDto objects, each representing a location fetched
      *         from the URL.
      */
-    public List<LocationDto> getResultFromUrl(String url) {
+
+    public List<LocationDto> getResultFromUrl(String fullUrl) {
         List<LocationDto> locationDtos = new ArrayList<>();
-        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
-        if (response != null && response.getBody() != null) {
-            List<Map<String, Object>> results = (List<Map<String, Object>>) response.getBody().get("results");
-            if (results != null) {
-                for (Map<String, Object> result : results) {
-                    Map<String, String> nameMap = new HashMap<>();
-                    nameMap.put("name", (String) result.get("name"));
-                    nameMap.put("name_en", (String) result.get("name_en"));
-                    locationDtos.add(LocationDto.builder()
-                        .id((String) result.get("code"))
-                        .parentId((String) result.get("parent_id"))
-                        .name(nameMap).build());
+        ResponseEntity<Map> response = restTemplate.getForEntity(fullUrl, Map.class);
+        try {
+            if (response != null && response.getBody() != null) {
+                List<Map<String, Object>> results = (List<Map<String, Object>>) response.getBody().get("results");
+                if (results != null) {
+                    for (Map<String, Object> result : results) {
+                        Map<String, String> nameMap = new HashMap<>();
+                        nameMap.put("name", (String) result.get("name"));
+                        nameMap.put("name_en", (String) result.get("name_en"));
+                        locationDtos.add(LocationDto.builder()
+                                .id((String) result.get("code"))
+                                .parentId((String) result.get("parent_id"))
+                                .name(nameMap).build());
+                    }
                 }
+            } else {
+                throw new NotFoundException(ErrorMessage.NOT_FOUND_LOCATION_BY_URL + fullUrl);
             }
-        } else {
-            throw new NotFoundException(ErrorMessage.NOT_FOUND_LOCATION_BY_URL + url);
+        } catch (NullPointerException e) {
+            throw new NotFoundException(ErrorMessage.NOT_FOUND_LOCATION_BY_URL + fullUrl);
         }
         return locationDtos;
     }
