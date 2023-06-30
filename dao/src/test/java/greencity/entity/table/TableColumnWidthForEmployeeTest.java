56b9dc228d0df7c8252e5d99efc9c89bf5aa5a50
Yurii Midianyi <loopfordev@gmail.com>
2023-06-30 15:58:26 +0300

    changed name for constants according to the code conventions

--- dao/src/test/java/greencity/entity/table/TableColumnWidthForEmployeeTest.java
+++ dao/src/test/java/greencity/entity/table/TableColumnWidthForEmployeeTest.java
@@ -9,51 +9,51 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TableColumnWidthForEmployeeTest {
-    private static final Employee employee = Employee.builder().build();
-    private static final TableColumnWidthForEmployee tableColumnWidth = new TableColumnWidthForEmployee(employee);
+    private static final Employee EMPLOYEE = Employee.builder().build();
+    private static final TableColumnWidthForEmployee TABLE_COLUMN_WIDTH = new TableColumnWidthForEmployee(EMPLOYEE);
 
     @Test
     void checkTableColumnWidthEmployee() {
-        assertEquals(employee, tableColumnWidth.getEmployee());
+        assertEquals(EMPLOYEE, TABLE_COLUMN_WIDTH.getEmployee());
     }
 
     static Stream<Arguments> testArguments() {
         int defaultWidth = 120;
         return Stream.of(
-            Arguments.of(defaultWidth, tableColumnWidth.getAddress()),
-            Arguments.of(defaultWidth, tableColumnWidth.getAmountDue()),
-            Arguments.of(defaultWidth, tableColumnWidth.getBagsAmount()),
-            Arguments.of(defaultWidth, tableColumnWidth.getBlockedBy()),
-            Arguments.of(defaultWidth, tableColumnWidth.getCity()),
-            Arguments.of(defaultWidth, tableColumnWidth.getClientEmail()),
-            Arguments.of(defaultWidth, tableColumnWidth.getClientName()),
-            Arguments.of(defaultWidth, tableColumnWidth.getClientPhone()),
-            Arguments.of(defaultWidth, tableColumnWidth.getCommentForOrderByClient()),
-            Arguments.of(defaultWidth, tableColumnWidth.getCommentToAddressForClient()),
-            Arguments.of(defaultWidth, tableColumnWidth.getCommentsForOrder()),
-            Arguments.of(defaultWidth, tableColumnWidth.getDateOfExport()),
-            Arguments.of(defaultWidth, tableColumnWidth.getDistrict()),
-            Arguments.of(defaultWidth, tableColumnWidth.getGeneralDiscount()),
-            Arguments.of(defaultWidth, tableColumnWidth.getOrderId()),
-            Arguments.of(defaultWidth, tableColumnWidth.getIdOrderFromShop()),
-            Arguments.of(defaultWidth, tableColumnWidth.getOrderCertificateCode()),
-            Arguments.of(defaultWidth, tableColumnWidth.getOrderDate()),
-            Arguments.of(defaultWidth, tableColumnWidth.getOrderPaymentStatus()),
-            Arguments.of(defaultWidth, tableColumnWidth.getOrderStatus()),
-            Arguments.of(defaultWidth, tableColumnWidth.getPaymentDate()),
-            Arguments.of(defaultWidth, tableColumnWidth.getReceivingStatus()),
-            Arguments.of(defaultWidth, tableColumnWidth.getRegion()),
-            Arguments.of(defaultWidth, tableColumnWidth.getResponsibleCaller()),
-            Arguments.of(defaultWidth, tableColumnWidth.getResponsibleDriver()),
-            Arguments.of(defaultWidth, tableColumnWidth.getResponsibleLogicMan()),
-            Arguments.of(defaultWidth, tableColumnWidth.getResponsibleNavigator()),
-            Arguments.of(defaultWidth, tableColumnWidth.getSenderEmail()),
-            Arguments.of(defaultWidth, tableColumnWidth.getSenderName()),
-            Arguments.of(defaultWidth, tableColumnWidth.getSenderPhone()),
-            Arguments.of(defaultWidth, tableColumnWidth.getTimeOfExport()),
-            Arguments.of(defaultWidth, tableColumnWidth.getTotalOrderSum()),
-            Arguments.of(defaultWidth, tableColumnWidth.getTotalPayment()),
-            Arguments.of(defaultWidth, tableColumnWidth.getViolationsAmount()));
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getAddress()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getAmountDue()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getBagsAmount()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getBlockedBy()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getCity()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getClientEmail()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getClientName()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getClientPhone()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getCommentForOrderByClient()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getCommentToAddressForClient()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getCommentsForOrder()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getDateOfExport()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getDistrict()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getGeneralDiscount()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getOrderId()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getIdOrderFromShop()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getOrderCertificateCode()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getOrderDate()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getOrderPaymentStatus()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getOrderStatus()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getPaymentDate()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getReceivingStatus()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getRegion()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getResponsibleCaller()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getResponsibleDriver()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getResponsibleLogicMan()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getResponsibleNavigator()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getSenderEmail()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getSenderName()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getSenderPhone()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getTimeOfExport()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getTotalOrderSum()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getTotalPayment()),
+            Arguments.of(defaultWidth, TABLE_COLUMN_WIDTH.getViolationsAmount()));
     }
 
     @ParameterizedTest
@@ -80,7 +80,7 @@ class TableColumnWidthForEmployeeTest {
             .build();
 
         assertNotNull(tableColumnWidth);
-        assertEquals(employee, tableColumnWidth.getEmployee());
+        assertEquals(EMPLOYEE, tableColumnWidth.getEmployee());
         assertEquals(100, tableColumnWidth.getAddress());
         assertEquals(200, tableColumnWidth.getAmountDue());
     }
