976787847c152d0b82799e5a74fc5c55e42d9a8c
Yurii Midianyi <loopfordev@gmail.com>
2023-06-30 17:20:09 +0300

    fixed method imports from library

--- dao/src/test/java/greencity/entity/table/TableColumnWidthForEmployeeTest.java
+++ dao/src/test/java/greencity/entity/table/TableColumnWidthForEmployeeTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class TableColumnWidthForEmployeeTest {
     private static final Employee EMPLOYEE = Employee.builder().build();
