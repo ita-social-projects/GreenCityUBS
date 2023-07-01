2e56f5269924b4165757a9f866e90e7103cc0ef1
ospodaryk <ospodaryk11@gmail.com>
2023-07-01 11:18:07 +0300

    Fixed controller

--- core/src/main/java/greencity/controller/AddressController.java
+++ core/src/main/java/greencity/controller/AddressController.java
@@ -16,15 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
@@ -154,7 +146,8 @@ public class AddressController {
      *         specified region and city.
      */
     @GetMapping("/get-all-districts")
-    public ResponseEntity<List<LocationDto>> getAllDistrictsForRegionAndCity(String region, String city) {
+    public ResponseEntity<List<LocationDto>> getAllDistrictsForRegionAndCity(@RequestParam String region,
+        @RequestParam String city) {
         return ResponseEntity.status(HttpStatus.OK)
             .body(ubsClientService.getAllDistrictsForRegionAndCity(region, city));
     }
