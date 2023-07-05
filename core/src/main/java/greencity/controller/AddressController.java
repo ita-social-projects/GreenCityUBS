91c6c59b31be77da72bb310454c75ae644889a30
ospodaryk <ospodaryk11@gmail.com>
2023-07-05 15:51:21 +0300

    added forbiden

--- core/src/main/java/greencity/controller/AddressController.java
+++ core/src/main/java/greencity/controller/AddressController.java
@@ -165,6 +165,7 @@ public class AddressController {
         @ApiResponse(code = 200, message = HttpStatuses.OK, response = OrderCancellationReasonDto.class),
         @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
         @ApiResponse(code = 401, message = HttpStatuses.UNAUTHORIZED),
+        @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN),
         @ApiResponse(code = 404, message = HttpStatuses.NOT_FOUND)
     })
     @GetMapping("/get-all-districts")
