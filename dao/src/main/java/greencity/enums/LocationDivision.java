3858eec81a5644a85e3da39cbcbd153caa13873c
ospodaryk <ospodaryk11@gmail.com>
2023-07-05 19:20:15 +0300

    Deleted nameUa from LocationDivision

--- dao/src/main/java/greencity/enums/LocationDivision.java
+++ dao/src/main/java/greencity/enums/LocationDivision.java
@@ -2,34 +2,33 @@ package greencity.enums;
 
 /**
  * Enum for representing various location divisions. Each enum instance
- * represents a different level of division, with an associated level ID and
- * Ukrainian name.
+ * represents a different level of division, with an associated level ID
  */
 public enum LocationDivision {
     /**
      * Represents a region or autonomous republic.
      */
-    REGION(1, "Область або АРК"),
+    REGION(1),
 
     /**
      * Represents a district within a region or autonomous republic.
      */
-    DISTRICT_IN_REGION(2, "Район в області або в АРК"),
+    DISTRICT_IN_REGION(2),
 
     /**
      * Represents a local community.
      */
-    LOCAL_COMMUNITY(3, "Територіальна громада"),
+    LOCAL_COMMUNITY(3),
 
     /**
      * Represents a city or village.
      */
-    CITY(4, "Місто або село"),
+    CITY(4),
 
     /**
      * Represents a district within a city.
      */
-    DISTRICT_IN_CITY(5, "Район в місті");
+    DISTRICT_IN_CITY(5);
 
     /**
      * ID for the location division level.
@@ -37,19 +36,12 @@ public enum LocationDivision {
     int levelId;
 
     /**
-     * Ukrainian name for the location division.
-     */
-    String nameUa;
-
-    /**
      * Constructor for creating a location division instance.
      *
      * @param levelId The ID for the location division level.
-     * @param nameUa  The Ukrainian name for the location division.
      */
-    LocationDivision(int levelId, String nameUa) {
+    LocationDivision(int levelId) {
         this.levelId = levelId;
-        this.nameUa = nameUa;
     }
 
     /**
