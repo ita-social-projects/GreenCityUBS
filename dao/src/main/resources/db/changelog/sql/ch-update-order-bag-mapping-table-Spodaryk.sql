bac014d64d27af27468987bc09aec1b71cca01d5
ospodaryk <ospodaryk11@gmail.com>
2023-08-14 11:43:02 +0300

    Fixed merge conflicts and replaced files

--- dao/src/main/resources/db/changelog/sql/ch-update-order-bag-mapping-table-Spodaryk.sql
+++ dao/src/main/resources/db/changelog/sql/ch-update-order-bag-mapping-table-Spodaryk.sql
@@ -1,6 +1,7 @@
-update order_bag_mapping
-set capacity=bag.capacity,
-    price=bag.full_price,
-    name=bag.name,
-    name_eng=bag.name_eng from bag
-where bag_id=bag.id
+UPDATE order_bag_mapping
+SET capacity = bag.capacity,
+    price = bag.full_price,
+    name = bag.name,
+    name_eng = bag.name_eng
+    FROM bag
+WHERE order_bag_mapping.bag_id = bag.id;
