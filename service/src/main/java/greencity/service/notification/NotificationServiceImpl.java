adfabf4b578176b2617e2bb1df459e7171957fe6
Anton Bondar <bondar.local@gmail.com>
2024-02-16 15:44:12 +0200

    formatter

--- service/src/main/java/greencity/service/notification/NotificationServiceImpl.java
+++ service/src/main/java/greencity/service/notification/NotificationServiceImpl.java
@@ -41,7 +41,6 @@ import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.text.StringSubstitutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
