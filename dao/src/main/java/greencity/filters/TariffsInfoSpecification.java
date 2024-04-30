486091056631550683f03d6bbfbb2360989c7ccc
Denys <122037168+ryhaldenys@users.noreply.github.com>
2024-04-30 11:48:25 +0300

    Fix bug GET /ubs/superAdmin/tariffs return 401 when user tries to filter by criterias. (#1321)
    * improved logic in TariffsInfoSpecification.
    
    * improved logic in TariffsInfoSpecification and implemented tests.

--- dao/src/main/java/greencity/filters/TariffsInfoSpecification.java
+++ dao/src/main/java/greencity/filters/TariffsInfoSpecification.java
@@ -1,8 +1,14 @@
 package greencity.filters;
 
-import greencity.enums.TariffStatus;
-import greencity.entity.order.Courier;
+import greencity.entity.order.Courier_;
+import greencity.entity.order.TariffLocation_;
 import greencity.entity.order.TariffsInfo;
+import greencity.entity.order.TariffsInfo_;
+import greencity.entity.order.Courier;
+import greencity.entity.user.Location_;
+import greencity.entity.user.Region_;
+import greencity.entity.user.employee.ReceivingStation_;
+import greencity.enums.TariffStatus;
 import greencity.entity.user.Location;
 import greencity.entity.user.Region;
 import greencity.entity.user.employee.ReceivingStation;
@@ -19,25 +25,25 @@ public class TariffsInfoSpecification implements Specification<TariffsInfo> {
 
     @Override
     public Predicate toPredicate(Root<TariffsInfo> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-        Path<Location> location = root.join("tariffLocations").get("location");
-        Path<Region> region = location.get("region");
-        Path<Courier> courier = root.get("courier");
-        Path<ReceivingStation> receivingStation = root.join("receivingStationList");
-        Path<TariffStatus> status = root.get("tariffStatus");
+        Path<Location> location = root.join(TariffsInfo_.TARIFF_LOCATIONS).get(TariffLocation_.LOCATION);
+        Path<Region> region = location.get(Location_.REGION);
+        Path<Courier> courier = root.get(TariffsInfo_.COURIER);
+        Path<ReceivingStation> receivingStation = root.join(TariffsInfo_.RECEIVING_STATION_LIST);
+        Path<TariffStatus> status = root.get(TariffsInfo_.TARIFF_STATUS);
 
         List<Predicate> predicates = new ArrayList<>();
 
         if (nonNull(criteria.getRegion())) {
-            predicates.add(criteriaBuilder.equal(region, criteria.getRegion()));
+            predicates.add(criteriaBuilder.equal(region.get(Region_.ID), criteria.getRegion()));
         }
         if (nonNull(criteria.getLocation())) {
-            predicates.add(location.in((Object[]) criteria.getLocation()));
+            predicates.add(location.get(Location_.ID).in((Object[]) criteria.getLocation()));
         }
         if (nonNull(criteria.getCourier())) {
-            predicates.add(criteriaBuilder.equal(courier, criteria.getCourier()));
+            predicates.add(criteriaBuilder.equal(courier.get(Courier_.ID), criteria.getCourier()));
         }
         if (nonNull(criteria.getReceivingStation())) {
-            predicates.add(receivingStation.in((Object[]) criteria.getReceivingStation()));
+            predicates.add(receivingStation.get(ReceivingStation_.ID).in((Object[]) criteria.getReceivingStation()));
         }
         if (nonNull(criteria.getStatus())) {
             predicates.add(criteriaBuilder.equal(status, criteria.getStatus()));
