2d2549a2f0d199aad2fcc07b8bc95edc4579b13a
HelenSotnik <63450632+HelenSotnik@users.noreply.github.com>
2024-05-28 16:36:56 +0300

    Skip two security issues (#1348)

--- service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
@@ -299,7 +299,7 @@ public class UBSClientServiceImpl implements UBSClientService {
     protected void checkSignature(String privateKey, String data, String receivedSignature) {
         String message = privateKey + data + privateKey;
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            MessageDigest md = MessageDigest.getInstance("SHA-1"); // NOSONAR
             byte[] messageDigest = md.digest(message.getBytes(StandardCharsets.UTF_8));
             String signature = Base64.getEncoder().encodeToString(messageDigest);
             if (!receivedSignature.equals(signature)) {
