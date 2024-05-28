b63d51237555ae8ebe01d6cadae03e60efb73cbd
Lena Sotnik <lenasotnik@Lenas-MacBook-Air.local>
2024-05-28 12:00:54 +0300

    Commit with fix houseNumber for save new address with placeId

--- service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
@@ -684,6 +684,7 @@ public class UBSClientServiceImpl implements UBSClientService {
         }
 
         OrderAddressDtoRequest dtoRequest = getLocationDto(addressRequestDto.getPlaceId());
+        dtoRequest.setHouseNumber(addressRequestDto.getHouseNumber());
 
         OrderAddressDtoRequest addressRequestDtoForNullCheck =
             modelMapper.map(addressRequestDto, OrderAddressDtoRequest.class);
