0a672c3b7839529761a0e853bbf3d63eeab4cb8d
ryhaldenys <denys.ryhal.knm.2020@lpnu.ua>
2024-06-07 13:09:38 +0300

    removed unused import.

--- service/src/main/java/greencity/mapping/tariff/GetTariffInfoForEmployeeDtoMapper.java
+++ service/src/main/java/greencity/mapping/tariff/GetTariffInfoForEmployeeDtoMapper.java
@@ -9,7 +9,6 @@ import greencity.entity.order.TariffsInfo;
 import org.modelmapper.AbstractConverter;
 import org.springframework.stereotype.Component;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class GetTariffInfoForEmployeeDtoMapper extends AbstractConverter<TariffsInfo, GetTariffInfoForEmployeeDto> {
