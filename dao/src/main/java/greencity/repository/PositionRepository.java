cc106b6b9bde712ce3be119e359af9097539454a
Dmytro <113462277+KizerovDmitriy@users.noreply.github.com>
2024-06-14 09:36:51 +0300

    Adds checks to block orders (#1374)

--- dao/src/main/java/greencity/repository/PositionRepository.java
+++ dao/src/main/java/greencity/repository/PositionRepository.java
@@ -2,7 +2,10 @@ package greencity.repository;
 
 import greencity.entity.user.employee.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PositionRepository extends JpaRepository<Position, Long> {
@@ -22,4 +25,13 @@ public interface PositionRepository extends JpaRepository<Position, Long> {
      * @return {@link Boolean}
      */
     boolean existsPositionByIdAndName(Long id, String name);
+
+    /**
+     * Finds all position IDs that match the given list of English names.
+     *
+     * @param names the list of English names to search for
+     * @return a list of position IDs that have names matching the given names
+     */
+    @Query("SELECT p.id FROM Position p WHERE p.nameEn IN :names")
+    List<Long> findAllIdsFromNames(@Param("names") List<String> names);
 }
