0bde25e21c5decb388c634c2f7defcb95fe0f7b4
lagysha <57960716+lagysha@users.noreply.github.com>
2024-06-24 14:29:42 +0000

    Deploying to gh-pages from  @ 1e5d27b792b9e730b2887f323028da774b693938 🚀

--- service/src/main/java/greencity/service/ubs/UBSManagementEmployeeServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSManagementEmployeeServiceImpl.java
@@ -230,10 +230,15 @@ public class UBSManagementEmployeeServiceImpl implements UBSManagementEmployeeSe
         updatedEmployee.setEmployeeStatus(upEmployee.getEmployeeStatus());
 
         if (image != null) {
+            String imageUrlToDelete = upEmployee.getImagePath();
             updatedEmployee.setImagePath(fileService.upload(image));
+            if (!imageUrlToDelete.equals(defaultImagePath)) {
+                fileService.delete(upEmployee.getImagePath());
+            }
         } else {
-            updatedEmployee.setImagePath(upEmployee.getImagePath());
+            updatedEmployee.setImagePath(dto.getEmployeeDto().getImage());
         }
+
         return modelMapper.map(employeeRepository.save(updatedEmployee), EmployeeWithTariffsDto.class);
     }
 
