d63b2c3ca91f06ca93ffa84f52a3216658b6ca22
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2024-06-27 10:28:53 +0300

    Modify access control for fetching all notification templates

--- core/src/main/java/greencity/controller/ManagementNotificationController.java
+++ core/src/main/java/greencity/controller/ManagementNotificationController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -38,6 +39,7 @@ public class ManagementNotificationController {
     })
     @GetMapping("/get-all-templates")
     @ApiPageable
+    @PreAuthorize("@preAuthorizer.hasAuthority('SEE_MESSAGES_PAGE', authentication)")
     public ResponseEntity<PageableDto<NotificationTemplateDto>> getAll(@ApiIgnore Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK)
             .body(notificationTemplateService.findAll(pageable));
