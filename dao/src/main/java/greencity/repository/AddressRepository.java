e97564855ddaa56a1df410f2756984d2a9a9b52a
lukovskj <volodymyr.lukovskyj@gmail.com>
2024-07-01 15:53:14 +0300

    formatter

--- dao/src/main/java/greencity/repository/AddressRepository.java
+++ dao/src/main/java/greencity/repository/AddressRepository.java
@@ -109,6 +109,8 @@ public interface AddressRepository extends CrudRepository<Address, Long> {
      *
      * @return list of {@link Address}
      */
-    @Query(value = "SELECT a FROM Address  a WHERE a.id IN (SELECT MIN(ad.id) FROM Address  ad WHERE ad.region = a.region)")
+    @Query(
+        value = "SELECT a FROM Address  a WHERE a.id IN (SELECT MIN(ad.id) "
+                + "FROM Address  ad WHERE ad.region = a.region)")
     List<Address> findDistinctRegions();
 }
