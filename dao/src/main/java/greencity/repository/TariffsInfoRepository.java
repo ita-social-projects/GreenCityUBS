59fe96b5e9dfcd7b975c607157d5c4b3e6443b33
Maryna-511750 <109355692+Maryna-511750@users.noreply.github.com>
2024-08-01 14:00:13 +0300

    Fix set tariffs id logic prod (#1454)
    * Fixed setTariffsId logic in getAllLocationsByCourierId method of UBSClientServiceImpl file. Added findTariffIdByLocationIdAndCourierId method to TariffsInfoRepository.
    
    * Fixed findTariffIdByLocationId method to TariffsInfoRepository.

--- dao/src/main/java/greencity/repository/TariffsInfoRepository.java
+++ dao/src/main/java/greencity/repository/TariffsInfoRepository.java
@@ -158,10 +158,27 @@ public interface TariffsInfoRepository extends JpaRepository<TariffsInfo, Long>,
      *         Optional.
      */
     @Query(nativeQuery = true,
+        value = "SELECT tariffs_info_id FROM tariffs_locations "
+            + "WHERE location_id = :locationId")
+    Optional<Long> findTariffIdByLocationId(Long locationId);
+
+    /**
+     * Retrieves the tariff ID associated with the specified location ID and courier
+     * ID.
+     *
+     * @param locationId The ID of the location to retrieve the tariff ID for.
+     * @param courierId  The ID of the courier to retrieve the tariff ID for.
+     * @return An Optional containing the tariff ID if found, otherwise an empty
+     *         Optional.
+     */
+    @Query(nativeQuery = true,
         value = "SELECT tariffs_info_id FROM tariffs_locations AS tl "
             + "INNER JOIN tariffs_info AS ti ON ti.id = tl.tariffs_info_id  "
+            + "JOIN courier AS c ON c.id = ti.courier_id "
             + "WHERE tl.location_id = :locationId "
             + "AND tl.location_status = 'ACTIVE' "
-            + "AND ti.tariff_status = 'ACTIVE'")
-    Optional<Long> findTariffIdByLocationId(Long locationId);
+            + "AND ti.tariff_status = 'ACTIVE'"
+            + "AND c.courier_status = 'ACTIVE' "
+            + "AND c.id = :courierId ")
+    Optional<Long> findTariffIdByLocationIdAndCourierId(Long locationId, Long courierId);
 }
