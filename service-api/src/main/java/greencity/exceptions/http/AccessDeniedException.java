5c7229eac64321aed0b24d30112db47defedfe85
DmytroDmytruk <97033032+DmytroDmytruk@users.noreply.github.com>
2024-08-02 14:34:12 +0300

    .

--- service-api/src/main/java/greencity/exceptions/http/AccessDeniedException.java
+++ service-api/src/main/java/greencity/exceptions/http/AccessDeniedException.java
@@ -2,6 +2,12 @@ package greencity.exceptions.http;
 
 public class AccessDeniedException extends RuntimeException {
     /**
+     * Default constructor.
+     */
+    public AccessDeniedException() {
+    }
+    
+    /**
      * Constructor with message.
      *
      * @param message message, that explains cause of the exception.
