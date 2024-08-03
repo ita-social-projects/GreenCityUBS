1e6f6ae9d1d74f7a67df8b82072b55e554e63b99
DmytroDmytruk <97033032+DmytroDmytruk@users.noreply.github.com>
2024-08-03 20:04:51 +0300

    fixed 401 error code

--- core/src/main/java/greencity/configuration/SecurityConfig.java
+++ core/src/main/java/greencity/configuration/SecurityConfig.java
@@ -97,6 +97,7 @@ public class SecurityConfig {
                 .sendError(SC_UNAUTHORIZED, "Authorize first."))
                 .accessDeniedHandler((req, resp, exc) -> resp.sendError(SC_FORBIDDEN, "You don't have authorities.")))
             .authorizeHttpRequests(req -> req
+                .requestMatchers("/error").permitAll()
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(HttpMethod.GET,
                     UBS_LINK + "/getAllActiveCouriers",
