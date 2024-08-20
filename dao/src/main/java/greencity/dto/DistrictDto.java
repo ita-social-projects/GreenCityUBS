7b28ece18875e9c58effeb66d1d36f6954ffddd6
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2024-08-20 11:33:08 +0300

    adds logic to retrieve information about all cities and districts, if cities or districts is empty

--- dao/src/main/java/greencity/dto/DistrictDto.java
+++ dao/src/main/java/greencity/dto/DistrictDto.java
@@ -1,12 +1,14 @@
 package greencity.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class DistrictDto {
     private String district;
     private String districtEn;
