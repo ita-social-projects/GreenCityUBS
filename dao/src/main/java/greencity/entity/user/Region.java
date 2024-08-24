230321034b385215854d7cbee05f834bff1faa4c
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2024-08-24 15:32:01 +0300

    removed wildcard imports

--- dao/src/main/java/greencity/entity/user/Region.java
+++ dao/src/main/java/greencity/entity/user/Region.java
@@ -1,7 +1,15 @@
 package greencity.entity.user;
 
 import greencity.entity.user.locations.City;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Column;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
--- dao/src/main/java/greencity/entity/user/locations/City.java
+++ dao/src/main/java/greencity/entity/user/locations/City.java
@@ -1,7 +1,17 @@
 package greencity.entity.user.locations;
 
 import greencity.entity.user.Region;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Column;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
