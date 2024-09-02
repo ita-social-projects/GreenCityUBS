9ccf8c17d084c72811624f9a8647f1ab9340c888
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2024-09-02 15:53:05 +0300

    Create new endpoint to receive information for all regions, added new dto classes

---
+++ service-api/src/main/java/greencity/dto/location/api/DistrictInfoDto.java
@@ -0,0 +1,16 @@
+package greencity.dto.location.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+public class DistrictInfoDto extends BaseLocationsClass {
+    private Long cityId;
+}
