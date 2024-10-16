e4ba4badd4b66dc48fa2529af32444b790a7e242
nazariusPr <protsnazar2004@gmail.com>
2024-10-16 12:41:54 +0300

    fix new issues

--- service-api/src/main/java/greencity/dto/location/api/CityInfoDto.java
+++ service-api/src/main/java/greencity/dto/location/api/CityInfoDto.java
@@ -6,8 +6,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Data
