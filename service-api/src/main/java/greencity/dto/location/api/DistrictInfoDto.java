b972753ba1219ac1ccc37d51ee768433feef24d3
nazariusPr <protsnazar2004@gmail.com>
2024-10-16 12:11:41 +0300

    Update DistrictInfoDto.java

--- service-api/src/main/java/greencity/dto/location/api/DistrictInfoDto.java
+++ service-api/src/main/java/greencity/dto/location/api/DistrictInfoDto.java
@@ -1,16 +1,18 @@
 package greencity.dto.location.api;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+@Data
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class DistrictInfoDto extends BaseLocationsClass {
     private Long cityId;
 }
