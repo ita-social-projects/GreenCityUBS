b2639e4599596795b458924bb598904ccedfe88b
Viktoriia Herchanivska <viktoriia.herchanivska@gmail.com>
2024-10-28 13:34:39 +0200

    remove unused api response

--- core/src/main/java/greencity/controller/OrderController.java
+++ core/src/main/java/greencity/controller/OrderController.java
@@ -382,7 +382,6 @@ public class OrderController {
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
         @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST, content = @Content),
-        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED, content = @Content),
         @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND, content = @Content)
     })
     public ResponseEntity<OrderCourierPopUpDto> getAllActiveLocationsByCourierId(
