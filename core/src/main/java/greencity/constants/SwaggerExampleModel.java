67b6f3e7808e52e589918fe8612b00ac00ddfd9b
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-12-09 00:05:57 +0200

    Code cleanup and optimization
    - Formatted blank lines for improved readability.
    - Replaced string concatenations with text blocks in SwaggerExampleModel for better readability and maintainability.

--- core/src/main/java/greencity/constants/SwaggerExampleModel.java
+++ core/src/main/java/greencity/constants/SwaggerExampleModel.java
@@ -4,39 +4,44 @@ public final class SwaggerExampleModel {
     private SwaggerExampleModel() {
     }
 
-    private static final String BEFORE_EXAMPLE = "<div>\n"
-        + "\t<ul class=\"tab\">\n"
-        + "\t\t<li class=\"tabitem active\">\n"
-        + "\t\t\t<a class=\"tablinks\" data-name=\"example\">Example Value</a>\n"
-        + "\t\t</li>\n"
-        + "\t\t<li class=\"tabitem\">\n"
-        + "\t\t\t<a class=\"tablinks\" data-name=\"model\">Model</a>\n"
-        + "\t\t</li>\n"
-        + "\t</ul>\n"
-        + "\t<pre>\n";
+    private static final String BEFORE_EXAMPLE = """
+        <div>
+            <ul class="tab">
+                <li class="tabitem active">
+                    <a class="tablinks" data-name="example">Example Value</a>
+                </li>
+                <li class="tabitem">
+                    <a class="tablinks" data-name="model">Model</a>
+                </li>
+            </ul>
+            <pre>
+        """;
 
-    private static final String AFTER_EXAMPLE = "\t</pre>\n"
-        + "</div>";
+    private static final String AFTER_EXAMPLE = """
+            </pre>
+        </div>""";
 
-    private static final String EMPLOYEE_BEGIN =
-        "  \"employeeDto\": \n"
-            + "{\n"
-            + "  \"firstName\": \"string\",\n"
-            + "  \"lastName\": \"string\",\n"
-            + "  \"phoneNumber\": \"string\",\n"
-            + "  \"email\": \"string\",\n";
+    private static final String EMPLOYEE_BEGIN = """
+          "employeeDto":\s
+        {
+          "firstName": "string",
+          "lastName": "string",
+          "phoneNumber": "string",
+          "email": "string",
+        """;
 
-    private static final String EMPLOYEE_END =
-        "  \"employeePositions\": [ \n"
-            + " {\n"
-            + "      \"id\": 0,\n"
-            + "      \"name\": \"string\"\n"
-            + "  }\n"
-            + " ]},\n"
-            + " \"tariffId\": "
-            + "  [\n 0 \n"
-            + "  ]\n"
-            + "}";
+    private static final String EMPLOYEE_END = """
+          "employeePositions": [\s
+         {
+              "id": 0,
+              "name": "string"
+          }
+         ]},
+         "tariffId": \
+          [
+         0\s
+          ]
+        }""";
 
     public static final String ADD_NEW_EMPLOYEE =
         BEFORE_EXAMPLE
