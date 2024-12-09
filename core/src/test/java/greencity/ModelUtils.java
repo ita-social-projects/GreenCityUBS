518f1a2c62191aec072c59f5d9c9304e118a09d5
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-12-09 13:53:51 +0200

    Refactor: Simplified method calls for better readability
    - Replaced get(0) with getFirst() for clearer intent
    - Updated collect(Collectors.toList()) to toList() for simplicity and immutability

--- core/src/test/java/greencity/ModelUtils.java
+++ core/src/test/java/greencity/ModelUtils.java
@@ -74,7 +74,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static greencity.enums.ViolationLevel.MAJOR;
 
@@ -260,7 +259,7 @@ public class ModelUtils {
     public static ViolationDetailInfoDto getViolationDetailInfoDto() {
         LocalDateTime localdatetime = LocalDateTime.of(
             2021, Month.MARCH,
-            16, 13, 00, 00);
+            16, 13, 0, 0);
 
         return ViolationDetailInfoDto.builder()
             .orderId(1L)
@@ -272,17 +271,16 @@ public class ModelUtils {
     }
 
     public static Address address() {
-        List<Long> id = addressDto().stream().map(AddressDto::getId).collect(Collectors.toList());
-        List<String> city = addressDto().stream().map(AddressDto::getCity).collect(Collectors.toList());
-        List<String> street = addressDto().stream().map(AddressDto::getStreet).collect(Collectors.toList());
-        List<String> district = addressDto().stream().map(AddressDto::getDistrict).collect(Collectors.toList());
-        List<String> houseNumber = addressDto().stream().map(AddressDto::getHouseNumber).collect(Collectors.toList());
-        List<String> entranceNumber =
-            addressDto().stream().map(AddressDto::getEntranceNumber).collect(Collectors.toList());
-        List<String> houseCorpus = addressDto().stream().map(AddressDto::getHouseCorpus).collect(Collectors.toList());
-        List<Boolean> actual = addressDto().stream().map(AddressDto::getActual).collect(Collectors.toList());
+        List<Long> id = addressDto().stream().map(AddressDto::getId).toList();
+        List<String> city = addressDto().stream().map(AddressDto::getCity).toList();
+        List<String> street = addressDto().stream().map(AddressDto::getStreet).toList();
+        List<String> district = addressDto().stream().map(AddressDto::getDistrict).toList();
+        List<String> houseNumber = addressDto().stream().map(AddressDto::getHouseNumber).toList();
+        List<String> entranceNumber = addressDto().stream().map(AddressDto::getEntranceNumber).toList();
+        List<String> houseCorpus = addressDto().stream().map(AddressDto::getHouseCorpus).toList();
+        List<Boolean> actual = addressDto().stream().map(AddressDto::getActual).toList();
         return Address.builder()
-            .id(id.get(0))
+            .id(id.getFirst())
             .city(String.valueOf(city))
             .district(String.valueOf(district))
             .street(String.valueOf(street))
