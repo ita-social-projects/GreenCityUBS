4dcf270069078138318303d8bf5285dfe45567dd
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2024-12-12 13:34:11 +0200

    refactor OrderAddressRepository repo and fixed tests

--- dao/src/main/java/greencity/repository/OrderAddressRepository.java
+++ dao/src/main/java/greencity/repository/OrderAddressRepository.java
@@ -9,17 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderAddressRepository extends JpaRepository<OrderAddress, Long> {
     /**
-     * Method return of {@link OrderAddress} address for current order.
-     *
-     * @return {@link OrderAddress}.
-     */
-    @Query(value = "SELECT * FROM orders as o "
-        + " JOIN ubs_user as ubs ON o.ubs_user_id = ubs.id "
-        + " JOIN address as addr ON addr.id = ubs.address_id "
-        + " WHERE o.id = :orderId", nativeQuery = true)
-    OrderAddress getOrderAddressByOrderId(Long orderId);
-
-    /**
      * Finds an {@link OrderAddress} by order ID.
      *
      * @param orderId the ID of the order whose address is being searched for
