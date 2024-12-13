1bcec75c4c2fb6394685cd285635e340fbb7e54a
KizerovDmitriy <113462277+KizerovDmitriy@users.noreply.github.com>
2024-12-13 07:24:04 +0000

    Deploying to gh-pages from  @ 64d0c8a3b6921d65112598294e5de758a9e290ec 🚀

--- dao/src/main/java/greencity/repository/OrderAddressRepository.java
+++ dao/src/main/java/greencity/repository/OrderAddressRepository.java
@@ -10,17 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderAddressRepository extends JpaRepository<OrderAddress, Long> {
     /**
-     * Method return of {@link OrderAddress} address for current order.
-     *
-     * @return {@link OrderAddress}.
-     */
-    @Query(value = "SELECT o.* FROM orders as o "
-        + " JOIN ubs_user as ubs ON o.ubs_user_id = ubs.id "
-        + " JOIN address as addr ON addr.id = ubs.address_id "
-        + " WHERE o.id = :orderId", nativeQuery = true)
-    OrderAddress getOrderAddressByOrderId(Long orderId);
-
-    /**
      * Method checks if {@link OrderAddress} exists by {@link Location}.
      *
      * @return boolean.
