dea3ae00e06764cd93e1772bcba552f469bd4405
Ivan Holotsvan <iholotsvan@gmail.com>
2024-12-16 16:47:49 +0200

    reduce redundancy in changesets

--- dao/src/main/resources/db/changelog/sql/ch-update-order-bag-mapping-table-Spodaryk.sql
+++
@@ -1,7 +0,0 @@
-UPDATE order_bag_mapping
-SET capacity = bag.capacity,
-    price = bag.full_price,
-    name = bag.name,
-    name_eng = bag.name_eng
-    FROM bag
-WHERE order_bag_mapping.bag_id = bag.id;
--- dao/src/main/resources/db/changelog/sql/tariff-name-length-change.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE service
-    ALTER COLUMN name TYPE varchar(255);
-ALTER TABLE service
-    ALTER COLUMN name_eng TYPE varchar(255);
