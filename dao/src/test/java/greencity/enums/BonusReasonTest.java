fd6d9dd3deab559cdc0a978b057334c566d8c01d
Chernenko Vitaliy <chernenko_vitaliy@ukr.net>
2024-12-19 16:23:30 +0200

    Refactored BonusReasonTest according to sonar issue;

--- dao/src/test/java/greencity/enums/BonusReasonTest.java
+++ dao/src/test/java/greencity/enums/BonusReasonTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BonusReasonTest {
+class BonusReasonTest {
     @Test
     void returnOverpayConstantValidTest() {
         BonusReason reason = BonusReason.RETURN_OVERPAY;
