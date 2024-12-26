7ec377a4b8117cd57302fd4e6daa7fa336485d59
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2024-12-26 11:40:22 +0200

    fixed regex in ChatLinkDto.java

--- service-api/src/main/java/greencity/dto/user/ChatLinkDto.java
+++ service-api/src/main/java/greencity/dto/user/ChatLinkDto.java
@@ -7,6 +7,6 @@ import org.hibernate.validator.constraints.Length;
 
 public record ChatLinkDto(
     Long userId,
-    @JsonProperty("link") @Length(max = 255) @NotNull @Pattern(regexp = "^https://my\\.binotel\\.ua.*",
+    @JsonProperty("link") @Length(max = 255) @NotNull @Pattern(regexp = "^$|^https://my.binotel.ua.*",
         message = "Link must start with 'https://my.binotel.ua'") String link) {
 }
