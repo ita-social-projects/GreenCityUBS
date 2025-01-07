14dfa9535dc9a69930510218aba35609a6776efe
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2025-01-07 13:33:15 +0200

    resolved issues

--- service/src/main/java/greencity/mapping/order/BigOrderTableDtoMapper.java
+++ service/src/main/java/greencity/mapping/order/BigOrderTableDtoMapper.java
@@ -14,6 +14,8 @@ import static java.util.Objects.nonNull;
 
 @Component
 public class BigOrderTableDtoMapper extends AbstractConverter<BigOrderTableViews, BigOrderTableDTO> {
+    private static final String DEFAULT_VALUE = "-";
+
     @Override
     protected BigOrderTableDTO convert(BigOrderTableViews bigViews) {
         return new BigOrderTableDTO()
@@ -39,20 +41,20 @@ public class BigOrderTableDtoMapper extends AbstractConverter<BigOrderTableViews
             .setMixedWaste120L(Optional.ofNullable(bigViews.getMixedWaste120())
                 .filter(value -> value != 0)
                 .map(String::valueOf)
-                .orElse("-"))
+                .orElse(DEFAULT_VALUE))
             .setTextileWaste60L(Optional.ofNullable(bigViews.getTextileWaste60())
                 .filter(value -> value != 0)
                 .map(String::valueOf)
-                .orElse("-"))
+                .orElse(DEFAULT_VALUE))
             .setTextileWaste20L(Optional.ofNullable(bigViews.getTextileWaste20())
                 .filter(value -> value != 0)
                 .map(String::valueOf)
-                .orElse("-"))
+                .orElse(DEFAULT_VALUE))
             .setOtherPackages(new OtherPackages(
                 Optional.ofNullable(bigViews.getOtherPackages())
-                    .orElse("-"),
+                    .orElse(DEFAULT_VALUE),
                 Optional.ofNullable(bigViews.getOtherPackagesEng())
-                    .orElse("-")))
+                    .orElse(DEFAULT_VALUE)))
             .setTotalOrderSum(convertCoinsIntoBills(bigViews.getTotalOrderSum()))
             .setOrderCertificateCode(bigViews.getOrderCertificateCode())
             .setGeneralDiscount(bigViews.getGeneralDiscount())
