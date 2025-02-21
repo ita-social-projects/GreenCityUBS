c6323c1599f10984dd5b3446062a812669aef198
Chernenko Vitaliy <chernenko_vitaliy@ukr.net>
2025-02-21 13:56:12 +0200

    Fixed swagger doc for method getOrdersCount in the ManagementOrderController;

--- core/src/main/java/greencity/controller/ManagementOrderController.java
+++ core/src/main/java/greencity/controller/ManagementOrderController.java
@@ -590,7 +590,7 @@ public class ManagementOrderController {
     @Operation(summary = "Returns the total number of orders.")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
-            content = @Content(array = @ArraySchema(schema = @Schema(implementation = Long.class)))),
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = OrderCountDto.class)))),
         @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED, content = @Content),
         @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN, content = @Content),
     })
