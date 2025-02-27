bce4fe00bd7c152de4ac6c5cca1ea394053f0c32
ChernenkoVitaliy <Chernenko_vitaliy@ukr.net>
2025-02-27 18:34:48 +0200

    Removed import wildcards, refactored tests

--- service-api/src/main/java/greencity/service/ubs/UBSManagementService.java
+++ service-api/src/main/java/greencity/service/ubs/UBSManagementService.java
@@ -1,9 +1,29 @@
 package greencity.service.ubs;
 
+import greencity.dto.address.UpdateAddressDto;
 import greencity.dto.bag.AdditionalBagInfoDto;
 import greencity.dto.certificate.CertificateDtoForSearching;
 import greencity.dto.employee.EmployeePositionDtoRequest;
-import greencity.dto.order.*;
+import greencity.dto.order.AdminCommentDto;
+import greencity.dto.order.BigOrderTableDTO;
+import greencity.dto.order.CounterOrderDetailsDto;
+import greencity.dto.order.OrderCountDto;
+import greencity.dto.order.DetailsOrderInfoDto;
+import greencity.dto.order.EcoNumberDto;
+import greencity.dto.order.ExportDetailsDto;
+import greencity.dto.order.ExportDetailsDtoUpdate;
+import greencity.dto.order.NotTakenOrderReasonDto;
+import greencity.dto.order.OrderAddressDtoResponse;
+import greencity.dto.order.OrderAddressExportDetailsDtoUpdate;
+import greencity.dto.order.OrderCancellationReasonDto;
+import greencity.dto.order.OrderDetailInfoDto;
+import greencity.dto.order.OrderDetailStatusDto;
+import greencity.dto.order.OrderDetailStatusRequestDto;
+import greencity.dto.order.OrderInfoDto;
+import greencity.dto.order.OrderStatusPageDto;
+import greencity.dto.order.ReadAddressByOrderDto;
+import greencity.dto.order.UpdateAllOrderPageDto;
+import greencity.dto.order.UpdateOrderPageAdminDto;
 import greencity.dto.pageble.PageableDto;
 import greencity.dto.user.AddingPointsToUserDto;
 import greencity.dto.violation.ViolationsInfoDto;
@@ -11,6 +31,7 @@ import greencity.entity.order.Order;
 import greencity.enums.SortingOrder;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -39,6 +60,27 @@ public interface UBSManagementService {
      * @author Nazar Struk
      */
     ViolationsInfoDto getAllUserViolations(String email);
+
+    /**
+     * Method that read user address by order id.
+     *
+     * @param orderId of {@link Long} order id;
+     * @return {@link ReadAddressByOrderDto} that contains one address;
+     * @author Mahdziak Orest
+     */
+    ReadAddressByOrderDto getAddressByOrderId(Long orderId);
+
+    /**
+     * Method that update address.
+     *
+     * @param dtoUpdate of {@link OrderAddressExportDetailsDtoUpdate} order id.
+     * @param order     {@link Order}.
+     * @param email     {@link String}.
+     * @return {@link OrderAddressDtoResponse} that contains address.
+     * @author Mahdziak Orest
+     */
+    Optional<OrderAddressDtoResponse> updateAddress(OrderAddressExportDetailsDtoUpdate dtoUpdate, Order order,
+        String email);
 
     /**
      * Method for getting order detail by language and order id.
@@ -95,6 +137,7 @@ public interface UBSManagementService {
      * Method that returns total number of orders.
      *
      * @return {@link Long}.
+     *
      * @author Chernenko Vitaliy
      */
     OrderCountDto getTotalNumberOfOrders();
@@ -158,6 +201,7 @@ public interface UBSManagementService {
      *
      * @param adminCommentDto {@link AdminCommentDto}.
      * @param email           {@link String}.
+     *
      * @author Yuriy Bahlay.
      */
     void saveAdminCommentToOrder(AdminCommentDto adminCommentDto, String email);
@@ -168,6 +212,7 @@ public interface UBSManagementService {
      * @param ecoNumberDto {@link EcoNumberDto}.
      * @param orderId      {@link Long}.
      * @param email        {@link String}.
+     *
      * @author Yuriy Bahlay.
      */
     void updateEcoNumberForOrderById(EcoNumberDto ecoNumberDto, Long orderId, String email);
@@ -178,6 +223,7 @@ public interface UBSManagementService {
      * @param ecoNumberDto {@link EcoNumberDto}.
      * @param order        {@link Order}.
      * @param email        {@link String}.
+     *
      * @author Yuriy Bahlay.
      */
     void updateEcoNumberForOrder(EcoNumberDto ecoNumberDto, Order order, String email);
@@ -190,6 +236,7 @@ public interface UBSManagementService {
      * @param language                {@link String}.
      * @param email                   {@link String}.
      * @param images                  {@link MultipartFile}.
+     *
      * @author Anton Bondar.
      */
     BigOrderTableDTO updateOrderAdminPageInfoAndSaveReason(Long orderId,
@@ -202,6 +249,7 @@ public interface UBSManagementService {
      * @param updateOrderPageAdminDto {@link UpdateOrderPageAdminDto}.
      * @param order                   {@link Order}.
      * @param email                   {@link String}.
+     *
      * @author Yuriy Bahlay.
      */
     void updateOrderAdminPageInfo(UpdateOrderPageAdminDto updateOrderPageAdminDto, Order order, String lang,
@@ -212,6 +260,7 @@ public interface UBSManagementService {
      *
      * @param updateAllOrderPageDto {@link UpdateOrderPageAdminDto}.
      * @param email                 {@link String} currentUser.
+     *
      * @author Max Boiarchuk.
      */
     void updateAllOrderAdminPageInfo(UpdateAllOrderPageDto updateAllOrderPageDto, String email, String lang);
@@ -221,6 +270,7 @@ public interface UBSManagementService {
      *
      * @param orderId {@link Long}.
      * @param email   {@link String}.
+     *
      * @author Hlazova Nataliia.
      */
     Boolean checkEmployeeForOrder(Long orderId, String email);
@@ -238,6 +288,7 @@ public interface UBSManagementService {
      *
      * @param orderId {@link Long}.
      * @return {@link OrderCancellationReasonDto}
+     *
      * @author Kharchenko Volodymyr.
      */
     OrderCancellationReasonDto getOrderCancellationReason(Long orderId);
@@ -247,6 +298,7 @@ public interface UBSManagementService {
      *
      * @param orderId {@link Long}.
      * @return {@link NotTakenOrderReasonDto}.
+     *
      * @author Kharchenko Volodymyr.
      */
     NotTakenOrderReasonDto getNotTakenOrderReason(Long orderId);
@@ -257,7 +309,21 @@ public interface UBSManagementService {
      *
      * @param orderId {@link Long} the ID of the order.
      * @return {@link Boolean}
+     *
      * @author Volodymyr Lukovskyi
      */
     Boolean checkIfOrderStatusIsFormedToCanceled(Long orderId);
+
+    /**
+     * Method updates order address. This method updates order address. It takes
+     * {@link UpdateAddressDto} as an argument and updates the order address in the
+     * database. It also checks if the address exists and if the user is authorized
+     * to update the address.
+     *
+     * @param addressDto {@link UpdateAddressDto}
+     * @param email      {@link String} the user's email
+     *
+     * @author Kizerov Dmytro
+     */
+    void addressUpdate(UpdateAddressDto addressDto, String email);
 }
