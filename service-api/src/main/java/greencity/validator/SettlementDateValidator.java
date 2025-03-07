61f256597c6308b25484fd262f3761204a0ce638
Andrii <Ineedmoreweekends@gmail.com>
2025-03-07 19:32:43 +0200

    Fixed bug where employee can select future payment date in edit popup, added new test case, and fixed old tests being dummies.

--- service-api/src/main/java/greencity/validator/SettlementDateValidator.java
+++ service-api/src/main/java/greencity/validator/SettlementDateValidator.java
@@ -5,12 +5,20 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SettlementDateValidator implements ConstraintValidator<ValidSettlementDate, String> {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return LocalDate.parse(s, formatter).isBefore(LocalDate.now().plusDays(1));
+        if (s == null) {
+            return false;
+        }
+        try {
+          return LocalDate.parse(s, formatter).isBefore(LocalDate.now().plusDays(1));
+        } catch (DateTimeParseException e) {
+            return false;
+        }
     }
 }
