03beaaff829392c905af0b995347292c55ac2526
Andrii <Ineedmoreweekends@gmail.com>
2025-03-10 14:48:44 +0200

    Renamed the variable to a more descriptive name

--- service-api/src/main/java/greencity/validator/SettlementDateValidator.java
+++ service-api/src/main/java/greencity/validator/SettlementDateValidator.java
@@ -11,12 +11,12 @@ public class SettlementDateValidator implements ConstraintValidator<ValidSettlem
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        if (s == null) {
+    public boolean isValid(String settlementDateToValidate, ConstraintValidatorContext constraintValidatorContext) {
+        if (settlementDateToValidate == null) {
             return false;
         }
         try {
-            return LocalDate.parse(s, formatter).isBefore(LocalDate.now().plusDays(1));
+            return LocalDate.parse(settlementDateToValidate, formatter).isBefore(LocalDate.now().plusDays(1));
         } catch (DateTimeParseException e) {
             return false;
         }
