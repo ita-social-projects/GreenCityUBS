24a7e2018d7635c7cf96bd557d1402b5598905f0
whatIsLove <vlad.galyara@gmail.com>
2025-03-12 12:43:52 +0200

    Add repository and add new changelog to changelog-master

---
+++ dao/src/main/java/greencity/repository/ChatFeedbackRepository.java
@@ -0,0 +1,9 @@
+package greencity.repository;
+
+import greencity.entity.telegram.ChatFeedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatFeedbackRepository extends JpaRepository<ChatFeedback,Long> {
+}
