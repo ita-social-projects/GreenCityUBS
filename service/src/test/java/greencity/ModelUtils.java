ad8807218b646875c07064c69e2d26f37af8ad79
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-03-16 23:43:43 +0200

    Added a mapper for EventDto

--- service/src/test/java/greencity/ModelUtils.java
+++ service/src/test/java/greencity/ModelUtils.java
@@ -5929,4 +5929,24 @@ public class ModelUtils {
             .amountBeforePayment(120d)
             .build();
     }
+
+    public static Event getEvent3() {
+        return Event.builder()
+                .eventDate(LocalDateTime.of(2025, 3, 12, 20, 20))
+                .eventNameEn("Event 3")
+                .eventNameUk("Івент 3")
+                .authorNameEn("Author 3")
+                .authorNameUk("Автор 3")
+                .id(1L)
+                .build();
+    }
+
+    public static EventDto getEventDto() {
+        return EventDto.builder()
+                .eventDate(LocalDateTime.of(2025, 3, 12, 20, 20))
+                .eventName("Івент 3")
+                .authorName("Автор 3")
+                .id(1L)
+                .build();
+    }
 }
