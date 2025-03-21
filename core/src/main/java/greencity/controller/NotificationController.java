9d0075e7c702e4d2ad26d4402033139a742c81fa
Maksym Kozak <bersercer70@gmail.com>
2025-03-21 20:41:12 +0200

    fix build failures

--- core/src/main/java/greencity/controller/NotificationController.java
+++ core/src/main/java/greencity/controller/NotificationController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Locale;
 
 @RestController
