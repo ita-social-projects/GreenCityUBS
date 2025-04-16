d93395ce67034ef2eb89ca018c79f2436c1d1c1d
Maksym Kozak <bersercer70@gmail.com>
2025-04-16 20:52:55 +0300

    remove spacing between imports

--- service-api/src/test/java/greencity/dto/user/UserProfileCreateDtoTest.java
+++ service-api/src/test/java/greencity/dto/user/UserProfileCreateDtoTest.java
@@ -8,7 +8,6 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.Set;
 import java.util.stream.Stream;
 
