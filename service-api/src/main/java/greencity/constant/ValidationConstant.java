a4400e919c094fc72e3071e180b665b8fe35cb78
Maksym Kozak <bersercer70@gmail.com>
2025-04-16 17:42:32 +0300

    simplify message about invalid username to be more user-friendly

--- service-api/src/main/java/greencity/constant/ValidationConstant.java
+++ service-api/src/main/java/greencity/constant/ValidationConstant.java
@@ -38,17 +38,13 @@ public class ValidationConstant {
             {1,30}\
             (?<![ЭэЁёъЪЫы])$\
             """;
-    public static final String USERNAME_MESSAGE =
-        """
-            The name ${validatedValue} cannot be empty, \
-            starts with a number or not a capital letter, \
-            ends with dot, \
-            contain 2 consecutive dots or dashes and symbols like @#$. \
-            Use English or Ukrainian letters, \
-            no longer than 30 symbols, \
-            the name ${validatedValue} could contain numbers, symbols '’, \
-            dot in the middle of the name, dash and whitespaces.\
-            """;
+    public static final String USERNAME_MESSAGE = """
+        Name must start with a capital letter, \
+        cannot end with dot \
+        or contain 2 consecutive dots, dashes and special symbols. \
+        Use English or Ukrainian letters, \
+        no longer than 30 symbols.\
+        """;
     public static final String PAYMENT_DATE_IS_BEFORE_ORDER_CREATION_MESSAGE =
         "Payment date should be set after the order creation.";
     public static final String PAYMENT_DATE_IS_AFTER_CURRENT_DATE_MESSAGE =
