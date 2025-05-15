a09cdb089c45b273a430251fce77bc6b3b1a8e98
whatIsLove <vlad.galyara@gmail.com>
2025-05-15 16:24:21 +0300

    Add @Profile annotation to TelegramBotConfig
    Restrict TelegramBotConfig to non-test profiles to prevent its initialization during tests. This ensures cleaner test environments and avoids unnecessary bot-related configurations in test scenarios.

--- service/src/main/java/greencity/config/TelegramBotConfig.java
+++ service/src/main/java/greencity/config/TelegramBotConfig.java
@@ -5,10 +5,12 @@ import greencity.ubstelegrambot.UBSTelegramBot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Profile("!test")
 @Configuration
 public class TelegramBotConfig {
     @Value("${greencity.bots.ubs-bot-name}")
