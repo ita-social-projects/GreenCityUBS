a58a307493ff63fb793e86b4bcd4ce3942eec3a5
whatIsLove <vlad.galyara@gmail.com>
2025-05-20 17:23:09 +0300

    Add missed security config

--- core/src/main/java/greencity/configuration/SecurityConfig.java
+++ core/src/main/java/greencity/configuration/SecurityConfig.java
@@ -274,6 +274,17 @@ public class SecurityConfig {
                     "/notifications/**")
                 .hasAnyRole(USER, ADMIN)
                 .requestMatchers(HttpMethod.GET,
+                    UBS_LINK + "/**",
+                    UBS_LINK + "/client/**",
+                    UBS_LINK + "/order/{id}/cancellation",
+                    UBS_LINK + "/certificate/{responseCode}",
+                    "/notifications",
+                    "/notifications/**",
+                    "/notifications/quantityUnreadenNotifications",
+                    UBS_LINK + "/check-if-tariff-exists/{id}",
+                    UBS_LINK + "/locations")
+                .hasAnyRole(USER, ADMIN, UBS_EMPLOYEE)
+                .requestMatchers(HttpMethod.GET,
                     UBS_LINK + "/telegram/**")
                 .hasRole(UBS_EMPLOYEE)
                 .requestMatchers(HttpMethod.POST,
