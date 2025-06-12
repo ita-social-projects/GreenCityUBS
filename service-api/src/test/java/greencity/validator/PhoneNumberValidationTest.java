86144dbd4058dc5beff49afa180cb9d7ffde7cbc
SaenkoVova <vova080520@gmail.com>
2025-06-12 18:34:43 +0300

    Bugfix #8392, #8394 removed unused imports

--- service-api/src/test/java/greencity/validator/PhoneNumberValidationTest.java
+++ service-api/src/test/java/greencity/validator/PhoneNumberValidationTest.java
@@ -1,15 +1,11 @@
 package greencity.validator;
 
-import greencity.constant.ErrorMessage;
-import greencity.exceptions.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import jakarta.validation.ConstraintValidatorContext;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
