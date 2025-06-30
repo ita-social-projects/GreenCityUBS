705ee0f9107c58865cb664ef2cb96007856f790b
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-06-30 12:40:39 +0000

    Deploying to gh-pages from  @ c7000cd1526ef9350a1dc5c5182b2cdde8ded72d 🚀

--- service-api/src/main/java/greencity/dto/user/UserProfileUpdateDto.java
+++ service-api/src/main/java/greencity/dto/user/UserProfileUpdateDto.java
@@ -27,9 +27,9 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class UserProfileUpdateDto implements Serializable {
     @NotBlank
-    @Pattern(regexp = ValidationConstant.NAME_REGEXP)
+    @Pattern(regexp = ValidationConstant.NAME_REGEXP, message = ValidationConstant.NAME_VALIDATION_MESSAGE)
     private String recipientName;
-    @Pattern(regexp = "^$|" + ValidationConstant.NAME_REGEXP)
+    @Pattern(regexp = "^$|" + ValidationConstant.NAME_REGEXP, message = ValidationConstant.NAME_VALIDATION_MESSAGE)
     private String recipientSurname;
     @Email(regexp = ValidationConstant.EMAIL_REGEXP)
     private String alternateEmail;
