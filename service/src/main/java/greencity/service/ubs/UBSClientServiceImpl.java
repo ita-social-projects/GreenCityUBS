d3fa107a8c07bdd42922d590b8ba7f7d701286ee
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-07-03 16:23:04 +0300

    Fixed issue where user was unable to pay with points for existing order

--- service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/UBSClientServiceImpl.java
@@ -1795,6 +1795,7 @@ public class UBSClientServiceImpl implements UBSClientService {
                 .amount(-pointsToUse)
                 .date(LocalDateTime.now())
                 .order(order)
+                .reason(BonusReason.DEBIT_PAYMENT)
                 .build());
 
         orderRepository.save(order);
