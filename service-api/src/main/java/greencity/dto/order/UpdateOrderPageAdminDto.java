a6ef33133fdbb55494f25c6938a8847e99a4e64f
BohdanLys <152965302+BohdanLys@users.noreply.github.com>
2025-07-16 15:08:02 +0300

    Bugfix #8390, #8391 Added missing validation for fields in UpdateOrderPageAdmin dto. (#1702)
    * Create custom annotation for UpdateOrderPageAdmin validation
    
    * Create validator
    
    * Added tests for UpdateOrderPageValidator
    
    * Added annotation to DTO
    
    * Remove spaces in code
    
    * Added additional tests for uncovered lines
    
    * Resolved issues from sonar
    
    * Executed plugins for sonar
    
    * Renamed parameterized test
    
    * Added regex for name and surname
    
    * Imported regex from ValidationConstant file
    
    * Added null check in custom validator

--- service-api/src/main/java/greencity/dto/order/UpdateOrderPageAdminDto.java
+++ service-api/src/main/java/greencity/dto/order/UpdateOrderPageAdminDto.java
@@ -1,5 +1,6 @@
 package greencity.dto.order;
 
+import greencity.annotations.ValidUpdateOrderPageAdmin;
 import greencity.dto.customer.UbsCustomersDtoUpdate;
 import greencity.dto.employee.UpdateResponsibleEmployeeDto;
 import greencity.dto.refund.RefundDto;
@@ -18,6 +19,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ValidUpdateOrderPageAdmin
 public class UpdateOrderPageAdminDto {
     private OrderDetailStatusRequestDto generalOrderInfo;
     @Valid
