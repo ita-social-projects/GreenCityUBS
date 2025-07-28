5ca09fb0f8a011d21ca55ec18a4067253e089361
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-07-28 15:29:32 +0300

    Fix NotificationTemplate retrieval logic

--- service/src/main/java/greencity/service/notification/NotificationServiceImpl.java
+++ service/src/main/java/greencity/service/notification/NotificationServiceImpl.java
@@ -965,9 +965,8 @@ public class NotificationServiceImpl implements NotificationService {
 
     private NotificationShortDto createNotificationShortDto(UserNotification notification, String language,
         Long monthsOfAccountInactivity) {
-        NotificationTemplate template = templateRepository
-            .findNotificationTemplateByNotificationTypeAndNotificationReceiverType(
-                notification.getNotificationType(), SITE)
+        NotificationTemplate template = templateRepository.findNotificationTemplateByIdAndNotificationReceiverType(
+            notification.getTemplateId(), SITE)
             .orElseThrow(() -> new NotFoundException("Template not found"));
 
         String templateBody = resolveTemplateBody(language, SITE, template);
