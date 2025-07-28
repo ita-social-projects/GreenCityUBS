6a5100b7d96f8728d78db98bb33cde1e11ac444e
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-07-28 13:19:23 +0300

    Properly format CustomExceptionHandler to satisfy build

--- core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
+++ core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
@@ -284,10 +284,10 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
      * Handles exceptions of type {@link ConstraintViolationException} thrown during
-     * validation of method parameters or path variables.
-     * Extracts detailed violation messages from the exception and sets them into a
-     * custom {@link ExceptionResponse} object. The response is sent with HTTP
-     * status 400 (Bad Request).
+     * validation of method parameters or path variables. Extracts detailed
+     * violation messages from the exception and sets them into a custom
+     * {@link ExceptionResponse} object. The response is sent with HTTP status 400
+     * (Bad Request).
      *
      * @param ex      the {@link ConstraintViolationException} containing validation
      *                errors
