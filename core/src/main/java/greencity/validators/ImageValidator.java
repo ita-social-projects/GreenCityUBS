0568e6e7e78ade306feec805d63ce57ddaaac043
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-07-28 11:23:51 +0300

    Enhance logic for MultipartFile validation to support single object as well as array of MultipartFiles

--- core/src/main/java/greencity/validators/ImageValidator.java
+++ core/src/main/java/greencity/validators/ImageValidator.java
@@ -5,14 +5,38 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.web.multipart.MultipartFile;
 
-public class ImageValidator implements ConstraintValidator<ImageValidation, MultipartFile> {
-    @Override
-    public boolean isValid(MultipartFile file, ConstraintValidatorContext constraintValidatorContext) {
-        if (file == null) {
+public class ImageValidator {
+    public static class Single implements ConstraintValidator<ImageValidation, MultipartFile> {
+        @Override
+        public boolean isValid(MultipartFile file, ConstraintValidatorContext constraintValidatorContext) {
+            if (file == null) {
+                return true;
+            }
+
+            String mimeType = file.getContentType();
+            return mimeType != null && mimeType.startsWith("image/");
+        }
+    }
+
+    public static class Array implements ConstraintValidator<ImageValidation, MultipartFile[]> {
+        @Override
+        public boolean isValid(MultipartFile[] files, ConstraintValidatorContext constraintValidatorContext) {
+            if (files == null) {
+                return true;
+            }
+
+            for (MultipartFile file : files) {
+                if  (file == null) {
+                    continue;
+                }
+
+                String mimeType = file.getContentType();
+                if (mimeType == null || !mimeType.startsWith("image/")) {
+                    return false;
+                }
+            }
+
             return true;
         }
-
-        String mimeType = file.getContentType();
-        return mimeType != null && mimeType.startsWith("image/");
     }
 }
