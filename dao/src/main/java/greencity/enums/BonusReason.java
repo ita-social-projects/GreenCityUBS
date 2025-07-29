a9a6007f6843d402c4b6980aa5f051b4a4c8cee3
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-07-29 12:12:42 +0300

    Edited bonus reason for payment expiry for better representation

--- dao/src/main/java/greencity/enums/BonusReason.java
+++ dao/src/main/java/greencity/enums/BonusReason.java
@@ -9,7 +9,7 @@ public enum BonusReason {
     RETURN_OVERPAY("Повернення переплати за замовлення", "Refund of overpayment for the order"),
     REFUND_CANCELED_ORDER("Зарахування оплати скасованого замовлення", "Enrollment of payment for canceled order"),
     DEBIT_PAYMENT("Списання у рахунок оплати замовлення", "Write-off of the payment of the order"),
-    RETURN_UNPAID_ORDER("Повернення з неуспішної оплати", "Refund from unsuccessful payment");
+    RETURN_UNPAID_ORDER("Повернення зі скасованої оплати", "Refund from canceled payment");
 
     private final String descriptionUk;
     private final String descriptionEn;
