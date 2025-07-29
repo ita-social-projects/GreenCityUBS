53543fdd8d4df6d090d1f62c75a3b4d6a59493b6
Anton Pampukha <antonpampuha4@gmail.com>
2025-07-29 16:57:58 +0300

    add docs to methods in some TelegramServices

--- service-api/src/main/java/greencity/service/ubs/TelegramGreenOfficeService.java
+++ service-api/src/main/java/greencity/service/ubs/TelegramGreenOfficeService.java
@@ -4,5 +4,12 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 public interface TelegramGreenOfficeService {
+    /**
+     * Processes the user's email input for a Green Office request via Telegram.
+     *
+     * @param message the Telegram {@link Message}  containing the email address from the user
+     *
+     * @return a {@link SendMessage}  containing either an error or a thank-you response
+     */
     SendMessage processGreenOfficeEmail(Message message);
 }
--- service/src/main/java/greencity/ubstelegrambot/service/TelegramFeedbackServiceImpl.java
+++ service/src/main/java/greencity/ubstelegrambot/service/TelegramFeedbackServiceImpl.java
@@ -26,6 +26,10 @@ public class TelegramFeedbackServiceImpl implements TelegramFeedbackService {
     private final TelegramChatRepository telegramChatRepository;
     private final ChatFeedbackRepository chatFeedbackRepository;
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public SendMessage processInputCommentRequest(Message message) {
         Optional<TelegramChat> telegramChat = telegramChatRepository.findByChatId(message.getChatId().toString());
@@ -52,6 +56,9 @@ public class TelegramFeedbackServiceImpl implements TelegramFeedbackService {
         return MessageFactory.createFeedbackThanksMessage(message.getChatId().toString());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public SendMessage processRatingFeedbackRequest(String chatId, int rating) {
         Optional<TelegramChat> chat = telegramChatRepository.findByChatId(chatId);
@@ -88,7 +95,6 @@ public class TelegramFeedbackServiceImpl implements TelegramFeedbackService {
     }
 
     /**
-     *
      * {@inheritDoc}
      */
     @Override
@@ -112,7 +118,6 @@ public class TelegramFeedbackServiceImpl implements TelegramFeedbackService {
     }
 
     /**
-     *
      * {@inheritDoc}
      */
     @Override
