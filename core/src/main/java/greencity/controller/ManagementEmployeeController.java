72f31d5549f237005fefdde75c0657674857f4c1
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-07-30 14:45:50 +0300

    Add implemented validation to EmployeeWithTariffsDto

--- core/src/main/java/greencity/controller/ManagementEmployeeController.java
+++ core/src/main/java/greencity/controller/ManagementEmployeeController.java
@@ -52,8 +52,8 @@ public class ManagementEmployeeController {
     @ApiResponses(value = {
         @ApiResponse(responseCode = "201", description = HttpStatuses.CREATED,
             content = @Content(schema = @Schema(implementation = EmployeeWithTariffsDto.class))),
-        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED, content = @Content),
         @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST, content = @Content),
+        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED, content = @Content),
         @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN, content = @Content),
         @ApiResponse(responseCode = "422", description = HttpStatuses.UNPROCESSABLE_ENTITY, content = @Content)
     })
