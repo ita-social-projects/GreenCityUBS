11997ce5c46ff88274268777878f1d951c122c56
markotymofeiuk <146076997+markotymofeiuk@users.noreply.github.com>
2025-08-06 13:50:09 +0000

    Deploying to gh-pages from  @ 07cc59af1d34584cb69d2002c1810c34f7c371dc 🚀

--- service/src/main/java/greencity/service/notification/NotificationServiceImpl.java
+++ service/src/main/java/greencity/service/notification/NotificationServiceImpl.java
@@ -965,10 +965,7 @@ public class NotificationServiceImpl implements NotificationService {
 
     private NotificationShortDto createNotificationShortDto(UserNotification notification, String language,
         Long monthsOfAccountInactivity) {
-        NotificationTemplate template = templateRepository
-            .findNotificationTemplateByNotificationTypeAndNotificationReceiverType(
-                notification.getNotificationType(), SITE)
-            .orElseThrow(() -> new NotFoundException("Template not found"));
+        NotificationTemplate template = getNotificationTemplate(notification, SITE, templateRepository);
 
         String templateBody = resolveTemplateBody(language, SITE, template);
         if (notification.getParameters() == null) {
