651210d94e5c5c28e3fcdf81a8f4b52c8a7fdcbd
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2025-08-06 09:32:44 +0300

    Fix telegram bot: handle invalid password error on manager login

--- service/src/main/java/greencity/constant/TelegramBotConstants.java
+++ service/src/main/java/greencity/constant/TelegramBotConstants.java
@@ -178,4 +178,6 @@ public class TelegramBotConstants {
 
     public static final String PHOTO_CONTENT =
         "Фото контент";
+
+    public static final String INCORRECT_PASSWORD = "Неправильний пароль. Спробуйте ще раз";
 }
