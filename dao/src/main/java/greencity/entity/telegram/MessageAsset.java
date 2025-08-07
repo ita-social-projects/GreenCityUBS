01557ba29552016fa7c7b22524191a895feea8ec
Anton Pampukha <antonpampuha4@gmail.com>
2025-08-07 14:46:29 +0300

    update type of the fields from LocalDateTime to Instant

--- dao/src/main/java/greencity/entity/telegram/MessageAsset.java
+++ dao/src/main/java/greencity/entity/telegram/MessageAsset.java
@@ -1,7 +1,16 @@
 package greencity.entity.telegram;
 
 import greencity.enums.AssetType;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +19,8 @@ import org.hibernate.validator.constraints.URL;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import java.time.LocalDateTime;
+
+import java.time.Instant;
 
 @Data
 @Entity
@@ -39,10 +49,10 @@ public class MessageAsset {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @LastModifiedDate
-    private LocalDateTime updatedAt;
+    private Instant updatedAt;
 
     @ManyToOne
     @JoinColumn(name = "message_id", nullable = false)
