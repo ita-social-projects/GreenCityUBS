c5c4868c6943886dc797d5a77eba74ca91961d64
Anton Pampukha <antonpampuha4@gmail.com>
2025-08-08 13:16:42 +0300

    fix formatting

--- dao/src/main/java/greencity/entity/telegram/MessageAsset.java
+++ dao/src/main/java/greencity/entity/telegram/MessageAsset.java
@@ -19,7 +19,6 @@ import org.hibernate.validator.constraints.URL;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.Instant;
 
 @Data
--- dao/src/main/java/greencity/entity/telegram/TelegramMessage.java
+++ dao/src/main/java/greencity/entity/telegram/TelegramMessage.java
@@ -22,7 +22,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.Instant;
 import java.util.List;
 
