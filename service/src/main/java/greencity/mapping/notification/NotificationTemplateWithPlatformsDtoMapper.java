d71cc84b51aa7ce9b077a694b081e3b7c22cf933
Олег Попович <146092129+olehio-p@users.noreply.github.com>
2025-08-11 22:51:04 +0300

    8961 api admin notifications notifications same field is different on different endpoints (#1769)
    * Add new helper util method to map notifications
    
    * Add test class to test new helper class NotificationMappers
    
    * Modify existing mapper to use new functionality of helper class
    
    * Add proper documentation for new helper class
    
    * Refactor to use static helper class methods

--- service/src/main/java/greencity/mapping/notification/NotificationTemplateWithPlatformsDtoMapper.java
+++ service/src/main/java/greencity/mapping/notification/NotificationTemplateWithPlatformsDtoMapper.java
@@ -1,13 +1,9 @@
 package greencity.mapping.notification;
 
-import greencity.dto.notification.NotificationPlatformDto;
-import greencity.dto.notification.NotificationTemplateMainInfoDto;
 import greencity.dto.notification.NotificationTemplateWithPlatformsDto;
 import greencity.entity.notifications.NotificationTemplate;
-import java.util.Objects;
 import org.modelmapper.AbstractConverter;
 import org.springframework.stereotype.Component;
-import java.util.stream.Collectors;
 
 @Component
 class NotificationTemplateWithPlatformsDtoMapper
@@ -15,37 +11,11 @@ class NotificationTemplateWithPlatformsDtoMapper
     @Override
     protected NotificationTemplateWithPlatformsDto convert(NotificationTemplate notificationTemplate) {
         return NotificationTemplateWithPlatformsDto.builder()
-            .notificationTemplateMainInfoDto(NotificationTemplateMainInfoDto.builder()
-                .type(notificationTemplate.getNotificationType())
-                .trigger(notificationTemplate.getTrigger())
-                .triggerDescriptionUk(notificationTemplate.getTrigger().getDescriptionUk())
-                .triggerDescriptionEn(notificationTemplate.getTrigger().getDescriptionEn())
-                .time(notificationTemplate.getTime())
-                .timeDescriptionUk(notificationTemplate.getTime().getDescriptionUk())
-                .timeDescriptionEn(notificationTemplate.getTime().getDescriptionEn())
-                .schedule(notificationTemplate.getSchedule())
-                .titleUk(notificationTemplate.getTitleUk())
-                .titleEn(notificationTemplate.getTitleEn())
-                .notificationStatus(notificationTemplate.getNotificationStatus())
-                .scheduleUpdateForbidden(notificationTemplate.isScheduleUpdateForbidden())
-                .userCategoryDescriptionUk(
-                    Objects.isNull(notificationTemplate.getUserCategory()) ? null
-                        : notificationTemplate.getUserCategory().getDescriptionUk())
-                .userCategoryDescriptionEn(Objects.isNull(notificationTemplate.getUserCategory()) ? null
-                    : notificationTemplate.getUserCategory().getDescriptionEn())
-                .build())
+            .notificationTemplateMainInfoDto(
+                NotificationMappers.toNotificationTemplateMainInfoDto(notificationTemplate))
             .platforms(notificationTemplate.getNotificationPlatforms().stream()
-                .map(platform -> NotificationPlatformDto.builder()
-                    .id(platform.getId())
-                    .receiverType(platform.getNotificationReceiverType())
-                    .nameEn(platform
-                        .getNotificationReceiverType()
-                        .getName())
-                    .bodyUk(platform.getBodyUk())
-                    .bodyEn(platform.getBodyEn())
-                    .status(platform.getNotificationStatus())
-                    .build())
-                .collect(Collectors.toList()))
+                .map(NotificationMappers::toNotificationPlatformDto)
+                .toList())
             .build();
     }
 }
