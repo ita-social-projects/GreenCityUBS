780976f21808ac6f661269e052bc834be88f015e
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2025-08-12 10:09:00 +0300

    Remove TelegramConstants class

--- service/src/test/java/greencity/ubstelegrambot/service/TelegramNotificationServiceTest.java
+++ service/src/test/java/greencity/ubstelegrambot/service/TelegramNotificationServiceTest.java
@@ -18,10 +18,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.time.Instant;
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
