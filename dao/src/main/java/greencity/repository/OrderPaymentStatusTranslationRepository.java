84c804c2c8a295262348a8259f826c2230ed8946
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-08-13 15:02:59 +0300

    Fixed style issues

--- dao/src/main/java/greencity/repository/OrderPaymentStatusTranslationRepository.java
+++ dao/src/main/java/greencity/repository/OrderPaymentStatusTranslationRepository.java
@@ -4,7 +4,6 @@ import greencity.entity.order.OrderPaymentStatusTranslation;
 import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.QueryHints;
-
 import java.util.List;
 import java.util.Optional;
 
