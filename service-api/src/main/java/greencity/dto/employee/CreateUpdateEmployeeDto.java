bca1016bbcb4d814f0171f93a7269a3add8568c7
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-08-19 09:43:03 +0300

    Add new DTO CreateUpdateEmployeeDto where positions of employee is represented as a list of ids not the full dto object

---
+++ service-api/src/main/java/greencity/dto/employee/CreateUpdateEmployeeDto.java
@@ -0,0 +1,38 @@
+package greencity.dto.employee;
+
+import greencity.annotations.ValidPhoneNumber;
+import greencity.constant.ValidationConstant;
+import greencity.enums.EmployeeStatus;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUpdateEmployeeDto {
+    private Long id;
+    @NotNull
+    @Pattern(regexp = ValidationConstant.NAME_REGEXP, message = ValidationConstant.NAME_VALIDATION_MESSAGE)
+    private String firstName;
+    @NotNull
+    @Pattern(regexp = ValidationConstant.NAME_REGEXP, message = ValidationConstant.NAME_VALIDATION_MESSAGE)
+    private String lastName;
+    @NotNull
+    @ValidPhoneNumber
+    private String phoneNumber;
+    @NotNull
+    @Email(regexp = ValidationConstant.EMAIL_REGEXP)
+    private String email;
+    private EmployeeStatus employeeStatus;
+    private String image;
+    @NotEmpty
+    private List<Long> employeePositionIds;
+}
