c4a03da4296f02b89d7f0b9efef268160f5d1a2e
antonpampukha <139760146+antonpampukha@users.noreply.github.com>
2025-08-22 15:48:21 +0000

    Deploying to gh-pages from  @ e9cc782b991bfdcedb10d19d2b2d3a1d95f34319 🚀

--- dao/src/main/java/greencity/specification/ChatSpecifications.java
+++ dao/src/main/java/greencity/specification/ChatSpecifications.java
@@ -11,17 +11,27 @@ import org.springframework.data.jpa.domain.Specification;
 @UtilityClass
 public class ChatSpecifications {
     public static Specification<TelegramChat> hasNameLike(String searchTerm) {
-        return (root, query, criteriaBuilder) -> {
+        return (root, query, cb) -> {
             if (searchTerm == null || searchTerm.trim().isEmpty()) {
-                return criteriaBuilder.conjunction();
+                return cb.conjunction();
             }
 
             String pattern = "%" + searchTerm.toLowerCase() + "%";
 
-            return criteriaBuilder.or(
-                criteriaBuilder.like(criteriaBuilder.lower(root.get("firstName")), pattern),
-                criteriaBuilder.like(criteriaBuilder.lower(root.get("lastName")), pattern),
-                criteriaBuilder.like(criteriaBuilder.lower(root.get("username")), pattern));
+            Expression<String> chatId = cb.lower(root.get("chatId"));
+            Expression<String> firstName = cb.lower(cb.coalesce(root.get("firstName"), ""));
+            Expression<String> lastName = cb.lower(cb.coalesce(root.get("lastName"), ""));
+            Expression<String> username = cb.lower(cb.coalesce(root.get("username"), ""));
+            Expression<String> fullNameFirstAndLast = cb.concat(cb.concat(firstName, " "), lastName);
+            Expression<String> fullNameLastAndFirst = cb.concat(cb.concat(lastName, " "), firstName);
+
+            return cb.or(
+                cb.like(chatId, pattern),
+                cb.like(firstName, pattern),
+                cb.like(lastName, pattern),
+                cb.like(username, pattern),
+                cb.like(fullNameFirstAndLast, pattern),
+                cb.like(fullNameLastAndFirst, pattern));
         };
     }
 
