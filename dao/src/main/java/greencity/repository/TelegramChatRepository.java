d5d8534aeebb2bce2679accd929d174722deffec
Anton Pampukha <antonpampuha4@gmail.com>
2025-09-02 08:52:04 +0300

    add docs

--- dao/src/main/java/greencity/repository/TelegramChatRepository.java
+++ dao/src/main/java/greencity/repository/TelegramChatRepository.java
@@ -54,5 +54,11 @@ public interface TelegramChatRepository
     @EntityGraph(attributePaths = "lastMessage")
     Page<TelegramChat> findAll(Specification<TelegramChat> spec, Pageable pageable);
 
+    /**
+     * The method finds telegram bot by userId.
+     *
+     * @param userId {@link Long}.
+     * @return {@link TelegramChat}.
+     */
     Optional<TelegramChat> findByUserId(Long userId);
 }
