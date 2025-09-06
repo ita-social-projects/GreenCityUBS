8de2cf0481b679b2491fe09b5ef45a1a78b6cf96
BohdanLys <mrboba04@gmail.com>
2025-09-06 14:20:17 +0300

    Run formatter

--- core/src/test/java/greencity/repository/BigOrderTableRepositoryTest.java
+++ core/src/test/java/greencity/repository/BigOrderTableRepositoryTest.java
@@ -42,7 +42,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
     "DOMAIN_NAME=http://testdomain/",
     "WAY_FOR_PAY_RETURN_URL=testUrl",
     "WAY_FOR_PAY_REDIRECT=testRedirectUrl"
-    })
+})
 class BigOrderTableRepositoryTest extends IntegrationTestBase {
     @MockBean
     private UBSTelegramBot telegramBot;
