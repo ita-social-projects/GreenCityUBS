b56cbcf4f3e55c867f759f6b6f026cdfaebdb708
Anton Pampukha <antonpampuha4@gmail.com>
2025-09-10 17:34:46 +0300

    fix formatting

--- service/src/main/java/greencity/ubstelegrambot/service/TelegramUtils.java
+++ service/src/main/java/greencity/ubstelegrambot/service/TelegramUtils.java
@@ -51,7 +51,8 @@ public class TelegramUtils {
      * Detects the asset type based on the given content type string.
      *
      * <ul>
-     * <li>If {@code fileType} is {@code null}, throws {@link UnsupportedTelegramAssetException}.</li>
+     * <li>If {@code fileType} is {@code null}, throws
+     * {@link UnsupportedTelegramAssetException}.</li>
      * <li>If content type starts with {@code image/webp} or
      * {@code application/x-tgsticker}, returns {@link AssetType#STICKER}.</li>
      * <li>If content type starts with {@code image/} (excluding SVG), returns
