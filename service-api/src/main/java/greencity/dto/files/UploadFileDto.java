fd1aa870b14c8ad47446213083301eeba0dbe0dd
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-09-16 22:29:16 +0300

    Implement UploadFileDto

---
+++ service-api/src/main/java/greencity/dto/files/UploadFileDto.java
@@ -0,0 +1,18 @@
+package greencity.dto.files;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadFileDto {
+    private MultipartFile file;
+    private String owner;
+}
