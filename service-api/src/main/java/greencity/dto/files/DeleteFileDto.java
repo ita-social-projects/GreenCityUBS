07ea2ca2dbe059ba605f41a45b74c3c31f377546
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-09-16 22:29:40 +0300

    Implement DeleteFileDto

---
+++ service-api/src/main/java/greencity/dto/files/DeleteFileDto.java
@@ -0,0 +1,17 @@
+package greencity.dto.files;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteFileDto {
+    private String path;
+    private String owner;
+}
