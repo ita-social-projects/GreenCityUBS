6a335e9787da20015552c3a7b4e169b143747101
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-09-16 22:51:14 +0300

    Create FileService interface as an abstraction for file operations

---
+++ service-api/src/main/java/greencity/service/files/FileService.java
@@ -0,0 +1,25 @@
+package greencity.service.files;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+    /**
+     * Method for uploading a file.
+     *
+     * @param file file to save.
+     * @return url of the saved file.
+     */
+    String uploadFile(MultipartFile file);
+
+    /**
+     * Method for deleting a file.
+     *
+     * @param path url of the file to delete.
+     */
+    void deleteFile(String path);
+
+    /**
+     * Method for scheduled Azure Blob Storage cleanup.
+     */
+    void cleanUp();
+}
