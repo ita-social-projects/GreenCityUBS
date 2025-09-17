d6ae4094f671e4242263224eefb1ee4c8d769f1d
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-09-17 17:03:19 +0300

    Enhance endpoint documentation to properly display response body

--- core/src/main/java/greencity/controller/ManagementNotificationController.java
+++ core/src/main/java/greencity/controller/ManagementNotificationController.java
@@ -11,6 +11,7 @@ import greencity.dto.pageble.PageableDto;
 import greencity.service.notification.NotificationTemplateService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -168,7 +169,7 @@ public class ManagementNotificationController {
     @Operation(summary = "Get all user categories")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
-            content = @Content(schema = @Schema(implementation = UserCategoryDto.class))),
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserCategoryDto.class)))),
         @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED, content = @Content),
         @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN, content = @Content)
     })
