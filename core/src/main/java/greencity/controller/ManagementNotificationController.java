d5875a8d883c3fa0cf6c88bfc598aef7c4cbc95f
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-09-17 13:40:40 +0300

    Add documentation for the endpoint

--- core/src/main/java/greencity/controller/ManagementNotificationController.java
+++ core/src/main/java/greencity/controller/ManagementNotificationController.java
@@ -12,6 +12,7 @@ import greencity.service.notification.NotificationTemplateService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.Positive;
@@ -166,7 +167,8 @@ public class ManagementNotificationController {
      */
     @Operation(summary = "Get all user categories")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = HttpStatuses.OK, content = @Content),
+        @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
+            content = @Content(schema = @Schema(implementation = UserCategoryDto.class))),
         @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED, content = @Content),
         @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN, content = @Content)
     })
