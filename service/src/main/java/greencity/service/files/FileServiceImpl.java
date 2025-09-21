11fe66bf6313dae5abac822a2f73b2cd4e74039c
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-09-21 23:04:37 +0300

    Fix injected bean type

--- service/src/main/java/greencity/service/files/FileServiceImpl.java
+++ service/src/main/java/greencity/service/files/FileServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
-    private final FileStorageFacadeImpl fileStorageFacade;
+    private final FileStorageFacade fileStorageFacade;
     private final UserRemoteWebClient userRemoteWebClient;
     @Value("UBS")
     private String owner;
