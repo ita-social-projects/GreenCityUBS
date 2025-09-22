a08b4b573578ba85a599badf18f6b747275b0b3c
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-09-22 14:14:59 +0300

    Test FileStorageFacade

---
+++ service/src/test/java/greencity/service/files/FileStorageFacadeTest.java
@@ -0,0 +1,53 @@
+package greencity.service.files;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import greencity.repository.EmployeeRepository;
+import greencity.repository.MessageAssetRepository;
+import greencity.repository.OrderRepository;
+import greencity.repository.PaymentRepository;
+import greencity.repository.ViolationRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class FileStorageFacadeTest {
+    @Mock
+    private PaymentRepository paymentRepository;
+    @Mock
+    private ViolationRepository violationRepository;
+    @Mock
+    private EmployeeRepository employeeRepository;
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private MessageAssetRepository messageAssetRepository;
+    @InjectMocks
+    private FileStorageFacade fileStorageFacade;
+
+    @Test
+    void getFilePathsTest(){
+        when(paymentRepository.findDistinctImagePaths()).thenReturn(List.of("payment1.png", "payment2.png"));
+        when(violationRepository.findDistinctImagePaths()).thenReturn(List.of("violation1.png", "violation1.png"));
+        when(employeeRepository.findDistinctImagePaths()).thenReturn(Arrays.asList("employee1.png", "employee2.png"));
+        when(orderRepository.findImagePaths()).thenReturn(Arrays.asList(
+                List.of("order1.png", "order2.png"),
+                null,
+                List.of("order2.png")));
+        when(messageAssetRepository.findDistinctImagePaths()).thenReturn(List.of("message1.png", "message1.png"));
+
+        List<String> filePaths = fileStorageFacade.getFilePaths();
+
+        assertThat(filePaths).containsExactlyInAnyOrder("payment1.png", "payment2.png",
+                "violation1.png", "employee1.png", "employee2.png",
+                "order1.png", "order2.png", "message1.png");
+    }
+}
