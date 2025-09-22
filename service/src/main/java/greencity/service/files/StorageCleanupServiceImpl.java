5ebba3fceab63bc516ff25d5d8ff31931a35c366
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-09-22 16:17:53 +0300

    Add logging to StorageCleanupServiceImpl

--- service/src/main/java/greencity/service/files/StorageCleanupServiceImpl.java
+++ service/src/main/java/greencity/service/files/StorageCleanupServiceImpl.java
@@ -3,11 +3,13 @@ package greencity.service.files;
 import greencity.client.config.UserRemoteWebClient;
 import greencity.dto.files.CleanupFilesDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class StorageCleanupServiceImpl implements StorageCleanupService {
     private final FileStorageFacade fileStorageFacade;
@@ -23,6 +25,7 @@ public class StorageCleanupServiceImpl implements StorageCleanupService {
             .owner(owner)
             .build();
 
+        log.info("Starting storage cleanup with {} files to process", filePaths.size());
         userRemoteWebClient.cleanUp(cleanupFilesDto);
     }
 }
