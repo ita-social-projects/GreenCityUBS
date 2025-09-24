0b318e535f8ebc625200de3da6af2437c96ba7bf
Olexandr <Mega.alex.alex2000@gmail.com>
2025-09-24 18:33:02 +0300

    fix after codeRabbit

--- core/src/test/java/greencity/IntegrationTestBase.java
+++ core/src/test/java/greencity/IntegrationTestBase.java
@@ -1,11 +1,11 @@
 package greencity;
 
 import greencity.initializer.PostgresInitializer;
-import jakarta.transaction.Transactional;
 import java.util.TimeZone;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
 @ContextConfiguration(initializers = {
