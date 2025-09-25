15318b6ef6790fc6218123e0a559d667f60fd6c5
Olexandr <Mega.alex.alex2000@gmail.com>
2025-09-25 14:19:05 +0300

    9043/add Test for PointUtil

--- service/src/main/java/greencity/util/PointsUtils.java
+++ service/src/main/java/greencity/util/PointsUtils.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PointsUtils {
-    //TODO add docs
-    //TODO add test
+    /**
+     * Checks if a user has enough points to perform an action.
+     *
+     * @param userPoints the current number of points the user has
+     * @param requiredPoints the number of points required to perform the action
+     * @throws BadRequestException if the user does not have enough points
+     */
     public void checkIfUserHaveEnoughPoints(Integer userPoints, Integer requiredPoints) {
         if (userPoints < requiredPoints) {
             throw new BadRequestException("User doesn't have enough points");
