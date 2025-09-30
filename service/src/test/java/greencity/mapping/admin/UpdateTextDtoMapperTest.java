365ff2016c3e4ea27a9cbd54279932b8eb5fac50
BohdanLys <mrboba04@gmail.com>
2025-09-30 12:05:35 +0300

    Added test case for UpdateTextDtoMapper

---
+++ service/src/test/java/greencity/mapping/admin/UpdateTextDtoMapperTest.java
@@ -0,0 +1,35 @@
+package greencity.mapping.admin;
+
+import greencity.dto.admin.UpdateSectionTextsDto;
+import greencity.entity.admin.SettingsText;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateTextDtoMapperTest {
+    @InjectMocks
+    UpdateTextDtoMapper updateTextDtoMapper;
+
+    @Test
+    void convertTest() {
+        UpdateSectionTextsDto dto = UpdateSectionTextsDto.builder()
+            .field("field")
+            .valueEN("valueEN")
+            .valueUK("valueUK")
+            .build();
+        SettingsText expected = SettingsText.builder()
+            .field("field")
+            .valueEN("valueEN")
+            .valueUK("valueUK")
+            .build();
+        SettingsText actual = updateTextDtoMapper.convert(dto);
+
+        assertEquals(expected.getField(), actual.getField());
+        assertEquals(expected.getValueEN(), actual.getValueEN());
+        assertEquals(expected.getValueUK(), actual.getValueUK());
+    }
+}
