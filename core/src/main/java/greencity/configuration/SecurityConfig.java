2b5d120537f0dc7c92a9fc315713dc9d62ddf6ed
BohdanLys <152965302+BohdanLys@users.noreply.github.com>
2025-10-01 14:18:09 +0000

    Deploying to gh-pages from  @ 14d3bf4c0870dbb3be63669713b27cbb0f61cf16 🚀

--- core/src/main/java/greencity/configuration/SecurityConfig.java
+++ core/src/main/java/greencity/configuration/SecurityConfig.java
@@ -150,6 +150,7 @@ public class SecurityConfig {
                     SUPER_ADMIN_LINK + "/{tariffId}/getService",
                     SUPER_ADMIN_LINK + "/getTariffLimits/{tariffId}",
                     SUPER_ADMIN_LINK + "/**",
+                    SUPER_ADMIN_LINK + "/settingsText",
                     USER_AGREEMENT_LINK,
                     USER_AGREEMENT_LINK + "/{id}",
                     UBS_MANAG_LINK + "/locations-details",
@@ -196,7 +197,8 @@ public class SecurityConfig {
                     SUPER_ADMIN_LINK + "/setTariffLimits/{tariffId}",
                     SUPER_ADMIN_LINK + "/editTariffInfo/{id}",
                     SUPER_ADMIN_LINK + "/activate-employee/{id}",
-                    SUPER_ADMIN_LINK + "/**")
+                    SUPER_ADMIN_LINK + "/**",
+                    SUPER_ADMIN_LINK + "/settingsText/section")
                 .hasAnyRole(ADMIN, UBS_EMPLOYEE)
                 .requestMatchers(HttpMethod.DELETE,
                     ADMIN_EMPL_LINK + "/**",
