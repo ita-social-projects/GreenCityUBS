b654ea4c9bb9e59cdbd323ec5209f7c43bb2c81d
markotymofeiuk <146076997+markotymofeiuk@users.noreply.github.com>
2025-10-10 22:24:30 +0300

    Bugfix #9170 - Fix certificate deletion in case of order cancelation (#1847)
    * Remove cascade deletion of certificates in case of order cancelation
    
    * Add BonusReason enum for draft order cancelation
    
    * Fix draft order deletion flow
    
    * Expand OrderServiceImplTest to cover draft order deletion flow

--- dao/src/main/java/greencity/enums/BonusReason.java
+++ dao/src/main/java/greencity/enums/BonusReason.java
@@ -9,7 +9,8 @@ public enum BonusReason {
     RETURN_OVERPAY("Повернення переплати за замовлення", "Refund of overpayment for the order"),
     REFUND_CANCELED_ORDER("Зарахування оплати скасованого замовлення", "Enrollment of payment for canceled order"),
     DEBIT_PAYMENT("Списання у рахунок оплати замовлення", "Write-off of the payment of the order"),
-    RETURN_UNPAID_ORDER("Повернення зі скасованої оплати", "Refund from canceled payment");
+    RETURN_UNPAID_ORDER("Повернення зі скасованої оплати", "Refund from canceled payment"),
+    RETURN_CANCELED_DRAFT_ORDER("Повернення зі скасованого замовлення", "Refund from canceled order");
 
     private final String descriptionUk;
     private final String descriptionEn;
