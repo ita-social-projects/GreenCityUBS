baae31c75c7aec89442cafce3ac173e5c5908b12
BohdanLys <mrboba04@gmail.com>
2025-10-21 17:59:15 +0300

    Fixed coderabbit comments

--- dao/src/main/java/greencity/repository/TariffsInfoRepository.java
+++ dao/src/main/java/greencity/repository/TariffsInfoRepository.java
@@ -1,8 +1,6 @@
 package greencity.repository;
 
 import greencity.entity.order.TariffsInfo;
-import greencity.enums.TariffStatus;
-import org.hibernate.annotations.processing.SQL;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
