63a10387a98cde14924b9f20b8abda880f1134fb
BohdanLys <mrboba04@gmail.com>
2025-10-21 18:07:43 +0300

    Fixed issues with migration and set appropriate ids

--- service-api/src/main/java/greencity/dto/tariff/GetActiveTariffInfoDto.java
+++ service-api/src/main/java/greencity/dto/tariff/GetActiveTariffInfoDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class GetActiveTariffInfoDto {
-    Long id;
+    private Long id;
     private String tariffNameUk;
     private String tariffNameEn;
     private String descriptionMessageUk;
