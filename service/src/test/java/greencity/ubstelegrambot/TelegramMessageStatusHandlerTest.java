1651a756fefa21dded4b62c596850abd4373d0f3
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-10-30 15:28:35 +0200

    Unify test import style

--- service/src/test/java/greencity/ubstelegrambot/TelegramMessageStatusHandlerTest.java
+++ service/src/test/java/greencity/ubstelegrambot/TelegramMessageStatusHandlerTest.java
@@ -1,5 +1,8 @@
 package greencity.ubstelegrambot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import greencity.dto.telegram.MarkMessagesAsReadRequestDto;
 import greencity.entity.telegram.TelegramChat;
 import greencity.entity.telegram.TelegramMessage;
@@ -11,11 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TelegramMessageStatusHandlerTest {
--- service/src/test/java/greencity/ubstelegrambot/TelegramUnreadCountNotifierTest.java
+++ service/src/test/java/greencity/ubstelegrambot/TelegramUnreadCountNotifierTest.java
@@ -1,5 +1,9 @@
 package greencity.ubstelegrambot;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import greencity.enums.MessageViewingStatus;
 import greencity.producers.TelegramChatProducer;
 import greencity.repository.TelegramMessageRepository;
@@ -9,11 +13,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TelegramUnreadCountNotifierTest {
