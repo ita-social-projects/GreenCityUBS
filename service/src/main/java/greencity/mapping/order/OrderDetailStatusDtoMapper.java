8f37f8801c2eb1af01ec66c11c509be58e43b28c
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2025-11-03 12:34:26 +0200

    Bugfix (#9266) - Manual payment error
    * added mapper to convert order to order dto with statuses

---
+++ service/src/main/java/greencity/mapping/order/OrderDetailStatusDtoMapper.java
@@ -0,0 +1,19 @@
+package greencity.mapping.order;
+
+import greencity.dto.order.OrderDetailStatusDto;
+import greencity.entity.order.Order;
+import org.modelmapper.AbstractConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderDetailStatusDtoMapper extends AbstractConverter<Order, OrderDetailStatusDto> {
+    @Override
+    protected OrderDetailStatusDto convert(Order order) {
+        return OrderDetailStatusDto.builder()
+            .id(order.getId())
+            .orderStatus(order.getOrderStatus().name())
+            .paymentStatus(order.getOrderPaymentStatus().name())
+            .date(order.getOrderDate().toLocalDate())
+            .build();
+    }
+}
--- service/src/test/java/greencity/service/ubs/UBSManagementServiceImplTest.java
+++ service/src/test/java/greencity/service/ubs/UBSManagementServiceImplTest.java
@@ -446,6 +446,7 @@ class UBSManagementServiceImplTest {
         user.setRecipientName("Петро");
         user.setRecipientSurname("Петренко");
         Order order = user.getOrders().getFirst();
+        order.setOrderStatus(null);
         order.setOrderDate((LocalDateTime.now()));
 
         List<Payment> payment = new ArrayList<>();
