9a90d27292a1907bf561d48a099cf7d466510ef0
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2025-11-03 15:25:25 +0200

    Add WebSocketController

---
+++ core/src/main/java/greencity/controller/WebSocketController.java
@@ -0,0 +1,32 @@
+package greencity.controller;
+
+import greencity.service.ubs.TelegramUnreadCountNotifier;
+import io.github.springwolf.bindings.stomp.annotations.StompAsyncOperationBinding;
+import io.github.springwolf.core.asyncapi.annotations.AsyncListener;
+import io.github.springwolf.core.asyncapi.annotations.AsyncOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class WebSocketController {
+    private final TelegramUnreadCountNotifier telegramUnreadCountNotifier;
+
+    /**
+     * Handles client subscriptions to the "/app/unread" destination.
+     *
+     * <p>When a client subscribes, this method triggers the notifier service to
+     * calculate and broadcast the current unread message count to all subscribers
+     * via the WebSocket message broker.
+     */
+    @AsyncListener(
+            operation = @AsyncOperation(
+                    channelName = "/app/unread",
+                    description = "Subscription endpoint that triggers initial unread message count broadcast"))
+    @StompAsyncOperationBinding
+    @SubscribeMapping("/unread")
+    public void onSubscribe() {
+        telegramUnreadCountNotifier.notifyUnreadCount();
+    }
+}
