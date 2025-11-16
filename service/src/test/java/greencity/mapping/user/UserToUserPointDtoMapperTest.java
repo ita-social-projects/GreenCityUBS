5c37f16b3ad54f357d9e366f82b6f9c7fcb8c8d2
BohdanLys <mrboba04@gmail.com>
2025-11-16 13:15:52 +0200

    Added test case for new mapper

---
+++ service/src/test/java/greencity/mapping/user/UserToUserPointDtoMapperTest.java
@@ -0,0 +1,25 @@
+package greencity.mapping.user;
+
+import greencity.ModelUtils;
+import greencity.dto.user.UserPointDto;
+import greencity.entity.user.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class UserToUserPointDtoMapperTest {
+    @InjectMocks
+    private UserToUserPointDtoMapper userToUserPointDtoMapper;
+
+    @Test
+    void convert() {
+        User user = ModelUtils.getUser();
+        UserPointDto expected = UserPointDto.builder().points(100).build();
+        UserPointDto actual = userToUserPointDtoMapper.convert(user);
+        assertEquals(actual.getPoints(), expected.getPoints());
+    }
+}
