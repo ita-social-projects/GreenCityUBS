e24bfea526176e0ab9609a001735700ee6554e95
BohdanLys <mrboba04@gmail.com>
2025-11-20 19:04:01 +0200

    Fixed issue with changing price for paid orders and small improvements in payment process through QR-code

--- service/src/main/java/greencity/service/ubs/order/OrderServiceImpl.java
+++ service/src/main/java/greencity/service/ubs/order/OrderServiceImpl.java
@@ -256,6 +256,7 @@ public class OrderServiceImpl implements OrderService {
         List<CertificateDto> certificates = mapCertificates(order);
 
         Long fullPrice = bagCalculatorService.calculateBagsSum(bags);
+        log.info("fullPrice - " + fullPrice);
         Long amountWithDiscount = calculateDiscountedAmount(order, fullPrice, certificates);
         List<PaymentWithStatusDto> payments = order.getPayment().stream()
             .map(payment -> modelMapper.map(payment, PaymentWithStatusDto.class))
