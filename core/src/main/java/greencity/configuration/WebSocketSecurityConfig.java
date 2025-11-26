91909910155d7e029de71c7ad5b9379ec1f7aa3d
SaenkoVova <vova080520@gmail.com>
2025-11-26 15:38:39 +0200

    Task #9287 fixed duplications

--- core/src/main/java/greencity/configuration/WebSocketSecurityConfig.java
+++ core/src/main/java/greencity/configuration/WebSocketSecurityConfig.java
@@ -1,5 +1,6 @@
 package greencity.configuration;
 
+import greencity.constant.AppConstant;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
@@ -9,9 +10,9 @@ public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBro
     @Override
     protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
         messages
-            .simpSubscribeDestMatchers("/topic/chats/**").hasRole("UBS_EMPLOYEE")
-            .simpSubscribeDestMatchers("/topic/messages/**").hasRole("UBS_EMPLOYEE")
-            .simpSubscribeDestMatchers("/topic/unread/**").hasRole("UBS_EMPLOYEE")
+            .simpSubscribeDestMatchers("/topic/chats/**").hasRole(AppConstant.UBS_EMPLOYEE)
+            .simpSubscribeDestMatchers("/topic/messages/**").hasRole(AppConstant.UBS_EMPLOYEE)
+            .simpSubscribeDestMatchers("/topic/unread/**").hasRole(AppConstant.UBS_EMPLOYEE)
             .anyMessage().authenticated();
     }
 
