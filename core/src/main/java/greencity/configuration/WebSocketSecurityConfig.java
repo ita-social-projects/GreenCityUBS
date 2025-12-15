f2ac5fb44bc498bf0526b7dace802f18beee6197
SaenkoVova <48810972+SaenkoVova@users.noreply.github.com>
2025-12-15 14:30:31 +0000

    Deploying to gh-pages from  @ c852c552ae74ea2385501ef3a03c276be862eb92 🚀

---
+++ core/src/main/java/greencity/configuration/WebSocketSecurityConfig.java
@@ -0,0 +1,36 @@
+package greencity.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.security.authorization.AuthorizationEventPublisher;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSocketSecurityConfig implements WebSocketMessageBrokerConfigurer {
+    private final ApplicationContext applicationContext;
+    private final AuthorizationManager<Message<?>> authorizationManager;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        AuthorizationChannelInterceptor authz = new AuthorizationChannelInterceptor(authorizationManager);
+        AuthorizationEventPublisher publisher = new SpringAuthorizationEventPublisher(applicationContext);
+        authz.setAuthorizationEventPublisher(publisher);
+        registration.interceptors(new SecurityContextChannelInterceptor(), authz);
+    }
+}
